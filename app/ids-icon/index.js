69e592b7e59b6c90fd2b5a48474cd6e397679bff
Tim McConechy <tim.mcconechy@infor.com>
2020-08-12 11:34:11 -0400

    Bump a number of dependencies and reroute using the new templating system

--- app/ids-icon/index.js
+++ app/ids-icon/index.js
@@ -8,13 +8,15 @@ import IdsLayoutColumn from '../../src/ids-layout-grid/ids-layout-column';
 // Example Code used in the examples
 
 // Here we append all the HTML to show off the icons to the icon div section
-const section = document.querySelector('.ids-icon-list');
-let iconHtml = '';
-Object.entries(pathData).map((x) => {
-  iconHtml += `<span class="ids-grid-wrap"><span class="ids-label ids-text-px-10">icon-${x[0]}</span><br/>
-    <ids-icon icon="${x[0]}" compactness="wide"></ids-icon>
-    <ids-icon icon="${x[0]}"></ids-icon>
-    <ids-icon icon="${x[0]}" compactness="condensed"></ids-icon></span>`;
-  return x;
+document.addEventListener('DOMContentLoaded', () => {
+  const section = document.querySelector('.ids-icon-list');
+  let iconHtml = '';
+  Object.entries(pathData).map((x) => {
+    iconHtml += `<span class="ids-grid-wrap"><span class="ids-label ids-text-px-10">icon-${x[0]}</span><br/>
+      <ids-icon icon="${x[0]}" compactness="wide"></ids-icon>
+      <ids-icon icon="${x[0]}"></ids-icon>
+      <ids-icon icon="${x[0]}" compactness="condensed"></ids-icon></span>`;
+    return x;
+  });
+  section.insertAdjacentHTML('beforeend', iconHtml);
 });
-section.insertAdjacentHTML('beforeend', iconHtml);
