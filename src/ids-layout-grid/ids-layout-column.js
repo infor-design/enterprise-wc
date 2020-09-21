80d27f7d66dd67be4e20f2dee84ee201f82c5ca8
John Macaluso <John.Macaluso@infor.com>
2020-09-21 16:53:20 -0400

    Add some row settings for implicit grid

--- src/ids-layout-grid/ids-layout-column.js
+++ src/ids-layout-grid/ids-layout-column.js
@@ -18,7 +18,7 @@ class IdsLayoutColumn extends IdsElement {
    * @returns {Array} The properties in an array
    */
   static get properties() {
-    return ['fill', 'col-span', 'col-start', 'col-end', 'row-start', 'row-end'];
+    return ['fill', 'col-span', 'col-start', 'col-end', 'row-span', 'row-start', 'row-end'];
   }
 
   /**
@@ -44,12 +44,6 @@ class IdsLayoutColumn extends IdsElement {
   }
 
   /**
-   * Handle the span setting
-   * @returns {string} The amount of columns to span in the grid
-   */
-  get colSpan() { return this.getAttribute('span') };
-
-  /**
    * Set the amount of columns to span
    * @param {string} value The number value for the columns to span in the grid
    */
@@ -63,6 +57,93 @@ class IdsLayoutColumn extends IdsElement {
     this.classList.remove(`ids-layout-col-span-${value}`);
     this.removeAttribute('col-span');
   }
+
+  get colSpan() { return this.getAttribute('col-span') };
+
+  /**
+   * Set the starting column
+   * @param {string} value The number value for the column starting point
+   */
+  set colStart(value) {
+    if (value) {
+      this.setAttribute('col-start', value);
+      this.classList.add(`ids-layout-col-start-${value}`);
+      return;
+    }
+
+    this.classList.remove(`ids-layout-col-start-${value}`);
+    this.removeAttribute('col-start');
+  }
+
+  get colStart() { return this.getAttribute('col-start') };
+
+  /**
+   * Set the ending column
+   * @param {string} value The number value for the column starting point
+   */
+  set colEnd(value) {
+    if (value) {
+      this.setAttribute('col-end', value);
+      this.classList.add(`ids-layout-col-end-${value}`);
+      return;
+    }
+
+    this.classList.remove(`ids-layout-col-end-${value}`);
+    this.removeAttribute('col-end');
+  }
+
+  get colEnd() { return this.getAttribute('col-end') };
+
+  /**
+   * Set the amount of rows to span
+   * @param {string} value The number value for the rows to span in the grid
+   */
+  set rowSpan(value) {
+    if (value) {
+      this.setAttribute('row-span', value);
+      this.classList.add(`ids-layout-row-span-${value}`);
+      return;
+    }
+
+    this.classList.remove(`ids-layout-row-span-${value}`);
+    this.removeAttribute('row-span');
+  }
+
+  get rowSpan() { return this.getAttribute('row-span') };
+
+  /**
+   * Set the starting row
+   * @param {string} value The number value for the row starting point
+   */
+  set rowStart(value) {
+    if (value) {
+      this.setAttribute('row-start', value);
+      this.classList.add(`ids-layout-row-start-${value}`);
+      return;
+    }
+
+    this.classList.remove(`ids-layout-row-start-${value}`);
+    this.removeAttribute('row-start');
+  }
+
+  get rowStart() { return this.getAttribute('row-start') };
+
+  /**
+   * Set the ending row
+   * @param {string} value The number value for the row ending point
+   */
+  set rowEnd(value) {
+    if (value) {
+      this.setAttribute('row-end', value);
+      this.classList.add(`ids-layout-row-end-${value}`);
+      return;
+    }
+
+    this.classList.remove(`ids-layout-row-end-${value}`);
+    this.removeAttribute('row-end');
+  }
+
+  get rowEnd() { return this.getAttribute('row-end') };
 }
 
 export default IdsLayoutColumn;
