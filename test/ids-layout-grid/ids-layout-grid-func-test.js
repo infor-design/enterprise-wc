cd28523c1aeb76d89b87ab8b7dcf78c88dc46cac
John Macaluso <John.Macaluso@infor.com>
2020-09-28 13:12:47 -0400

    Add tests for new grid settings

--- test/ids-layout-grid/ids-layout-grid-func-test.js
+++ test/ids-layout-grid/ids-layout-grid-func-test.js
@@ -78,4 +78,85 @@ describe('IdsLayoutGrid Component', () => {
     expect(col.fill).toEqual(null);
     expect(document.querySelectorAll('.ids-background-fill').length).toEqual(0);
   });
+
+  it('renders auto setting', () => {
+    const elem = new IdsLayoutGrid();
+    elem.auto = true;
+    document.body.appendChild(elem);
+    expect(elem.auto).toEqual('true');
+    expect(document.querySelectorAll('.ids-layout-cols-auto').length).toEqual(1);
+  });
+
+  it('renders auto setting then removes it', () => {
+    const elem = new IdsLayoutGrid();
+    elem.auto = true;
+    elem.auto = false;
+    document.body.appendChild(elem);
+    expect(elem.auto).toEqual(null);
+    expect(document.querySelectorAll('.ids-layout-cols-auto').length).toEqual(0);
+  });
+
+  it('renders cols setting', () => {
+    const elem = new IdsLayoutGrid();
+    elem.cols = 16;
+    document.body.appendChild(elem);
+    expect(elem.cols).toEqual('16');
+    expect(document.querySelectorAll('.ids-layout-cols-16').length).toEqual(1);
+  });
+
+  it('renders rows setting', () => {
+    const elem = new IdsLayoutGrid();
+    elem.rows = 4;
+    document.body.appendChild(elem);
+    expect(elem.rows).toEqual('4');
+    expect(document.querySelectorAll('.ids-layout-rows-4').length).toEqual(1);
+  });
+
+  it('renders col-span setting', () => {
+    const col = new IdsLayoutColumn();
+    col.colSpan = 4;
+    document.body.appendChild(col);
+    expect(col.colSpan).toEqual('4');
+    expect(document.querySelectorAll('.ids-layout-col-span-4').length).toEqual(1);
+  });
+
+  it('renders col-start setting', () => {
+    const col = new IdsLayoutColumn();
+    col.colStart = 4;
+    document.body.appendChild(col);
+    expect(col.colStart).toEqual('4');
+    expect(document.querySelectorAll('.ids-layout-col-start-4').length).toEqual(1);
+  });
+
+  it('renders col-end setting', () => {
+    const col = new IdsLayoutColumn();
+    col.colEnd = 4;
+    document.body.appendChild(col);
+    expect(col.colEnd).toEqual('4');
+    expect(document.querySelectorAll('.ids-layout-col-end-4').length).toEqual(1);
+  });
+
+  it('renders row-span setting', () => {
+    const col = new IdsLayoutColumn();
+    col.rowSpan = 4;
+    document.body.appendChild(col);
+    expect(col.rowSpan).toEqual('4');
+    expect(document.querySelectorAll('.ids-layout-row-span-4').length).toEqual(1);
+  });
+
+  it('renders row-start setting', () => {
+    const col = new IdsLayoutColumn();
+    col.rowStart = 4;
+    document.body.appendChild(col);
+    expect(col.rowStart).toEqual('4');
+    expect(document.querySelectorAll('.ids-layout-row-start-4').length).toEqual(1);
+  });
+
+  it('renders row-end setting', () => {
+    const col = new IdsLayoutColumn();
+    col.rowEnd = 4;
+    document.body.appendChild(col);
+    expect(col.rowEnd).toEqual('4');
+    expect(document.querySelectorAll('.ids-layout-row-end-4').length).toEqual(1);
+  });
 });
