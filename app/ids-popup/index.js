3bb42bf5aa5e0ed6bf0a5f67e50d2c5fb261a549
Ed Coyle <Ed.Coyle@infor.com>
2020-11-11 13:27:04 -0500

    Add supporting components to popup example

--- app/ids-popup/index.js
+++ app/ids-popup/index.js
@@ -1,5 +1,11 @@
 import IdsPopup from '../../src/ids-popup/ids-popup';
 
+// Supporting Components
+import IdsButton from '../../src/ids-button/ids-button';
+import IdsLabel from '../../src/ids-label/ids-label';
+import IdsLayoutGridCell from '../../src/ids-layout-grid/ids-layout-grid-cell';
+import IdsLayoutGrid from '../../src/ids-layout-grid/ids-layout-grid';
+
 import './index.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
