2b8abb1c60c0aa17c9e167fa275a5e31520fc732
Deepak Verma <deepak.verma@infor.com>
2021-01-11 17:38:29 -0500

    Fix e2e test

--- test/ids-checkbox/ids-checkbox-e2e-test.js
+++ test/ids-checkbox/ids-checkbox-e2e-test.js
@@ -22,8 +22,7 @@ describe('Ids Checkbox e2e Tests', () => {
   it('should not have visual regressions (percy)', async () => {
     page = await browser.newPage();
     await page.setBypassCSP(true);
-    await page.goto(url, { waitUntil: 'load' });
-    await page.waitForSelector('.no-required-indicator', { timeout: 5000 });
+    await page.goto(url, { waitUntil: 'networkidle0' });
     await percySnapshot(page, 'ids-checkbox');
   });
 });
