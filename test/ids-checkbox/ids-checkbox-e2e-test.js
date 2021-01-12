25f5b28a464e35d332748648b7721a6159259b9c
Deepak Verma <deepak.verma@infor.com>
2021-01-11 19:10:09 -0500

    Fix e2e test

--- test/ids-checkbox/ids-checkbox-e2e-test.js
+++ test/ids-checkbox/ids-checkbox-e2e-test.js
@@ -23,37 +23,7 @@ describe('Ids Checkbox e2e Tests', () => {
     page = await browser.newPage();
     await page.setBypassCSP(true);
     await page.goto(url, { waitUntil: 'load' });
-    // -----
-    const waitTillHTMLRendered = async (pg, timeout = 30000) => {
-      const checkDurationMsecs = 1000;
-      const maxChecks = timeout / checkDurationMsecs;
-      let lastHTMLSize = 0;
-      let checkCounts = 1;
-      let countStableSizeIterations = 0;
-      const minStableSizeIterations = 3;
-
-      while (checkCounts++ <= maxChecks) {
-        const html = await pg.content(); // eslint-disable-line
-        const currentHTMLSize = html.length;
-
-        const bodyHTMLSize = await pg.evaluate(() => document.body.innerHTML.length); // eslint-disable-line
-
-        if (lastHTMLSize !== 0 && currentHTMLSize === lastHTMLSize) {
-          countStableSizeIterations++;
-        } else {
-          countStableSizeIterations = 0; // reset the counter
-        }
-
-        if (countStableSizeIterations >= minStableSizeIterations) {
-          break;
-        }
-
-        lastHTMLSize = currentHTMLSize;
-        await pg.waitFor(checkDurationMsecs); // eslint-disable-line
-      }
-    };
-    await waitTillHTMLRendered(page);
+    await page.waitFor(5000);
     await percySnapshot(page, 'ids-checkbox');
-    // await percySnapshot(page, 'ids-checkbox');
   });
 });
