5451d9f3c3695777cba30eb95feedf792ca57ad2
Eric Angeles <enrico.angeles@infor.com>
2021-02-05 00:11:45 +0800

    Add ids badge component

---
+++ src/ids-badge/ids-badge.js
@@ -0,0 +1,96 @@
+import {
+  IdsElement,
+  customElement,
+  scss
+} from '../ids-base/ids-element';
+// @ts-ignore
+import styles from './ids-badge.scss';
+
+/**
+ * IDS Badge Component
+ */
+@customElement('ids-badge')
+@scss(styles)
+class IdsBadge extends IdsElement {
+  constructor() {
+    super();
+  }
+
+  /**
+   * Return the properties we handle as getters/setters
+   * @returns {Array} The properties in an array
+   */
+  static get properties() {
+    return ['color', 'shape'];
+  }
+
+  /**
+   * Create the Template for the contents
+   * @returns {string} The Template
+   */
+  template() {
+    const shape = this.shape;
+
+    return `<span class="ids-badge ${shape}"><slot></slot></span>`;
+  }
+
+  /**
+   * Return the badge shape
+   * @returns {string} The path data
+   */
+  get shape() { return this.getAttribute('shape') || 'normal'; }
+
+  /**
+   * Set the shape of the badge
+   * @param {string} value The Badge Shape
+   */
+  set shape(value) {
+    if (value) {
+      this.setAttribute('shape', value.toString());
+    } else {
+      this.removeAttribute('shape');
+    }
+  }
+
+  /**
+   * Return the badge color
+   * @returns {string} the path data
+   */
+  get color() { return this.getAttribute('color'); }
+
+  /**
+   * Set the color
+   * @param {string} value The Badge Color [base, error, info, success and alert]
+   */
+  set color(value) {
+    if (value) {
+      this.setAttribute('color', value);
+
+      let statusColor;
+
+      if (value === 'error') {
+        statusColor = 'var(--ids-color-status-danger)';
+      } else if (value === 'alert') {
+        statusColor = 'var(--ids-color-status-caution)';
+      } else if (value === 'info') {
+        statusColor = 'var(--ids-color-status-base)';
+      } else {
+        statusColor = `var(--ids-color-status-${value})`;
+      }
+
+      this.container.style.backgroundColor = statusColor;
+      this.container.style.borderColor = statusColor;
+      if (value === 'error' || value === 'info' || value === 'success') {
+        this.container.classList.add('ids-white');
+      }
+    } else {
+      this.removeAttribute('color');
+      this.container.style.backgroundColor = '';
+      this.container.style.borderColor = '';
+      this.container.style.color = '';
+      this.container.style.position = '';
+    }
+  }
+}
+
+export default IdsBadge;
