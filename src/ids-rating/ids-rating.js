3653cb3f31b3765f215c2c231d92f2293c7e4dff
dortiz <dortiz@dortizs-MacBook-Pro.local>
2021-03-31 15:40:48 -0400

    4972 - removed unused shadowRoot added index attr

--- src/ids-rating/ids-rating.js
+++ src/ids-rating/ids-rating.js
@@ -5,7 +5,7 @@ import {
     mix
   } from '../ids-base/ids-element';
 
-  import { IdsEventsMixin } from '../ids-base/ids-events-mixin';
+import { IdsEventsMixin } from '../ids-base/ids-events-mixin';
 import { IdsKeyboardMixin } from '../ids-base/ids-keyboard-mixin';
 
 // @ts-ignore
@@ -46,25 +46,26 @@ import styles from './ids-rating.scss';
       this.addRemoveClass();
     }
 
-     /**
-   * Create the Template for the contents
-   * @returns {string} The template
-   */
-  template() {
-    return `<div id="rating"></div>`;
-  }
+    /**
+     * Create the Template for the contents
+     * @returns {string} The template
+     */
+    template() {
+      return `<div id="rating"></div>`;
+    }
 
     buildDOM() {
       const ratingsContainer = this.shadowRoot.querySelector('#rating')
       this.ratingBuilder(ratingsContainer);
-      this.shadowRoot.append(ratingsContainer);
     }
 
     /**
     * @param {HTMLDivElement} element
     */
-    buildRatingStar(element){
-      const section = window.document.createElement('section')
+    buildRatingStar(element, index){
+      const section = window.document.createElement('section');
+      section.classList.add('rating-item');
+      section.setAttribute('item-index', `item-${index}`);
       element.appendChild(section);
     }
 
@@ -72,30 +73,30 @@ import styles from './ids-rating.scss';
     * @param {HTMLDivElement} el
     */
     ratingBuilder(el){
-      const amount = this.ratingsConfig.ratingsAttr.star
+      const amount = this.ratingsConfig.ratingsAttr.star;
       for(let i=0; i<amount; i++){
-          this.buildRatingStar(el);
+          this.buildRatingStar(el, i);
       }
     }
 
     addRemoveClass(){
-      const ratingContainer = this.shadowRoot.querySelector('#rating')
+      const ratingContainer = this.shadowRoot.querySelector('#rating');
       ratingContainer.addEventListener('click', (/** @type {{ target: any; }} */ e) => {
           let action = 'add';
           for (const section of ratingContainer.children) {
               section.classList[action]('active');
-              this.udpateValue()
+              this.udpateValue();
               if (section === e.target) {
-                  action = 'remove'
+                  action = 'remove';
               }
           }
       })
     }
 
     udpateValue(){
-      const ratingChildren = this.shadowRoot.querySelector('#rating').childNodes
-      const activeArr = [...ratingChildren].filter( el => el.classList.contains('active'))
-      this.ratingsConfig.ratingsAttr.value = this.setAttribute('value', activeArr.length)
+      const ratingChildren = this.shadowRoot.querySelector('#rating').childNodes;
+      const activeArr = [...ratingChildren].filter( el => el.classList.contains('active'));
+      this.ratingsConfig.ratingsAttr.value = this.setAttribute('value', activeArr.length);
     }
  }
 
