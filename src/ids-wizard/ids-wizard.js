5ac41381a29e69d5a8c9ecc47a963ac482f1d734
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-04-07 19:41:08 -0400

    ids-wizard: add labels, deprecate unused css in previous layout

--- src/ids-wizard/ids-wizard.js
+++ src/ids-wizard/ids-wizard.js
@@ -29,7 +29,8 @@ class IdsWizard extends IdsElement {
    * @returns {string} The Template
    */
   template() {
-    let wizardStepHtml = '';
+    let stepsBarInnerHtml = '';
+    let stepLabelsInnerHtml = '';
 
     // iterate through ids-wizard-step
     // lightDOM to create shadowDOM markup
@@ -40,9 +41,26 @@ class IdsWizard extends IdsElement {
     for (const [i, stepEl] of [...this.children].entries()) {
       const isCurrentStep = stepIndex === i;
       const isVisitedStep = i <= stepIndex;
+      const isClickable = stepEl.getAttribute('clickable');
+      const aHrefAttrib = isClickable ? ' href="#"' : '';
 
-      wizardStepHtml += (
-        `<div class="ids-wizard-step">
+      // --------------------- //
+      // construct bar steps   //
+      // ===================== //
+
+      const markerClassName = clsx(
+        'bar-step',
+        isCurrentStep && 'current',
+        isVisitedStep && 'visited',
+        isClickable && 'clickable'
+      );
+
+      const pathSegmentHtml = (i >= this.children.length - 1) ? '' : (
+        `<div class="path-segment${stepIndex <= i ? '' : ' visited'}"></div>`
+      );
+
+      stepsBarInnerHtml += (
+        `<a class="${markerClassName}"${aHrefAttrib}>
           <div class="step-node">
             <svg viewBox="0 0 24 24">
               <circle cx="12" cy="12" r="12" />
@@ -52,23 +70,34 @@ class IdsWizard extends IdsElement {
                 <circle cx="12" cy="12" r="12" />
               </svg>`) }
           </div>
-        </div>`
+        </a>
+        ${pathSegmentHtml}`
       );
 
-      if (i < this.children.length - 1) {
-        wizardStepHtml += (
-          `<div class="path-segment${
-            stepIndex <= i ? '' : ' visited'
-          }">
-          </div>`
-        );
-      }
+      // --------------------- //
+      // construct labels      //
+      // ===================== //
+
+      // @ts-ignore
+      const label = stepEl.innerText;
+      stepLabelsInnerHtml += (
+        `<a class="step-label${isVisitedStep ? ' visited' : ''}">
+          <ids-text
+            size=18
+            font-weight="${isCurrentStep ? 'bold' : 'normal'}"
+          >${label}
+          </ids-text>
+        </a>`
+      );
     }
 
     return (
       `<div class="ids-wizard">
-        <div class="bar">
-          ${wizardStepHtml}
+        <div class="marker-bar">
+          ${stepsBarInnerHtml}
+        </div>
+        <div class="step-labels">
+          ${stepLabelsInnerHtml}
         </div>
       </div>`
     );
