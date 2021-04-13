2e8c7dec335dcd19dacd0d6b99142dfebe23d69b
Chris Akinrinade <cakinrinade@USNYMTEST01.local>
2021-04-12 22:33:31 -0400

    continuing to iterate on counts

--- src/ids-counts/ids-counts.js
+++ src/ids-counts/ids-counts.js
@@ -1,14 +1,28 @@
 import {
   IdsElement,
   customElement,
-  // mix,
   scss,
-  // props
+  props
 } from '../ids-base/ids-element';
+
+// @ts-ignore
+import IdsText from '../ids-text/ids-text';
 
 // @ts-ignore
 import styles from './ids-counts.scss';
 
+// Boilerplate text for ShadowDOM tags
+const textTags = {
+  text: '<ids-text font-size="16"><slot name="text"></slot></ids-text>',
+  value1: '<ids-text font-size=',
+  value2: '><slot name="value"></slot></ids-text>'
+};
+
+/**
+ * IDS Counts Component
+ * @type {IdsCounts}
+ * @inherits IdsElement
+ */
 @customElement('ids-counts')
 @scss(styles)
 class IdsCounts extends IdsElement {
@@ -17,21 +31,44 @@ class IdsCounts extends IdsElement {
   }
 
   /**
+   * Template helper function for creating ShadowDOM tags
+   * @returns {number} the font size
+   */
+  get numSize() {
+    return this.getAttribute('short') ? 32 : 40;
+  }
+
+  static get properties() {
+    return [props.COLOR];
+  }
+
+  /**
+   * Template helper function for creating ShadowDOM tags
+   * @param {string | boolean} disabled the disabled attribute or false
+   * @returns {object} Object containing the outer HTML to be rendered
+   */
+  generateTags(disabled) {
+    return {
+      openingTag: disabled ? '<span>' : `<a href=${this.getAttribute('href') || '#'}>`,
+      closingTag: disabled ? '</span>' : '</a>',
+      innerTags: disabled
+        ? `${textTags.text}${textTags.value1}${this.numSize}${textTags.value2}`
+        : `${textTags.value1}${this.numSize}${textTags.value2}${textTags.text}`
+    };
+  }
+
+  /**
    * Inner template contents
    * @returns {string} The template
    */
   template() {
-    const notActionable = this.attributes?.class?.nodeValue?.match('not-actionable');
-    const short = this.attributes?.class?.nodeValue?.match('short') ? ' class="short"' : '';
-    const opener = notActionable ? `<span class="ids-counts not-actionable">` : `<a class="ids-counts hyperlink" href="#">`;
-    const second = notActionable ? '<span><slot name="addl-text"></slot></span>' : `<span${short}><slot name="xl-text"></slot></span>`;
-    const third = notActionable ? `<span${short}><slot name="xl-text"></slot></span>` : '<span><slot name="addl-text"></slot></span>';
-    const closer = notActionable ? '</span>' : '</a>';
+    const { openingTag, closingTag, innerTags } = this.generateTags(this.getAttribute('disabled') || false);
 
-    return `${opener}
-     ${second}
-     ${third}
-     ${closer}`;
+    return `
+      ${openingTag}
+      ${innerTags}
+      ${closingTag}
+    `;
   }
 }
 
