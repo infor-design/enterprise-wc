2997fc0b8a217980592d557e7eddf2217b2260eb
dortiz <danielortiz1982@gmail.com>
2021-04-15 08:19:28 -0400

    4972 - update to testing file

--- test/ids-rating/ids-rating-func-test.js
+++ test/ids-rating/ids-rating-func-test.js
@@ -8,11 +8,11 @@
     beforeEach(async () => {
         const ratingComponent = new IdsRating();
         window.document.body.appendChild(ratingComponent)
-    })
+    });
 
     afterEach(async () => {
         window.document.body.innerHTML = '';
-    })
+    });
 
     it('renders with no errors', () => {
         const errors = jest.spyOn(global.console, 'error');
@@ -21,5 +21,21 @@
         elem.remove();
         expect(document.querySelectorAll('ids-rating').length).toEqual(1);
         expect(errors).not.toHaveBeenCalled();
-    })
+    });
+
+    it('renders correctly', () => {
+        const elem = new IdsRating();
+        window.document.body.appendChild(elem);
+        elem.template();
+        expect(elem.outerHTML).toMatchSnapshot();
+    });
+
+    it('renders rating item correctly', () => {
+        const elem = new IdsRating();
+        window.document.body.appendChild(elem);
+        elem.template();
+        const ratingItem = window.document.querySelectorAll('.rating-item');
+        console.log(elem)
+        //expect(ratingItem).toEqual(5)
+    });
  })
