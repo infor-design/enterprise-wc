5ee0bf0720e8d034940e62181838b4bc4fec7b14
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-04-20 12:29:51 -0400

    app/ids-wizard/example.js vs index.js for auxiliary imports

--- app/ids-wizard/index.js
+++ app/ids-wizard/index.js
@@ -1,5 +1,2 @@
 import IdsWizard from '../../src/ids-wizard/ids-wizard';
 import IdsWizardStep from '../../src/ids-wizard/ids-wizard-step';
-import IdsLayoutGrid from '../../src/ids-layout-grid/ids-layout-grid';
-import IdsLayoutGridCell from '../../src/ids-layout-grid/ids-layout-grid-cell';
-import IdsText from '../../src/ids-text/ids-text';
