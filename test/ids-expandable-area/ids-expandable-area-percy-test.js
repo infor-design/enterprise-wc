8978c1deb4633bc44721e708818cfc105e105ab2
Tim McConechy <tim.mcconechy@infor.com>
2021-04-22 14:26:51 -0400

    Fix tests?

--- test/ids-expandable-area/ids-expandable-area-percy-test.js
+++ test/ids-expandable-area/ids-expandable-area-percy-test.js
@@ -4,13 +4,11 @@ describe('Ids Expandable Area Percy Tests', () => {
   const url = 'http://localhost:4444/ids-expandable-area';
 
   it('should not have visual regressions in new light theme (percy)', async () => {
-    await page.setBypassCSP(true);
     await page.goto(url, { waitUntil: ['networkidle0', 'domcontentloaded'] });
     await percySnapshot(page, 'ids-expandable-area-new-light');
   });
 
   it('should not have visual regressions in new dark theme (percy)', async () => {
-    await page.setBypassCSP(true);
     await page.goto(url, { waitUntil: ['networkidle0', 'domcontentloaded'] });
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher').setAttribute('mode', 'dark');
@@ -19,7 +17,6 @@ describe('Ids Expandable Area Percy Tests', () => {
   });
 
   it('should not have visual regressions in new contrast theme (percy)', async () => {
-    await page.setBypassCSP(true);
     await page.goto(url, { waitUntil: ['networkidle0', 'domcontentloaded'] });
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher').setAttribute('mode', 'contrast');
