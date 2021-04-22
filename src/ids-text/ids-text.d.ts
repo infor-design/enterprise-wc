65ebad52d4bf6d2445d5692450e8c89a82c00b70
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-04-22 17:38:38 -0400

    Update src/ids-text/ids-text.d.ts
    PR Suggestion
    
    Co-authored-by: Tim McConechy <tim.mcconechy@infor.com>

--- src/ids-text/ids-text.d.ts
+++ src/ids-text/ids-text.d.ts
@@ -1,7 +1,7 @@
 // Ids is a JavaScript project, but we define TypeScript declarations so we can
 // confirm our code is type safe, and to support TypeScript users.
 
-/**
+/** A segment of text with standardized settings, theming and fonts*/
  * a segment of text with standardized settings, theming and fonts
  */
 export default class IdsText extends HTMLElement {
