c6ab25b1716721daef45fcbdb56545072f4aa5ec
dortiz <dortiz@dortizs-MacBook-Pro.local>
2021-04-29 15:43:57 -0400

    4972 - setup default values and base setters and getters

--- src/ids-rating/ids-rating.js
+++ src/ids-rating/ids-rating.js
@@ -29,7 +29,7 @@ class IdsRating extends mix(IdsElement).with(IdsEventsMixin, IdsKeyboardMixin) {
    }
 
     connectedCallback() {
-      this.addRemoveAttrName()
+      this.addRemoveAttrName();
     }
 
     /**
@@ -37,12 +37,13 @@ class IdsRating extends mix(IdsElement).with(IdsEventsMixin, IdsKeyboardMixin) {
      * @returns {string} The template
      */
     template() {
+      const stars = this.hasAttribute('stars') ? this.getAttribute('stars') : this.setAttribute('stars', '5');
+      const size = this.hasAttribute('size') ? this.getAttribute('size') : this.setAttribute('size', 'large');
       let html = '<div id="rating">';
-      const stars = this.hasAttribute('stars') ? this.getAttribute('stars') : 5;
-      for(let i=0; i < stars; i++) {
-       html += `<ids-icon class="${i} star" icon="star-outlined" size="large"></ids-icon>`;
+      for(let i = 0; i < stars; i++) {
+       html += `<ids-icon class="star" aria-label="${i} Star" role-"button" icon="star-outlined" tabindex="0" size="${size}"></ids-icon>`;
       }
-      html += '</div>'
+      html += '</div>';
       return html;
     }
 
@@ -54,32 +55,80 @@ class IdsRating extends mix(IdsElement).with(IdsEventsMixin, IdsKeyboardMixin) {
     }
 
     set value(val) {
+      if(val) {
+        this.setAttribute('value', val.toString());
+      }
     }
 
     get value() {
-      return 
+      return this.getAttribute('value');
       
     }
 
     set stars(num) {
-
+      if(num) {
+        this.setAttribute('stars', num.toString());
+      }
     }
 
     get stars() {
-      return
+      return this.getAttribute('stars');
     }
 
     set readonly(ro) {
+      if(ro){
+        this.setAttribute('readonly', ro.toString());
+      }
     }
 
     get readonly() {
-      return
+      return this.getAttribute('readonly');
     }
 
-    addRemoveAttrName(valaue) {
+    set clickable(cl) {
+      if(c) {
+        this.setAttribute('clickable', cl.toString());
+      }
+    }
+
+    get clickable() {
+      return this.getAttribute('clickable');
+    }
+
+    set compact(com) {
+      if(com) {
+        this.setAttribute('compact', com.toString());
+      }
+    }
+
+    get compact() {
+      return this.getAttribute('compact');
+    }
+
+    set color(col) {
+      if(col) {
+        this.setAttribute('color', col.toString());
+      }
+    }
+
+    get color() {
+      return this.getAttribute('color');
+    }
+
+    set size(s) {
+      if(s) {
+        this.setAttribute('size', s.toString());
+      }
+    }
+
+    get size() {
+      return this.getAttribute('size');
+    }
+
+    addRemoveAttrName() {
       const ratingContainer = this.shadowRoot.querySelector('#rating');
       const ratingArr = [...ratingContainer.children];
-      ratingArr.forEach((e, index) => e.classList.add(`star-${index}`))
+      ratingArr.forEach((e, index) => e.classList.add(`star-${index}`));
       this.onEvent('click', ratingContainer, (/** @type {{ target: any; }} */ e) => {
         const activeElements = ratingArr.filter((item) => item.classList.contains('active'));
         let attrName = 'star-filled';
