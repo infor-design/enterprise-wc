32e5892c6a24eec5de6c5d3af1fd32c759c6bcd1
dortiz <dortiz@dortizs-MacBook-Pro.local>
2021-05-11 12:07:21 -0400

    4972 - fn test renders with no errors

--- test/ids-rating/ids-rating-func-test.js
+++ test/ids-rating/ids-rating-func-test.js
@@ -5,37 +5,21 @@
  import IdsRating from '../../src/ids-rating/ids-rating';
 
  describe('IdsRating Component', () => {
+     let rating;
+
     beforeEach(async () => {
-        const ratingComponent = new IdsRating();
-        window.document.body.appendChild(ratingComponent)
+        const element = `<ids-rating></ids-rating>`
+        window.document.body.innerHTML = element;
     });
 
     afterEach(async () => {
         window.document.body.innerHTML = '';
+        rating = null;
     });
 
     it('renders with no errors', () => {
         const errors = jest.spyOn(global.console, 'error');
-        const elem = new IdsRating()
-        window.document.body.appendChild(elem)
-        elem.remove();
-        expect(document.querySelectorAll('ids-rating').length).toEqual(1);
+        expect(window.document.querySelectorAll('ids-rating').length).toEqual(1);
         expect(errors).not.toHaveBeenCalled();
-    });
-
-    it('renders correctly', () => {
-        const elem = new IdsRating();
-        window.document.body.appendChild(elem);
-        elem.template();
-        expect(elem.outerHTML).toMatchSnapshot();
-    });
-
-    it('renders rating item correctly', () => {
-        const elem = new IdsRating();
-        window.document.body.appendChild(elem);
-        elem.template();
-        const ratingItem = window.document.querySelectorAll('.rating-item');
-        elem.appendChild(ratingItem)
-        expect(elem.children).toBe(5)
     });
  })
