7b65ab885c808012d126cecef015851cf83e44c0
dortiz <dortiz@dortizs-MBP.lan1>
2021-05-12 14:48:39 -0400

    4972 - update value attr to fn test

--- test/ids-rating/ids-rating-func-test.js
+++ test/ids-rating/ids-rating-func-test.js
@@ -6,14 +6,23 @@
 
  describe('IdsRating Component', () => {
      let rating;
-
+     let icons;
 
     beforeEach(async () => {
-        const element = `<ids-rating value="0" stars="5" readonly="false" size="large"></ids-rating>`;
+        const element = `
+        <ids-rating value="0" stars="5" readonly="false" size="large">
+            <div id="rating">
+                <ids-icon class="star star-0" aria-label="Star-0" role="button" icon="star-outlined" tabindex="0" size="large"></ids-icon>
+                <ids-icon class="star star-1" aria-label="Star-1" role="button" icon="star-outlined" tabindex="0" size="large"></ids-icon>
+                <ids-icon class="star star-2" aria-label="Star-2" role="button" icon="star-outlined" tabindex="0" size="large"></ids-icon>
+                <ids-icon class="star star-3" aria-label="Star-3" role="button" icon="star-outlined" tabindex="0" size="large"></ids-icon>
+                <ids-icon class="star star-4" aria-label="Star-4" role="button" icon="star-outlined" tabindex="0" size="large"></ids-icon>
+            </div>
+        </ids-rating>`;
         const idsIcon = `<ids-icon icon="star-outlined"></ids-icon>`;
         window.document.body.innerHTML = element;
         rating = window.document.querySelectorAll('ids-rating');
-        rating.innerHTML = idsIcon;
+        icons = window.document.querySelectorAll('ids-icon');
     });
 
     afterEach(async () => {
@@ -29,8 +38,10 @@
 
     it('Has value attribute', () => {
         const el = rating[0];
-        const icon = window.document.querySelectorAll('ids-icon')[0]
         el.setAttribute('value', '3');
+        const iconSetAttr = window.document.querySelectorAll('ids-icon')[0]
+        iconSetAttr.setAttribute('icon', 'star-outlined');
+        expect(iconSetAttr.getAttribute('icon')).toEqual('star-outlined')
         expect(el.getAttribute('value')).toEqual('3');
     });
 
@@ -46,7 +57,7 @@
         expect(el.getAttribute('size')).toEqual('large');
     });
 
-    it('Has stars', () => {
+    it('Has stars attribute', () => {
         const el = rating[0];
         el.setAttribute('stars', '5');
         expect(el.getAttribute('stars')).toEqual('5');
