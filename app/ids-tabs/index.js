4a9ad7e36a3146a3f99f1013eb0d00044dce2ed0
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-05-18 12:56:38 -0400

    split up example into: example.html, vertical.html, counts.html

--- app/ids-tabs/index.js
+++ app/ids-tabs/index.js
@@ -1,14 +1,1 @@
 import IdsTabs, { IdsTab, IdsTabsDivider } from '../../src/ids-tabs';
-
-const overallContainer = document.querySelector('#test-ids-tabs');
-const tabContainers = [...overallContainer.children];
-
-for (const c of tabContainers) {
-  if (!c.matches(':first-child')) {
-    c.style.marginTop = '8px';
-  }
-
-  if (c.matches(':nth-child(2)')) {
-    c.style.maxWidth = '400px';
-  }
-}
