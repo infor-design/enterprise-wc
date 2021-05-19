6aa6b7947f677e7ab05da1bcfd2ea39099a2bad6
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-05-19 14:18:17 -0400

    Update src/ids-tabs/ids-tab.js
    Co-authored-by: Tim McConechy <tim.mcconechy@infor.com>

--- src/ids-tabs/ids-tab.js
+++ src/ids-tabs/ids-tab.js
@@ -146,7 +146,7 @@ class IdsTab extends mix(IdsElement).with(IdsEventsMixin) {
    * @param {string} value value which becomes selected by tabs component
    */
   set value(value) {
-    /* istanbul ignore else */
+    /* istanbul ignore next */
     if (value !== this.getAttribute(props.VALUE)) {
       /* istanbul ignore next */
       this.setAttribute(props.VALUE, value);
