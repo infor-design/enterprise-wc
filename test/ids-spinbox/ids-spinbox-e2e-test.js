17a79f907f65a1348eb13503ec7381b39fc9f899
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-05-21 00:07:53 -0400

    ids-spinbox: test coverage (90+%, just missing arrow keys)

---
+++ test/ids-spinbox/ids-spinbox-e2e-test.js
@@ -0,0 +1,17 @@
+describe('Ids Spinbox e2e Tests', () => {
+  const url = 'http://localhost:4444/ids-spinbox';
+
+  beforeAll(async () => {
+    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
+  });
+
+  it('should not have errors', async () => {
+    await expect(page.title()).resolves.toMatch('IDS Spinbox Component');
+  });
+
+  it('should pass Axe accessibility tests', async () => {
+    await page.setBypassCSP(true);
+    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
+    await expect(page).toPassAxeTests();
+  });
+});
