65d8f077a3cf6bfc1fa1319ce0dac72c77267873
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-06-01 19:11:04 -0400

    boilerplate on page-index/-item-count/-size, README

--- src/ids-pager/README.md
+++ src/ids-pager/README.md
@@ -1,13 +1,19 @@
 # Ids Pager Component
 
-
 ## Use Cases
 
 ## Terminology
+Page Section: `IdsPageSection`/`<ids-page-section>`; represents a panel on the pager (e.g. left, middle, right/up to three are embedded in the component, otherwise just one main section).
 
 ### Alignment
 
 ### Other
+
+## Settings
+
+- `page-item-count` {number} number of items to paginate.
+- `page-index` {number} 0-based page index. Represents what page is currently being viewed.
+- `page-size` {number} how many pages to display per-page.
 
 ## Themeable Parts
 
@@ -18,7 +24,8 @@
 **container** - the `ids-pager-section` container
 
 ## Features (with code samples)
-```
+
+```html
 <ids-pager>
   Test Content
 </ids-pager>
--- src/ids-pager/ids-pager.js
+++ src/ids-pager/ids-pager.js
@@ -21,7 +21,7 @@ export default class IdsPager extends IdsElement {
   }
 
   template() {
-    if (!this.hasSectionChildren()) {
+    if (!this.hasSectionContainers()) {
       return (
         `<ids-pager-section><slot></slot></ids-pager-section>`
       );
@@ -34,14 +34,12 @@ export default class IdsPager extends IdsElement {
     );
   }
 
-  hasSectionChildren() {
-    for (const el of this.children) {
-      if (el instanceof IdsPagerSection) {
-        return true;
-      }
-    }
-
-    return false;
+  get properties() {
+    return [
+      props.PAGE_INDEX,
+      props.PAGE_ITEM_COUNT,
+      props.PAGE_SIZE
+    ];
   }
 
   connectedCallback() {
@@ -53,10 +51,32 @@ export default class IdsPager extends IdsElement {
     });
 
     this.#updateChildrenProps();
+
+    if (!this.hasAttribute(props.PAGE_INDEX)) {
+      this.setAttribute(props.PAGE_INDEX, 1);
+    }
   }
 
+  /**
+   * @returns {boolean} whether or not IdsPageSection containers were
+   * provided to content
+   */
+  hasSectionContainers() {
+    for (const el of this.children) {
+      if (el instanceof IdsPagerSection) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * updates children start/end properties
+   * as needed for alignment
+   */
   #updateChildrenProps() {
-    if (!this.hasSectionChildren()) {
+    if (!this.hasSectionContainers()) {
       return;
     }
 
@@ -87,6 +107,47 @@ export default class IdsPager extends IdsElement {
       break;
     }
     }
+  }
+
+  /**
+   * @param {string|number} value 0-based index for page that is currently viewed
+   */
+  set pageIndex(value) {
+    if (Number.isNaN(Number.parseInt(value))) {
+      this.setAttribute(props.PAGE_INDEX, 1);
+      return;
+    }
+
+    this.setAttribute(props.PAGE_INDEX, Number.parseInt(value));
+  }
+
+  /**
+   * @param {string|number} value number of items shown per-page
+   */
+  set pageItemCount(value) {
+    if (Number.isNaN(Number.parseInt(value))) {
+      this.removeAttribute(props.PAGE_ITEM_COUNT);
+      console.error('ids-pager: non-numeric value sent to page-item-count');
+      return;
+    }
+
+    this.setAttribute(props.PAGE_ITEM_COUNT, Number.parseInt(value));
+  }
+
+  set pageSize(value) {
+    if (Number.isNaN(Number.parseInt(value))) {
+      this.setAttribute(props.PAGE_SIZE, 10);
+      console.error('ids-pager: non-numeric value sent to page-size');
+      return;
+    }
+
+    if (Number.isNaN(Number.parseInt(value)) <= 0) {
+      this.setAttribute(props.PAGE_SIZE, 10);
+      console.error('ids-pager: page-size cannot be <= 0');
+      return;
+    }
+
+    this.setAttribute(props.PAGE_SIZE, Number.parseInt(value));
   }
 
   /** observes changes in content/layout */
