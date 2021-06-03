ed273873e8b79b04fa57c9a84cdce3370a6a512f
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-06-03 16:35:14 -0400

    (cont'd)

--- src/ids-spinbox/ids-spinbox.d.ts
+++ src/ids-spinbox/ids-spinbox.d.ts
@@ -1,5 +1,27 @@
 import { IdsElement } from '../ids-base';
 
+type IdsValidationErrorMessageTypes = {
+  /** The unique id in the check messages */
+  id: string;
+
+  /** The Type of message and icon */
+  type: 'error' | 'info' | 'alert' | 'warn';
+
+  /** The localized message text */
+  message: string;
+
+  /** The Type of message icon */
+  icon: string;
+}
+
+type IdsValidationTypes = {
+  /** Add a message to the input */
+  addMessage(settings: IdsValidationErrorMessageTypes);
+
+  /** Remove a message(s) from the input */
+  removeMessage(settings: IdsValidationErrorMessageTypes);
+};
+
 export default class IdsSpinbox extends IdsElement {
   /**
    * the maximum value the spinbox' input can be set to
@@ -28,4 +50,9 @@ export default class IdsSpinbox extends IdsElement {
 
   /** validation message text; set to `required` to require validation. */
   validate?: string;
+
+  /** flags the spinbox value as non-interactive/presentational */
+  readonly?: boolean | string;
+
+  IdsValidationTypes;
 }
