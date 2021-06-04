bc6abc1b0c21216e516ad7e57f964643c1a011d8
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-06-04 14:48:14 -0400

    tests: new helper for simulating mouse down=>up events on elem w wait, export wait & processAnimFrame for re-use

---
+++ test/helpers/simulate-mouse-down-events.js
@@ -0,0 +1,27 @@
+import wait from './wait';
+import processAnimFrame from './process-anim-frame';
+
+const MOUSE_LEFT = 0x001;
+
+/**
+ * simulate a 'mousedown' followed by 'mouseup' with optional time delay
+ * (considers a marginal extra for natural callback/anim frame cycle between
+ * events)
+ *
+ * @param {HTMLElement} element increment or decrement button
+ */
+export default async function simulateMouseDownEvents({
+  element,
+  buttons = MOUSE_LEFT,
+  mouseDownTime = 0
+}) {
+  const downEvent = new MouseEvent('mousedown', { view: window, buttons });
+  const upEvent = new MouseEvent('mouseup', { view: window });
+
+  element.dispatchEvent(downEvent);
+  await processAnimFrame();
+  await wait(mouseDownTime);
+
+  element.dispatchEvent(upEvent);
+  await processAnimFrame();
+}
