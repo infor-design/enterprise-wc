dc3d3df5fa71923508e5469fd547f608b01bb840
Chris Akinrinade <72534276+chrisakinrinade@users.noreply.github.com>
2021-06-07 12:52:48 -0400

    Added missing files to repo

---
+++ app/ids-fieldset/index.js
@@ -0,0 +1,5 @@
+import IdsLayoutGrid from '../../src/ids-layout-grid/ids-layout-grid';
+import IdsText from '../../src/ids-text/ids-text';
+import IdsFieldset from '../../src/ids-fieldset/ids-fieldset';
+import IdsInput from '../../src/ids-input/ids-input';
+import IdsButton from '../../src/ids-button/ids-button';
---
+++ src/ids-fieldset/ids-fieldset.js
@@ -0,0 +1,42 @@
+import {
+  IdsElement,
+  customElement,
+  scss,
+  props,
+  mix,
+  stringUtils
+} from '../ids-base';
+
+// Import Mixins
+import {
+  IdsEventsMixin,
+  IdsThemeMixin
+} from '../ids-mixins';
+
+import styles from './ids-fieldset.scss';
+
+/** 
+ * IDS Fieldset Component
+ * @type {IdsFieldset}
+ * @inherits IdsElement
+ * @mixes IdsThemeMixin
+ * @mixes IdsEventsMixin
+ * @part fieldset - the fieldset element
+ */
+@customElement('ids-fieldset')
+@scss(styles)
+class IdsFieldset extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin) {
+  constructor() {
+    super();
+  }
+
+  /**
+   * Inner template contents
+   * @returns {string} The template
+   */
+  template() {
+    return `<fieldset class="ids-fieldset"><slot></slot></fieldset>`;
+  }
+}
+
+export default IdsFieldset
