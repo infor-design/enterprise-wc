0712c11ccce5461280baf20dbbc566bf1d1c63b3
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-06-15 12:54:40 -0400

    IdsPager: basic "render doesn't fail" tests in various scenarios

--- test/ids-pager/ids-pager-func-test.js
+++ test/ids-pager/ids-pager-func-test.js
@@ -3,17 +3,55 @@
  */
 import IdsPager from '../../src/ids-pager/ids-pager';
 
-const DEFAULT_PAGER_HTML = (
-  `<ids-pager page-size="20" page-number="10" total="200">
-    <ids-pager-section>
-      <ids-pager-button first></ids-pager-button>
-      <ids-pager-button previous></ids-pager-button>
-      <ids-pager-input></ids-pager-input>
-      <ids-pager-button next></ids-pager-button>
-      <ids-pager-button last></ids-pager-button>
-    </ids-pager-section>
+const HTMLSnippets = {
+  NAV_BUTTONS: (
+    `<ids-pager page-size="20" page-number="10" total="200">
+      <ids-pager-section>
+        <ids-pager-button first></ids-pager-button>
+        <ids-pager-button previous></ids-pager-button>
+        <ids-pager-input></ids-pager-input>
+        <ids-pager-button next></ids-pager-button>
+        <ids-pager-button last></ids-pager-button>
+      </ids-pager-section>
+    </ids-pager>`
+  ),
+  NUMBER_LIST_NAV: (
+  `<ids-pager page-size="20" page-number="10" total="150">
+      <ids-pager-section>
+        <ids-pager-button previous></ids-pager-button>
+        <ids-pager-number-list></ids-pager-number-list>
+        <ids-pager-button next></ids-pager-button>
+      </ids-pager-section>
   </ids-pager>`
-);
+  ),
+  RIGHT_ALIGNED_CONTENT: (
+    `<ids-pager page-size="20" page-number="10" total="150">
+      <ids-pager-section>
+        <ids-pager-button previous></ids-pager-button>
+        <ids-pager-number-list></ids-pager-number-list>
+        <ids-pager-button next></ids-pager-button>
+      </ids-pager-section>
+      <ids-pager-section>
+        Right-Aligned Content
+      </ids-pager-section>
+    </ids-pager>`
+  ),
+  DOUBLE_SIDED_CONTENT: (
+    `<ids-pager page-size="20" page-number="10" total="150">
+      <ids-pager-section>
+        Left-Aligned-Content
+      </ids-pager-section>
+      <ids-pager-section>
+        <ids-pager-button previous></ids-pager-button>
+        <ids-pager-number-list></ids-pager-number-list>
+        <ids-pager-button next></ids-pager-button>
+      </ids-pager-section>
+      <ids-pager-section>
+        Right-Aligned-Content
+      </ids-pager-section>
+    </ids-pager>`
+  )
+};
 
 const processAnimFrame = () => new Promise((resolve) => {
   window.requestAnimationFrame(() => {
@@ -41,11 +79,35 @@ describe('IdsPager Component', () => {
     elem?.remove();
   });
 
-  it('renders from HTML Template with no errors', async () => {
-    elem = await createElemViaTemplate(DEFAULT_PAGER_HTML);
+  it('renders from HTML Template with nav buttons with no errors', async () => {
+    elem = await createElemViaTemplate(HTMLSnippets.NAV_BUTTONS);
 
     const errors = jest.spyOn(global.console, 'error');
-    expect(document.querySelectorAll('ids-tabs').length).toEqual(1);
+    expect(document.querySelectorAll('ids-pager').length).toEqual(1);
+    expect(errors).not.toHaveBeenCalled();
+  });
+
+  it('renders from HTML Template with number list navigation with no errors', async () => {
+    elem = await createElemViaTemplate(HTMLSnippets.NAV_BUTTONS);
+
+    const errors = jest.spyOn(global.console, 'error');
+    expect(document.querySelectorAll('ids-pager').length).toEqual(1);
+    expect(errors).not.toHaveBeenCalled();
+  });
+
+  it('renders with content on both sides with no errors', async () => {
+    elem = await createElemViaTemplate(HTMLSnippets.DOUBLE_SIDED_CONTENT);
+
+    const errors = jest.spyOn(global.console, 'error');
+    expect(document.querySelectorAll('ids-pager').length).toEqual(1);
+    expect(errors).not.toHaveBeenCalled();
+  });
+
+  it('renders with content on right side with no errors', async () => {
+    elem = await createElemViaTemplate(HTMLSnippets.DOUBLE_SIDED_CONTENT);
+
+    const errors = jest.spyOn(global.console, 'error');
+    expect(document.querySelectorAll('ids-pager').length).toEqual(1);
     expect(errors).not.toHaveBeenCalled();
   });
 });
