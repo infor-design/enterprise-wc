4b0bf7589bc7d1925f43e69293e036c8727e98c6
dortiz <dortiz@dortizs-MacBook-Pro.local>
2021-06-16 09:55:57 -0400

    #131 - update to e2e fn percy test

---
+++ test/ids-color/ids-color-e2e-test.js
@@ -0,0 +1,17 @@
+describe('Ids Color e2e Tests', () => {
+  const url = 'http://localhost:4444/ids-color';
+
+  beforeAll(async () => {
+    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
+  });
+
+  it('should not have errors', async () => {
+    await expect(page.title()).resolves.toMatch('IDS Color Component');
+  });
+
+  it('should pass Axe accessibility tests', async () => {
+    await page.setBypassCSP(true);
+    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
+    await expect(page).toPassAxeTests();
+  });
+});
