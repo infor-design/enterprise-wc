f56bf128b66dabc50bb81642720c0194e963ee14
Ed Coyle <Ed.Coyle@infor.com>
2021-06-17 11:37:55 -0400

    Try again to fix test timing

--- test/ids-button/ids-button-e2e-test.js
+++ test/ids-button/ids-button-e2e-test.js
@@ -2,7 +2,7 @@ describe('Ids Button e2e Tests', () => {
   const url = 'http://localhost:4444/ids-button';
 
   beforeAll(async () => {
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(2000);
     // await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
   });
 
