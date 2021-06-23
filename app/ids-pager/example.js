76b02842e335156b0f392339b16fc11871be3c3c
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-06-23 18:01:03 -0400

    ids-pager: add button to example for toggling NumberList or Input layout

--- app/ids-pager/example.js
+++ app/ids-pager/example.js
@@ -3,6 +3,21 @@ import IdsLayoutGridCell from '../../src/ids-layout-grid/ids-layout-grid-cell';
 import IdsText from '../../src/ids-text/ids-text';
 import IdsButton from '../../src/ids-button/ids-button';
 
+const TemplateHTML = {
+  INPUT_AND_BUTTONS: (
+    `<ids-pager-button first></ids-pager-button>
+      <ids-pager-button previous></ids-pager-button>
+      <ids-pager-input></ids-pager-input>
+      <ids-pager-button next></ids-pager-button>
+      <ids-pager-button last></ids-pager-button>`
+  ),
+  NUMBER_LIST: (
+    `<ids-pager-button previous></ids-pager-button>
+      <ids-pager-number-list></ids-pager-number-list>
+      <ids-pager-button next></ids-pager-button>`
+  )
+};
+
 // Add an event listener to test clickable links
 const pager = document.querySelector('#ids-pager-example');
 pager?.addEventListener('pagenumberchange', (e) => {
@@ -16,4 +31,22 @@ toggleDisableButton.addEventListener('click', () => {
   toggleDisableButton.querySelector('[slot="text"]').textContent = pager.disabled
     ? 'Enable'
     : 'Disabled';
+});
+
+let mode = 'input-buttons';
+
+const toggleTypeButton = document.querySelector('#ids-pager-type-toggle-button');
+toggleTypeButton.addEventListener('click', () => {
+  mode = (mode === 'input-buttons') ? 'number-list' : 'input-buttons';
+
+  toggleTypeButton.querySelector('[slot="text"]').textContent = (mode === 'input-buttons')
+    ? 'IdsPagerNumberList'
+    : 'IdsPagerInput + IdsPagerButtons';
+
+  pager.innerHTML = '';
+
+  const template = document.createElement('template');
+  template.innerHTML = TemplateHTML[mode === 'input-buttons' ? 'INPUT_AND_BUTTONS' : 'NUMBER_LIST'];
+  const pagerContent = template.content.cloneNode(true);
+  pager.appendChild(pagerContent);
 });
