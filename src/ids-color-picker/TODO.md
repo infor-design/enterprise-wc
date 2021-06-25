59ec6b4921e927254c3bde19855a98ce9717829d
Tim McConechy <tim.mcconechy@infor.com>
2021-06-25 11:16:11 -0400

    More fixes from code review

--- src/ids-color-picker/TODO.md
+++ src/ids-color-picker/TODO.md
@@ -1,1 +1,13 @@
-# Ids Color Picker TODO 
+# Ids Color Picker TODO's
+
+- [ ] Tooltip on the colors
+- [ ] Fix focus state around input and swatch (needs trigger button issue)
+- [ ] Fix the keyboard (arrow down , keys around the colors ect)
+- [ ] Click the swatch to select / option for the "advanced" picker
+- [ ] Fix arrow position
+- [ ] Dirty tracker
+- [ ] Empty State
+- [ ] Clear Swatch
+- [ ] Input should be smaller size
+- [ ] Fix all the states [See example](https://main-enterprise.demo.design.infor.com/components/colorpicker/test-states.html)
+- [ ] Click out close (popup issue)
--- src/ids-color-picker/ids-color-picker.js
+++ src/ids-color-picker/ids-color-picker.js
@@ -32,46 +32,47 @@ class IdsColorPicker extends mix(IdsElement).with(IdsEventsMixin, IdsKeyboardMix
     super();
   }
 
-   idsColorPicker = this.shadowRoot
+  // Reference to the root
+  root = this.shadowRoot
 
-   body = document.getElementsByTagName('body')[0]
+  // Reference to swatch input
+  swatchInput = this.root.querySelector('.color-input')
 
-   colorpickerContainer = this.container;
+  // Reference to the color picker input
+  colorPickerInput = this.root.querySelector(this.label === '' ? '.color-input-value-no-label' : '.color-input-value')
 
-   colorContainer = this.idsColorPicker.querySelector('.color-container')
+  // Reference to the color preview
+  colorPreview = this.root.querySelector('.color-preview')
 
-   triggerButton = this.idsColorPicker.querySelector('#color-picker-button')
+  // Reference to the colors
+  idsColorsArr = document.querySelectorAll('ids-color')
 
-   colorpickerInput = this.idsColorPicker.querySelector('.color-input')
+  connectedCallback() {
+    // TODO: Need to do this and not sure why or the setters/getters do not work
+    // eslint-disable-next-line no-self-assign
+    this.value = this.value;
+    // eslint-disable-next-line no-self-assign
+    this.disabled = this.disabled;
+    // eslint-disable-next-line no-self-assign
+    this.swatch = this.swatch;
+    // eslint-disable-next-line no-self-assign
+    this.label = this.label;
+    this.#handleEvents();
 
-   colorInputValueClass = this.label === '' ? 'color-input-value-no-label' : 'color-input-value';
+    // TODO: Do this a cleaner way to lay the label out
+    this.inputLabel = this.colorPickerInput.shadowRoot.querySelector('label');
+    if (this.inputLabel) {
+      this.inputLabel.style.marginLeft = '-38px';
+    }
+  }
 
-   colorInputValue = this.idsColorPicker.querySelector(this.label === '' ? '.color-input-value-no-label' : '.color-input-value')
+  static get attributes() {
+    return [attributes.DISABLED, attributes.LABEL, attributes.MODE, attributes.NAME, 'swatch', attributes.VALUE, attributes.VERSION];
+  }
 
-   colorPreview = this.idsColorPicker.querySelector('.color-preview')
-
-   idsColorsArr = document.querySelectorAll('ids-color')
-
-   connectedCallback() {
-     // TODO: Need to do this and not sure why or the setters/getters do not work
-     // eslint-disable-next-line no-self-assign
-     this.value = this.value;
-     // eslint-disable-next-line no-self-assign
-     this.disabled = this.disabled;
-     // eslint-disable-next-line no-self-assign
-     this.swatch = this.swatch;
-     // eslint-disable-next-line no-self-assign
-     this.label = this.label;
-     this.#handleEvents();
-   }
-
-   static get attributes() {
-     return [attributes.DISABLED, attributes.LABEL, attributes.MODE, attributes.NAME, 'swatch', attributes.VALUE, attributes.VERSION];
-   }
-
-   template() {
-     const id = this.id || 'ids-color';
-     const template = `
+  template() {
+    const id = this.id || 'ids-color';
+    const template = `
       <div class="ids-color-picker">
         <ids-trigger-field tabbable="false">
           <span class="color-preview" tabindex="-1">
@@ -86,88 +87,86 @@ class IdsColorPicker extends mix(IdsElement).with(IdsEventsMixin, IdsKeyboardMix
           <slot slot="content" class="color-popup"></slot>
         </ids-popup>
       </div>`;
-     return template;
-   }
+    return template;
+  }
 
-   set value(v) {
-     this.#updateColorPickerValues(v);
-     this.setAttribute('value', v.toString().toLowerCase());
-   }
+  set value(v) {
+    this.#updateColorPickerValues(v);
+    this.setAttribute('value', v.toString().toLowerCase());
+  }
 
-   get value() {
-     return this.getAttribute('value') || '#B94E4E';
-   }
+  get value() {
+    return this.getAttribute('value') || '#B94E4E';
+  }
 
-   set disabled(d) {
-     this.setAttribute('disabled', d.toString());
-   }
+  set disabled(d) {
+    this.setAttribute('disabled', d.toString());
+  }
 
-   get disabled() {
-     return this.getAttribute('disabled') || 'false';
-   }
+  get disabled() {
+    return this.getAttribute('disabled') || 'false';
+  }
 
-   set swatch(s) {
-     this.setAttribute('swatch', s.toString());
-   }
+  set swatch(s) {
+    this.setAttribute('swatch', s.toString());
+  }
 
-   get swatch() {
-     return this.getAttribute('swatch') || 'true';
-   }
+  get swatch() {
+    return this.getAttribute('swatch') || 'true';
+  }
 
-   set label(l) {
-     this.setAttribute('label', l.toString());
-   }
+  set label(value) {
+    this.setAttribute('label', value.toString());
+  }
 
-   get label() {
-     return this.getAttribute('label') || '';
-   }
+  get label() {
+    return this.getAttribute('label') || '';
+  }
 
    #handleEvents() {
-     this.idsColorsArr.forEach((element) => {
-       element.style.backgroundColor = element.getAttribute('hex');
-     });
+    this.idsColorsArr.forEach((element) => {
+      element.style.backgroundColor = element.getAttribute('hex');
+    });
 
-     if (this.disabled === 'false') {
-       this.onEvent('click', this.container, (event) => {
-         const target = event.target;
-         const openColorCondition = (target.classList.contains('colorpicker-icon') || target.classList.contains('ids-dropdown'));
-         
-         if (openColorCondition) {
-           this.#openCloseColorpicker();
-         }
+    if (this.disabled === 'false') {
+      this.onEvent('click', this.container, (event) => {
+        const target = event.target;
+        const openColorCondition = (target.classList.contains('colorpicker-icon') || target.classList.contains('ids-dropdown'));
 
-         if (target.hasAttribute('hex')) {
-           this.#updateColorCheck(target);
-           this.setAttribute('value', target.getAttribute('hex'));
-           this.#openCloseColorpicker();
-         }
-       });
+        if (openColorCondition) {
+          this.#openCloseColorpicker();
+        }
 
-       this.onEvent('keyup', this.container, (keyup) => {
-         if (keyup.key === 'Enter') {
-           if (keyup.target.id === `${this.id}-button`) {
-             this.#openCloseColorpicker();
-           }
-           if (keyup.target.hasAttribute('hex')) {
-             this.setAttribute('value', keyup.target.getAttribute('hex'));
-             this.#openCloseColorpicker();
-             this.#updateColorCheck(keyup.target);
-           }
-         }
-       });
-     }
+        if (target.hasAttribute('hex')) {
+          this.#updateColorCheck(target);
+          this.setAttribute('value', target.getAttribute('hex'));
+          this.#openCloseColorpicker();
+        }
+      });
 
-     this.onEvent('change', this.colorpickerInput, () => this.setAttribute('value', this.colorpickerInput.value.toLowerCase()));
+      this.onEvent('keyup', this.container, (keyup) => {
+        if (keyup.key === 'Enter') {
+          if (keyup.target.id === `${this.id}-button`) {
+            this.#openCloseColorpicker();
+          }
+          if (keyup.target.hasAttribute('hex')) {
+            this.setAttribute('value', keyup.target.getAttribute('hex'));
+            this.#openCloseColorpicker();
+            this.#updateColorCheck(keyup.target);
+          }
+        }
+      });
+    }
 
-     this.onEvent('change', this.colorInputValue, () => this.setAttribute('value', this.colorInputValue.value.toLowerCase()));
-
-     this.onEvent('click', this.colorPreview, () => this.idsColorsArr.forEach((element) => element.removeAttribute('checked')));
-   }
+    this.onEvent('change', this.swatchInput, () => this.setAttribute('value', this.swatchInput.value.toLowerCase()));
+    this.onEvent('change', this.colorPickerInput, () => this.setAttribute('value', this.colorPickerInput.value.toLowerCase()));
+    this.onEvent('click', this.colorPreview, () => this.idsColorsArr.forEach((element) => element.removeAttribute('checked')));
+  }
 
    #updateColorPickerValues(colorValue) {
-     this.colorpickerInput.value = colorValue;
+     this.swatchInput.value = colorValue;
      this.colorPreview.style.backgroundColor = colorValue;
-     this.colorInputValue.value = colorValue;
+     this.colorPickerInput.value = colorValue;
    }
 
    #openCloseColorpicker() {
@@ -180,11 +179,11 @@ class IdsColorPicker extends mix(IdsElement).with(IdsEventsMixin, IdsKeyboardMix
    }
 
    #updateColorCheck(target) {
-    const checkedColor = target.parentElement.querySelector('[checked="true"]');
-    if (checkedColor) {
-      checkedColor.removeAttribute('checked');
-    }
-    target.setAttribute('checked', 'true');
+     const checkedColor = target.parentElement.querySelector('[checked="true"]');
+     if (checkedColor) {
+       checkedColor.removeAttribute('checked');
+     }
+     target.setAttribute('checked', 'true');
    }
 }
 
