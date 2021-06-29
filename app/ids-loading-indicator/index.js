fe0ea6f32ee2c2fb8495dd018742e3c40a0803df
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-06-29 16:41:27 -0400

    ids-loading-indicator: example layout

--- app/ids-loading-indicator/index.js
+++ app/ids-loading-indicator/index.js
@@ -1,1 +1,4 @@
+import { IdsBlockGrid } from '../../src/ids-block-grid/ids-block-grid';
+import { IdsBlockGridItem } from '../../src/ids-block-grid/ids-block-grid-item';
 import IdsLoadingIndicator from '../../src/ids-loading-indicator/ids-loading-indicator';
+import './index.scss';
