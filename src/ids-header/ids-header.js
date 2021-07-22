e2c08d2ea4979ccfbcc8056ef788e6976585095b
Daniel Ortiz <danielortiz1982@users.noreply.github.com>
2021-07-22 12:04:48 -0400

    Update src/ids-header/ids-header.js
    Co-authored-by: Tim McConechy <tim.mcconechy@infor.com>

--- src/ids-header/ids-header.js
+++ src/ids-header/ids-header.js
@@ -13,7 +13,7 @@ import {
 } from '../ids-mixins';
 
 import styles from './ids-header.scss';
-import '../ids-input/ids-input';
+import IdsInput from '../ids-input/ids-input';
 import '../ids-toolbar/ids-toolbar';
 import '../ids-button/ids-button';
 import '../ids-text/ids-text';
