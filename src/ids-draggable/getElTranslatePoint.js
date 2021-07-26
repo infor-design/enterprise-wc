d1912d7240318c50c8fb8c62d3e7051a0c57f6a6
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-07-26 17:15:45 -0400

    ci-env: Github Actions linter on repo does not like certain JSDoc annotations

--- src/ids-draggable/getElTranslatePoint.js
+++ src/ids-draggable/getElTranslatePoint.js
@@ -1,9 +1,17 @@
 /**
+ * @typedef TranslationPoint
+ * @type {object}
+ * @property {number} x? x-offset
+ * @property {number} y? y-offset
+ * @property {number} z? z-offset
+ */
+
+/**
  * Gets computed translate values from style prop; adapted from:
  * https://zellwk.com/blog/css-translate-values-in-javascript/
  *
  * @param {HTMLElement} element element to grab translation point from it's styles with
- * @returns {{ x?: number, y?: number, z?:number }} translation point if available
+ * @returns {TranslationPoint} translation point if available
  */
 export default function getElTranslatePoint(element) /* istanbul ignore next */ {
   const style = window.getComputedStyle(element);
--- test/helpers/test-elem-builder-factory.js
+++ test/helpers/test-elem-builder-factory.js
@@ -1,15 +1,20 @@
 import processAnimFrame from './process-anim-frame';
 
 /**
+ * @typedef ElemBuilder
+ * @type {object}
+ * @property {function(string):HTMLElement} createElemFromTemplate creates an element
+ * with a given string
+ * @property {function():void} clearElement clears the element built
+ * @property {HTMLElement} element an instance of the element currently held
+ */
+
+/**
  * factorizes element refresh/creation boilerplate for usage
  * in Jest tests; helps to add coverage in template on
  * initial construction
  *
- * @returns {{
- *  createElemFromTemplate: function(string):HTMLElement,
- *  clearElement: function():void,
- *  element?: HTMLElement
- * }} factory instance that manages a shared HTML element
+ * @returns {ElemBuilder} factory instance that manages a shared HTML element
  */
 export default function templateElemBuilderFactory() {
   let elem;
