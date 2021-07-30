ca839a38f6f7ed018b1e17c3e96f2b8cbd426cd3
Vivian Leung <vivz753@gmail.com>
2021-07-30 16:11:38 -0700

    Update ids-progress-chart.d.ts

--- src/ids-progress-chart/ids-progress-chart.d.ts
+++ src/ids-progress-chart/ids-progress-chart.d.ts
@@ -7,7 +7,7 @@ export default class IdsNotificationBanner extends IdsElement {
   /** Sets the main label of the chart */
   label: string;
 
-  /** Sets the labelValue of the chart */
+  /** Sets the labelProgress of the chart */
   labelProgress: string;
 
   /** Sets the labelTotal of the chart */
