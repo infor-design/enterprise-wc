99c89a03598023407a2483b6d53bdae604778925
Tim McConechy <tim.mcconechy@infor.com>
2021-08-03 16:11:36 -0400

    Add scroll container e2e test

--- test/ids-scroll-container/ids-scroll-container-e2e-test.js
+++ test/ids-scroll-container/ids-scroll-container-e2e-test.js
@@ -1,5 +1,5 @@
 describe('Ids Card e2e Tests', () => {
-  const url = 'http://localhost:4444/ids-card';
+  const url = 'http://localhost:4444/ids-scroll-container';
 
   beforeAll(async () => {
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
