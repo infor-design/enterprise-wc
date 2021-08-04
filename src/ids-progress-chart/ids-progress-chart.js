fa93a1fb7b831e5f82ac6f62ef56b0cf6546b004
Vivian Leung <vivian.leung@infor.com>
2021-08-04 16:52:29 -0400

    Revert "add john's request to conditionally render the progress/total labels"
    This reverts commit 02d71622274113574f05d65b6223c02bcfd29015.

--- src/ids-progress-chart/ids-progress-chart.js
+++ src/ids-progress-chart/ids-progress-chart.js
@@ -63,12 +63,12 @@ class IdsProgressChart extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixi
     return `
     <div class="ids-progress-chart" part="chart">
       <div class="labels">
-      ${this.label ? `<ids-text class="label-main">${this.label}</ids-text>` : ''}
+        <ids-text class="label-main">${this.label ? this.label : ''}</ids-text>
         <slot name="icon"></slot>
-        ${this.progressLabel ? `<ids-text class="label-progress">${this.progressLabel}</ids-text>` : ''}
-        ${this.totalLabel ? `<div class="label-end">
-          <ids-text class="label-total">${this.totalLabel}</ids-text>
-          </div>` : ''}
+        <ids-text class="label-progress">${this.progressLabel ? this.progressLabel : ''} </ids-text>
+        <div class="label-end">
+          <ids-text class="label-total">${this.totalLabel ? this.totalLabel : '' }</ids-text>
+        </div>
       </div>
       <div class="bar">
         <div class="bar-total">
