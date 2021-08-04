2578ef350f0598e85b93cfc116149f07d00399ca
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-08-04 14:13:08 -0400

    fixes on IdsAttributProviderMixin after changes

--- src/core/ids-mixin-builder.js
+++ src/core/ids-mixin-builder.js
@@ -3,16 +3,9 @@
  * @returns {any} a new base class with mixin attribs/methods included
  */
 export default function mix(superclass) {
-  const baseClass = class extends superclass {
-    constructor() {
-      super();
-      this.superclass = superclass;
-    }
-  };
-
   return {
     with(...mixins) {
-      return mixins.reduce((c, mixin) => mixin(c), baseClass);
+      return mixins.reduce((c, mixin) => mixin(c), superclass);
     }
   };
 }
