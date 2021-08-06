931ea619375046f9b232fc8eaa4129dbe6bb26c7
Vivian Leung <vivian.leung@infor.com>
2021-08-06 14:37:14 -0400

    working ids-summary-form

--- src/ids-summary-form/ids-summary-form.js
+++ src/ids-summary-form/ids-summary-form.js
@@ -25,10 +25,12 @@ import styles from './ids-summary-form.scss';
 @scss(styles)
 class IdsSummaryForm extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin) {
   constructor() {
+    console.log('constructor()');
     super();
   }
 
   connectedCallback() {
+    console.log('connectedCallback()');
     super.connectedCallback();
   }
 
@@ -37,8 +39,11 @@ class IdsSummaryForm extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin)
    * @returns {Array} The attributes in an array
    */
   static get attributes() {
+    console.log('getting attributes()');
     return [
-      // TODO: insert attributes here
+      attributes.DATA,
+      attributes.FONT_WEIGHT,
+      attributes.LABEL,
     ];
   }
 
@@ -49,14 +54,53 @@ class IdsSummaryForm extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin)
   template() {
     return `
       <div class="ids-summary-form">
-        <slot name="label" class="labelSlot"></slot>
-        <slot name="data" class="dataSlot"></slot>
+        <ids-text class="label">${this.label ?? ''}</ids-text>
+        <ids-text class="data" font-weight="${this.fontWeight ?? 'bold'}">${this.data ?? ''}</ids-text>
       </div>`;
   }
 
-  // set get color
+  set data(value) {
+    console.log('setting data to: ' + value);
+    this.setAttribute('data', value || '');
+    this.updateUI();
+  }
+  
+  get data() {
+    console.log('getting data: ' + this.getAttribute('data'));
+    return this.getAttribute('data');
+  }
+  
+  set label(value) {
+    console.log('setting label to: ' + value);
+    // const prop = value || 'label';
+    // console.log('label prop is ' + prop);
+    // this.value = prop;
+    this.setAttribute('label', value || '');
+    this.updateUI();
+  }
 
-  // set get font size
+  get label() {
+    console.log('getting label: ' + this.getAttribute('label'));
+    return this.getAttribute('label');
+  }
+
+  set fontWeight(value) {
+    console.log('font-weight is being set to ' + value);
+    this.setAttribute(attributes.FONT_WEIGHT, value);
+    // this.updateUI();
+  }
+
+  get fontWeight() {
+    console.log('getting font-weight: ' + this.getAttribute('font-weight'));
+    return this.getAttribute('font-weight');
+  }
+
+  updateUI() {
+    console.log('UI is being updated');
+
+    this.container.querySelector('.label').innerHTML = this.label;
+    this.container.querySelector('.data').innerHTML = this.data;
+  }
 }
 
 export default IdsSummaryForm;
