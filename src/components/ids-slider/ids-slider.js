2ee0bd336e83b41e5708a091a922a1844b00dee6
Vivian Leung <vivian.leung@infor.com>
2021-08-12 19:52:55 -0700

    slider w/ working progress color and moving tooltip and static min label

--- src/components/ids-slider/ids-slider.js
+++ src/components/ids-slider/ids-slider.js
@@ -14,6 +14,10 @@ import {
 
 import styles from './ids-slider.scss';
 
+const DEFAULT_VALUE = 50;
+const DEFAULT_MIN = 0;
+const DEFAULT_MAX = 100;
+
 /**
  * IDS Slider Component
  * @type {IdsSlider}
@@ -28,7 +32,9 @@ class IdsSlider extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin) {
     super();
   }
 
+  
   connectedCallback() {
+    this.container.querySelector('.slider').style.setProperty("--value", DEFAULT_VALUE);
     this.#handleEvents();
     super.connectedCallback();
   }
@@ -54,25 +60,46 @@ class IdsSlider extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin) {
    * Create the Template for the contents
    * @returns {string} The template
    */
+
+
+
   template() {
     return `
     <div class="ids-slider">
-      <div class="slidecontainer" style='--value:30;'>
-        <input type="range" min="1" max="100" value="30" class="slider" id="myRange"
-        onchange="this.parentNode.style.setProperty('--value', this.value);"
-        >
+      <div class="slidecontainer">
+        <div class="tooltip">${this.value ?? DEFAULT_VALUE}</div>
+        <input type="range" min="${this.min ?? DEFAULT_MIN}" max="${this.max ?? DEFAULT_MAX}" value="${this.value ?? DEFAULT_VALUE}" class="slider">
         <span class="range"></span>
         <span class="tick end"></span>
         <span class="tick start"></span>
-        <div class="label"></div>
+        <ids-text label class="label">${this.min ?? DEFAULT_MIN}</ids-text>
       </div>
     </div>`;
   }
 
-  updateUI(value) {
-    this.container.querySelector('.slidecontainer').styles.setProperty('--value', value);
-    this.container.querySelector('.label').innerHTML = value;
+  set value(value) {
+    this.setAttribute(attributes.VALUE, value || DEFAULT_VALUE);
+    this.container.querySelector('.slider').setAttribute('value', this.value);
+    this.container.querySelector('.slider').style.setProperty("--value", this.value);
+    this.container.querySelector('.tooltip').style.setProperty("--value", this.value);
+    this.container.querySelector('.tooltip').innerHTML = this.value;
   }
+
+  get value() { return this.getAttribute(attributes.VALUE); }
+
+  set min(value) {
+    this.setAttribute(attributes.MIN, value || DEFAULT_MIN);
+    this.container.querySelector('.label').innerHTML = this.min;
+  }
+
+  get min() { return this.getAttribute(attributes.MIN); }
+
+  set max(value) {
+    this.setAttribute(attributes.MAX, value || DEFAULT_MAX);
+    // this.container.querySelector('.label').innerHTML = this.max;
+  }
+
+  get min() { return this.getAttribute(attributes.MIN); }
 
   /**
    * Set the color of the bar
@@ -86,6 +113,15 @@ class IdsSlider extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin) {
   get color() { return this.getAttribute(attributes.COLOR); }
 
   #handleEvents() {
+
+    this.onEvent('input', this.container.querySelector('.slider'), () => {
+      this.setAttribute(attributes.VALUE, this.container.querySelector('.slider').value);
+    })
+
+    this.onEvent('click', this.container.querySelector('.slider'), () => {
+      console.log('slider clicked')
+      // this.container.querySelector('.slider').style.setProperty("border", "10px solid red")
+    })
     return this;
   }
 }
