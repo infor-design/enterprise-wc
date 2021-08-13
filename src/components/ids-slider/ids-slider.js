f4fbb23479abb3aa212d85de6c29095bfc9d3634
Vivian Leung <vivian.leung@infor.com>
2021-08-13 12:26:06 -0700

    working tool tip & proper movement, added focus shadow, event listeners for clicking on/outside of component

--- src/components/ids-slider/ids-slider.js
+++ src/components/ids-slider/ids-slider.js
@@ -67,7 +67,10 @@ class IdsSlider extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin) {
     return `
     <div class="ids-slider">
       <div class="slidecontainer">
-        <div class="tooltip">${this.value ?? DEFAULT_VALUE}</div>
+        <div class="tooltip">
+          <ids-text>${this.value ?? DEFAULT_VALUE}</ids-text>
+          <div class="pin"></div>
+        </div>
         <input type="range" min="${this.min ?? DEFAULT_MIN}" max="${this.max ?? DEFAULT_MAX}" value="${this.value ?? DEFAULT_VALUE}" class="slider">
         <span class="range"></span>
         <span class="tick end"></span>
@@ -79,27 +82,33 @@ class IdsSlider extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin) {
 
   set value(value) {
     this.setAttribute(attributes.VALUE, value || DEFAULT_VALUE);
+
+    const percent = (this.value - this.min) * 100 / (this.max - this.min);
+    const pos = -10 - (percent * 0.2);
+
     this.container.querySelector('.slider').setAttribute('value', this.value);
-    this.container.querySelector('.slider').style.setProperty("--value", this.value);
-    this.container.querySelector('.tooltip').style.setProperty("--value", this.value);
+    this.container.querySelector('.slider').style.setProperty("--percent", percent);
+    this.container.querySelector('.tooltip').style.setProperty("--percent", percent);
+    this.container.querySelector('.tooltip').style.setProperty("--pos", pos);
+
     this.container.querySelector('.tooltip').innerHTML = this.value;
   }
 
-  get value() { return this.getAttribute(attributes.VALUE); }
+  get value() { return this.getAttribute(attributes.VALUE) || DEFAULT_VALUE; }
 
   set min(value) {
     this.setAttribute(attributes.MIN, value || DEFAULT_MIN);
     this.container.querySelector('.label').innerHTML = this.min;
   }
 
-  get min() { return this.getAttribute(attributes.MIN); }
+  get min() { return this.getAttribute(attributes.MIN) || DEFAULT_MIN; }
 
   set max(value) {
     this.setAttribute(attributes.MAX, value || DEFAULT_MAX);
     // this.container.querySelector('.label').innerHTML = this.max;
   }
 
-  get min() { return this.getAttribute(attributes.MIN); }
+  get max() { return this.getAttribute(attributes.MIN) || DEFAULT_MAX; }
 
   /**
    * Set the color of the bar
@@ -112,6 +121,18 @@ class IdsSlider extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin) {
 
   get color() { return this.getAttribute(attributes.COLOR); }
 
+  set hideTooltip(value) {
+    // console.log("active Element is: ");
+    // console.log(document.activeElement);
+
+    // if false, remove the hidden attribute
+    if (!value) {
+      this.container.querySelector('.tooltip').removeAttribute('hidden');
+    } else {
+      this.container.querySelector('.tooltip').setAttribute('hidden', '');
+    }
+  }
+
   #handleEvents() {
 
     this.onEvent('input', this.container.querySelector('.slider'), () => {
@@ -120,7 +141,22 @@ class IdsSlider extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin) {
 
     this.onEvent('click', this.container.querySelector('.slider'), () => {
       console.log('slider clicked')
-      // this.container.querySelector('.slider').style.setProperty("border", "10px solid red")
+      this.hideTooltip = false;
+    })
+
+    window.addEventListener('click', () => {
+      const idsSliderSelected = document.activeElement.name === 'ids-slider';
+      this.hideTooltip = !idsSliderSelected;
+      if (idsSliderSelected) {
+        console.log('ids-slider selected')
+        this.container.querySelector('.slider:hover').style.removeProperty('box-shadow')
+        this.container.querySelector('.slider').style.setProperty('--hover-shadow', 'rgb(0 114 237 / 20%) 0px 0px 0px 8px')
+        this.container.querySelector('.slider').style.setProperty('--focus-shadow', 'rgb(0 114 237 / 20%) 0px 0px 0px 8px')
+      } else {
+        console.log('ids-slider NOT selected')
+        this.container.querySelector('.slider').style.setProperty('--focus-shadow', '');
+        this.container.querySelector('.slider').style.setProperty('--hover-shadow', '0 2px 5px rgb(0 0 0 / 20%)');
+      }
     })
     return this;
   }
