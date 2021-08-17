d87e89651e39a3cf8c3c12ae91bf62700c460972
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-08-17 10:43:18 -0400

    ids-attribute-provider: simplify interface/+data-structs to support lookups/callbacks to auto-fire on children

--- src/mixins/ids-attribute-provider-mixin/ids-attribute-provider-mixin.js
+++ src/mixins/ids-attribute-provider-mixin/ids-attribute-provider-mixin.js
@@ -16,97 +16,137 @@ const traversibleHTMLTags = new Set(['DIV', 'SPAN']);
  * children to avoid boilerplate/errors manually managing
  * this in child components
  *
- * @param {*} superclass class being mixed
+ * @param {Function} attributeProviderDefs definitions for attribute provider
  * @returns {any} the extended object
  */
-export default (superclass) => class extends superclass {
-  constructor() {
-    super();
-  }
+export default (attributeProviderDefs) => (superclass) => {
+  const {
+    attributesProvided = [],
+    attributesListenedFor = []
+  } = attributeProviderDefs;
 
-  /**
-   * Update registered props on registered child component types
-   * @param {object} attributes lookup object containing attributes
-   * and their relation when provided to children
-   * @param {boolean} recursive optional; specifies whether to recursively
-   * check simple search of IdsElement on container/perimeter level
-   * @param {HTMLElement} scannedEl Base case: element provider.
-   * Recursive case: scanned IdsElement in tree
-   */
-  provideAttributes(attributes = this.providedAttributes, recursive = true, scannedEl = this) {
-    const domNodes = [
-      ...(scannedEl.children || []),
-      ...(scannedEl.shadowRoot?.children || [])
-    ];
-    for (const el of domNodes) {
-      if (recursive && ((el instanceof IdsElement) || traversibleHTMLTags.has(el?.tagName))) {
-        this.provideAttributes(attributes, true, el);
-      }
+  const attributeList = attributesProvided.map((d) => d.attribute);
 
-      for (const [sourceAttribute, componentEntries] of Object.entries(attributes)) {
-        for (const entry of componentEntries) {
-          /** @type {IdsElement} */
-          let component;
+  return class extends superclass {
+    constructor() {
+      super();
+    }
 
-          /** @type {string} */
-          let targetAttribute;
+    /**
+     * Lookups for attributes to map to parent components
+     * @type {Map<string, object>}
+     */
+    #attributesProvidedMap = new Map(attributesProvided.map((d) => (
+      [d.attribute, d]
+    )));
 
-          /** @type {Function} */
-          let valueXformer;
+    /**
+     * Lookups for targetAttributes to map back to child components
+     * on-change
+     * @type {Map<string, object>}
+     */
+    #attributesListenedForMap = new Map(attributesListenedFor.map((d) => (
+      [d.targetAttribute, d]
+    )));
 
-          if (entry.prototype instanceof IdsElement) {
-            component = entry;
-            valueXformer = identityFn;
-            targetAttribute = sourceAttribute;
-          } else if (typeof entry === 'object') {
-            component = entry.component;
-            targetAttribute = entry.targetAttribute || sourceAttribute;
-            valueXformer = entry.valueXformer || identityFn;
-          }
+    /**
+     * Lookups for components to map to definitions.
+     * @type {Map<IdsElement, object>}
+     */
+    #componentsListenedForMap = (() => {
+      attributesListenedFor.forEach((d) => {
+        const map = new Map();
+        const components = d.component?.[0] ? d.component : [d.component];
+        components.forEach((c) => map.set(c, d));
 
-          if (!component || !(el instanceof component)) { continue; }
+        return map;
+      });
+    })();
 
-          const targetValue = valueXformer(this.getAttribute(sourceAttribute), el);
+    /**
+     * Update registered props on registered child component types
+     *
+     * @param {object} attributes lookup object containing attributes
+     * and their relation when provided to children
+     * @param {boolean} recursive optional; specifies whether to recursively
+     * check simple search of IdsElement on container/perimeter level
+     * @param {HTMLElement} scannedEl Base case: element provider.
+     * Recursive case: scanned IdsElement in tree
+     */
+    provideAttributes(attributes = attributeList, recursive = true, scannedEl = this) {
+      const domNodes = [
+        ...(scannedEl.children || []),
+        ...(scannedEl.shadowRoot?.children || [])
+      ];
 
-          /* istanbul ignore else */
-          if (this.hasAttribute(sourceAttribute) && (targetValue !== null)) {
-            if (el.getAttribute(targetAttribute) !== targetValue) {
-              el.setAttribute(targetAttribute, targetValue);
+      for (const element of domNodes) {
+        for (const sourceAttribute of attributes) {
+          const {
+            /** @type {IdsElement} */
+            component: componentDef,
+            /** @type {string} */
+            targetAttribute = sourceAttribute,
+            /** @type {Function} */
+            valueXformer = identityFn
+          } = this.#attributesProvidedMap.get(sourceAttribute);
+
+          /** @type {IdsElement|Array<IdsElement>} */
+          const components = componentDef?.[0] ? componentDef : [componentDef];
+
+          for (const component of components) {
+            if (!component || !(element instanceof component)) { continue; }
+
+            const targetValue = valueXformer({
+              value: this.getAttribute(sourceAttribute),
+              element
+            });
+
+            /* istanbul ignore else */
+            if (this.hasAttribute(sourceAttribute) && (targetValue !== null)) {
+              if (element.getAttribute(targetAttribute) !== targetValue) {
+                element.setAttribute(targetAttribute, targetValue);
+              }
+            } else if (element.hasAttribute(targetAttribute) && (targetValue === null)) {
+              element.removeAttribute(targetAttribute);
             }
-          } else if (el.hasAttribute(targetAttribute) && (targetValue === null)) {
-            el.removeAttribute(targetAttribute);
+          }
+        }
+
+        if (recursive && (
+          (element instanceof IdsElement)
+          || traversibleHTMLTags.has(element?.tagName))
+        ) {
+          this.provideAttributes(attributes, true, element);
+        }
+      }
+    }
+
+    /**
+     * observes when any of instance's props change in order
+     * to mirror them to child
+     */
+    attributeObserver = new MutationObserver((mutations) => {
+      for (const m of mutations) {
+        /* istanbul ignore else */
+        if (m.type === 'attributes') {
+          const value = this.getAttribute(m.attributeName);
+          if (this.#attributesProvidedMap.has(m.attributeName) && (value !== m.oldValue)) {
+            this.provideAttributes([m.attributeName]);
           }
         }
       }
-    }
-  }
-
-  /**
-   * observes when any of instance's props change in order
-   * to mirror them to child
-   *
-   */
-  attributeObserver = new MutationObserver((mutations) => {
-    for (const m of mutations) {
-      /* istanbul ignore else */
-      if (m.type === 'attributes') {
-        const value = this.getAttribute(m.attributeName);
-        if ((typeof this.providedAttributes?.[m.attributeName] !== 'undefined') && (value !== m.oldValue)) {
-          this.provideAttributes({ [m.attributeName]: this.providedAttributes[m.attributeName] });
-        }
-      }
-    }
-  });
-
-  connectedCallback() {
-    this.attributeObserver.observe(this, {
-      attributes: true,
-      attributeOldValue: true,
-      attributeFilter: Object.keys(this.providedAttributes),
-      subtree: false
     });
 
-    super.connectedCallback?.();
-    this.provideAttributes();
-  }
+    connectedCallback() {
+      this.attributeObserver.observe(this, {
+        attributes: true,
+        attributeOldValue: true,
+        attributeFilter: attributeList,
+        subtree: false
+      });
+
+      super.connectedCallback?.();
+      this.provideAttributes();
+    }
+  };
 };
