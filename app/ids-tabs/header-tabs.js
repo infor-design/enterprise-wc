52d09bdd3396030a3eb412958403ccded68655ce
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-08-20 19:27:17 -0400

    use console info on tabs/header-tabs examples to avoid need of eslint rules

--- app/ids-tabs/header-tabs.js
+++ app/ids-tabs/header-tabs.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const tabs = document.getElementById('ids-tabs-basic');
 
   tabs.addEventListener('change', (e) => {
-    /* eslint-disable-next-line */
-    console.log(`#${e.target.getAttribute('id')}.on('change') =>`, e.target.value);
+    console.info(`#${e.target.getAttribute('id')}.on('change') =>`, e.target.value);
   });
 });
