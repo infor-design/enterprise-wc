251a8df39aae87465566628cd91a3d92b13122ed
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-08-20 11:33:19 -0400

    PR feedback: use console.info vs console.log to avoid eslint log rule

--- demos/ids-draggable/example.js
+++ demos/ids-draggable/example.js
@@ -7,17 +7,14 @@ const draggables = [...document.querySelectorAll('ids-draggable')];
 
 draggables.forEach((d) => {
   d.addEventListener('ids-dragstart', (e) => {
-    /* eslint-disable-next-line no-console */
-    console.log('ids-dragstart', d, e.detail);
+    console.info('ids-dragstart', d, e.detail);
   });
 
   d.addEventListener('ids-drag', (e) => {
-    /* eslint-disable-next-line no-console */
-    console.log('ids-drag', d, e.detail);
+    console.info('ids-drag', d, e.detail);
   });
 
   d.addEventListener('ids-dragend', (e) => {
-    /* eslint-disable-next-line no-console */
-    console.log('ids-dragend', d, e.detail);
+    console.info('ids-dragend', d, e.detail);
   });
 });
