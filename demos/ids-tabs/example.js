2f2311623893b4b7cf2e842b28c8c866bedcc58c
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-08-30 17:47:33 -0400

    ids-tabs: index.html had two headers included after removing for no-padding on header example

--- demos/ids-tabs/example.js
+++ demos/ids-tabs/example.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelector('ids-tabs');
 
   tabs.addEventListener('change', (e) => {
-    console.info(`#${e.target.getAttribute('id')}.on('change') =>`, e.target.value);
+    console.info(`ids-tabs.on('change') =>`, e.target.value);
   });
 });
