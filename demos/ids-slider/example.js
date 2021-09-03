8365035282169c40b49883db3b9a1dbcfe90083d
Vivian Leung <vivian.leung@infor.com>
2021-09-03 14:59:03 -0700

    add yaml files, attempt to set labels on example.js

---
+++ demos/ids-slider/example.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('dom loaded')
+  document.querySelector('.survey').labels = ['very bad', 'poor', 'average', 'good', 'excellent'];
+});
