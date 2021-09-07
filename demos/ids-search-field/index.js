6ca9ef8ccab59f2ddac6eee5fff94dc8155dcd82
Vivian Leung <vivian.leung@infor.com>
2021-09-07 07:24:10 -0700

    demo shows up now, init attributes, and attachEventListeners()

--- demos/ids-search-field/index.js
+++ demos/ids-search-field/index.js
@@ -1,1 +1,5 @@
 import IdsSearchField from '../../src/components/ids-search-field';
+import IdsTriggerField from '../../src/components/ids-trigger-field';
+import IdsTriggerButton from '../../src/components/ids-trigger-field/ids-trigger-button';
+import IdsInput from '../../src/components/ids-input';
+import IdsIcon from '../../src/components/ids-icon';
