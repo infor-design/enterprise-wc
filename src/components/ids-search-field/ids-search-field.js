3c0230db35124ba358207f5f741a526a6278b30b
Vivian Leung <vivian.leung@infor.com>
2021-09-07 07:24:10 -0700

    demo shows up now, init attributes, and attachEventListeners()

--- src/components/ids-search-field/ids-search-field.js
+++ src/components/ids-search-field/ids-search-field.js
@@ -8,10 +8,24 @@ import {
 
 import {
   IdsEventsMixin,
-  IdsThemeMixin
+  IdsThemeMixin,
+  IdsKeyboardMixin,
+  IdsClearableMixin,
 } from '../../mixins';
 
 import styles from './ids-search-field.scss';
+
+import IdsTriggerField from '../ids-trigger-field';
+import IdsTriggerButton from '../ids-trigger-field/ids-trigger-button';
+import IdsInput from '../ids-input';
+import IdsIcon from '../ids-icon';
+
+const appliedMixins = [
+  IdsEventsMixin,
+  IdsThemeMixin,
+  IdsKeyboardMixin,
+  IdsClearableMixin
+];
 
 /** 
  * IDS Search Field Component
@@ -23,13 +37,80 @@ import styles from './ids-search-field.scss';
 
 @customElement('ids-search-field')
 @scss(styles)
-class IdsSearchField extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin) {
+class IdsSearchField extends mix(IdsElement).with(...appliedMixins) {
   constructor() {
     super();
   }
 
+  static get attributes() {
+    return [
+      attributes.VALUE,
+      attributes.PLACEHOLDER,
+      attributes.ID,
+      // and other ids-input settings
+      // attributes.BG_TRANSPARENT,
+      // attributes.CLEARABLE,
+      // attributes.CLEARABLE_FORCED,
+      // attributes.COMPACT,
+      // attributes.DISABLED,
+      // attributes.FIELD_HEIGHT,
+      // attributes.LABEL,
+      // attributes.LABEL_HIDDEN,
+      // attributes.LABEL_REQUIRED,
+      // attributes.ID,
+      // attributes.MODE,
+      // attributes.PLACEHOLDER,
+      // attributes.SIZE,
+      // attributes.READONLY,
+      // attributes.TEXT_ALIGN,
+      // attributes.TEXT_ELLIPSIS,
+      // attributes.TRIGGERFIELD,
+      // attributes.TYPE,
+      // attributes.VALUE,
+      // attributes.VERSION
+    ];
+  }
+
   connectedCallback() {
-    this.#attachEventListeners();
+    this.#attachEventHandlers();
     super.connectedCallback();
   }
+
+  template() {
+    return(
+      `
+      <div class="ids-search-field" id=${this.id}>
+        <ids-trigger-field 
+          size="md"
+          tabbable="false"
+          label="Search Field"
+          validate="required"
+        >
+          <ids-input 
+            id=""
+            value=""
+            placeholder="hi"
+          >
+          </ids-input>
+          <ids-trigger-button>
+            <ids-text audible="true">Icon trigger</ids-text>
+            <ids-icon
+              slot="icon"
+              icon="close"
+              size="small"
+            ></ids-icon>
+          </ids-trigger-button>
+        </ids-trigger-field>
+      </div>
+      `
+    )
+  }
+
+  #attachEventHandlers() {
+    // key press
+    // on change
+    // search (enter or finish typing)
+  }
 }
+
+export default IdsSearchField;
