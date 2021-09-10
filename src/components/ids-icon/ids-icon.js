05520b4f77fca608fce8600167ec7f68c029ec8f
Andrii Varhanov <andrii.varhanov@gmail.com>
2021-09-10 16:31:52 +0300

    Ignoring icon condition and step chart snap #308

--- src/components/ids-icon/ids-icon.js
+++ src/components/ids-icon/ids-icon.js
@@ -265,6 +265,7 @@ class IdsIcon extends mix(IdsElement).with(IdsEventsMixin, IdsLocaleMixin) {
   #adjustViewbox() {
     let viewboxSize = '0 0 18 18';
 
+    /* istanbul ignore next */
     switch (this.icon) {
     case 'logo':
       viewboxSize = '0 0 34 34';
