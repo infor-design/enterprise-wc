cf400b927d07dd16d6e095a02dec71cde6f2202c
Vivian Leung <vivian.leung@infor.com>
2021-09-13 16:32:54 -0700

    change example terms to boring terms

--- demos/ids-search-field/index.js
+++ demos/ids-search-field/index.js
@@ -1,5 +1,2 @@
 import IdsSearchField from '../../src/components/ids-search-field';
-import IdsTriggerField from '../../src/components/ids-trigger-field';
-import IdsTriggerButton from '../../src/components/ids-trigger-field/ids-trigger-button';
-import IdsInput from '../../src/components/ids-input';
-import IdsIcon from '../../src/components/ids-icon';
+import IdsHeader from '../../src/components/ids-header';
