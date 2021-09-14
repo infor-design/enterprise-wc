3bd6d38bc5723fc4a118c2543d7452142c43b0f8
Robert Concepcion III <robert.concepcion.iii@gmail.com>
2021-09-13 21:55:15 -0400

    revert mixin builder

--- src/core/ids-mixin-builder.js
+++ src/core/ids-mixin-builder.js
@@ -1,11 +1,28 @@
 /**
+ * Combines two mixins with a base class in a prettier syntax
+ * Adapted from https://justinfagnani.com/2015/12/21/real-mixins-with-javascript-classes/
+ */
+ class MixinBuilder {
+  /**
+   * @param {CustomElementConstructor} superclass the class to originate from
+   */
+  constructor(superclass) {
+    this.superclass = superclass;
+  }
+
+  /**
+   * Mix a base object with a set of mixins
+   * @param {...Function} mixins indeterminate list of function Classes to mix into the superclass
+   * @returns {HTMLElement} the new "mixed" Class
+   */
+  with(...mixins) {
+    return mixins.reduce((c, mixin) => mixin(c), this.superclass);
+  }
+}
+
+/**
  * @param {CustomElementConstructor} superclass the base class to start with
  * @returns {any} a new base class with mixin attribs/methods included
  */
-export default function mix(superclass) {
-  return {
-    with(...mixins) {
-      return mixins.reduce((c, mixin) => mixin(c), superclass);
-    }
-  };
-}
+const mix = (superclass) => new MixinBuilder(superclass);
+export default mix;
