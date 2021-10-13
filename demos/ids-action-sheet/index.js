e2af1a3920a3b8f02104af85483dbef47d333833
jmacaluso711 <macaluso.john@gmail.com>
2021-10-13 14:16:17 -0400

    Ids action sheet scaffolding

---
+++ demos/ids-action-sheet/index.js
@@ -0,0 +1,5 @@
+import IdsContainer from '../../src/components/ids-container';
+import IdsText from '../../src/components/ids-text';
+import IdsCard from '../../src/components/ids-card';
+import { IdsLayoutGrid, IdsLayoutGridCell } from '../../src/components/ids-layout-grid';
+import IdsActionSheet from '../../src/components/ids-action-sheet/ids-action-sheet';
---
+++ src/components/ids-action-sheet/ids-action-sheet.js
@@ -0,0 +1,58 @@
+import {
+  IdsElement,
+  customElement,
+  scss,
+  attributes,
+  mix
+} from '../../core';
+
+// Import Mixins
+import {
+  IdsEventsMixin,
+  IdsThemeMixin
+} from '../../mixins';
+
+import styles from './ids-action-sheet.scss';
+
+/**
+ * IDS Action Sheet Component
+ * @type {IdsActionSheet}
+ * @inherits IdsElement
+ * @mixes IdsEventsMixin
+ * @mixes IdsKeyboardMixin
+ * @mixes IdsThemeMixin
+ */
+@customElement('ids-action-sheet')
+@scss(styles)
+class IdsActionSheet extends mix(IdsElement).with(IdsEventsMixin, IdsThemeMixin) {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  /**
+   * Return the attributes we handle as getters/setters
+   * @returns {Array} The attributes in an array
+   */
+  static get attributes() {
+    return [attributes.HIDE];
+  }
+
+  /**
+   * Inner template contents
+   * @returns {string} The template
+   */
+  template() {
+    return `
+      <div class="ids-action-sheet">
+        <ids-text>IDS Action Sheet</ids-text>
+        <slot></slot>
+      </div>
+    `;
+  }
+}
+
+export default IdsActionSheet;
