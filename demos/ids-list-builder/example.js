8c9a02e0cba460b9cf73857c4b32844a6ae4047d
Vivian Leung <vivian.leung@infor.com>
2021-10-13 21:13:18 -0700

    commit example.js file

---
+++ demos/ids-list-builder/example.js
@@ -0,0 +1,121 @@
+const listBuilder = document.querySelector('#list-builder');
+
+listBuilder.data = [
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '1 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '2 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '3 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '4 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '5 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '6 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '7 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '8 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '9 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '10 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '11 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '12 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+  {
+    id: 1,
+    productId: '7439937961',
+    productName: '13 Steampan Lid',
+    inStock: true,
+    units: '9',
+    unitPrice: 23,
+    color: 'Green'
+  },
+];
