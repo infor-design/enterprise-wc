16b8dd014bcd9b1936b6c5c8d9e1064df12d0fe5
jmacaluso711 <macaluso.john@gmail.com>
2021-11-01 17:33:22 -0400

    Add test coverage for ids-hidden

--- demos/ids-hidden/index.js
+++ demos/ids-hidden/index.js
@@ -1,2 +1,2 @@
 import IdsHidden from '../../src/components/ids-hidden/ids-hidden';
-import Card from '../../src/components/ids-card';
+import IdsCard from '../../src/components/ids-card';
--- src/components/ids-hidden/ids-hidden.js
+++ src/components/ids-hidden/ids-hidden.js
@@ -29,18 +29,14 @@ class IdsHidden extends IdsElement {
     super();
   }
 
-  connectedCallback() {
-    // this.setHidden();
-  }
-
   /**
    * Return the attributes we handle as getters/setters
    * @returns {Array} The attributes in an array
    */
   static get attributes() {
     return [
-      'hide-up',
-      'hide-down'
+      attributes.HIDE_UP,
+      attributes.HIDE_DOWN
     ];
   }
 
@@ -52,34 +48,67 @@ class IdsHidden extends IdsElement {
     return `<slot part="children"></slot>`;
   }
 
+  /**
+   * Sets the max-width breakpoint
+   * @param {string} val xxl | xl | lg | md | sm | xs
+   * @memberof IdsHidden
+   */
   set hideDown(val) {
     if (val) {
       const mqUp = this.isWidthDown(breakpoints[val]);
+      this.setAttribute(attributes.HIDE_DOWN, val);
       mqUp.addEventListener('change', () => {
         this.checkScreen(mqUp);
       });
       this.checkScreen(mqUp);
+    } else {
+      this.removeAttribute(attributes.HIDE_DOWN);
     }
   }
 
+  /**
+   * Get the hideDown attribute
+   * @returns {string} hideDown
+   * @readonly
+   * @memberof IdsHidden
+   */
   get hideDown() {
-    return this.getAttribute('hide-down');
+    return this.getAttribute(attributes.HIDE_DOWN);
   }
 
+  /**
+   * Sets the min-width breakpoint
+   * @param {string} val xxl | xl | lg | md | sm | xs
+   * @memberof IdsHidden
+   */
   set hideUp(val) {
     if (val) {
       const mqUp = this.isWidthUp(breakpoints[val]);
+      this.setAttribute(attributes.HIDE_UP, val);
       mqUp.addEventListener('change', () => {
         this.checkScreen(mqUp);
       });
       this.checkScreen(mqUp);
+    } else {
+      this.removeAttribute(attributes.HIDE_UP);
     }
   }
 
+  /**
+   * Get the hideUp attribute
+   * @returns {string} hideUp
+   * @readonly
+   * @memberof IdsHidden
+   */
   get hideUp() {
-    return this.getAttribute('hide-up');
+    return this.getAttribute(attributes.HIDE_UP);
   }
 
+  /**
+   * Set the visible attribute
+   * @param {boolean} val of the visible attribute
+   * @memberof IdsHidden
+   */
   set visible(val) {
     const isValTruthy = stringToBool(val);
     if (isValTruthy) {
@@ -89,10 +118,21 @@ class IdsHidden extends IdsElement {
     }
   }
 
+  /**
+   * Get the visible attribute
+   * @returns {boolean} visible
+   * @readonly
+   * @memberof IdsHidden
+   */
   get visible() {
     return this.getAttribute('visible');
   }
 
+  /**
+   * Check the screen size
+   * @param {object} mq media query to check against
+   * @memberof IdsHidden
+   */
   checkScreen(mq) {
     if (mq.matches) {
       this.hidden = true;
@@ -103,11 +143,23 @@ class IdsHidden extends IdsElement {
     }
   }
 
+  /**
+   * Check for max width media query.
+   * @param {string} width size of the breakpoint
+   * @returns {object} media query
+   * @memberof IdsHidden
+   */
   isWidthDown(width) {
     const mq = window.matchMedia(`(max-width: ${width})`);
     return mq;
   }
 
+  /**
+   * Check for min width media query.
+   * @param {string} width size of the breakpoint
+   * @returns {object} media query
+   * @memberof IdsHidden
+   */
   isWidthUp(width) {
     const mq = window.matchMedia(`(min-width: ${width})`);
     return mq;
