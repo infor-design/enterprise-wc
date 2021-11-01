da2ab4bc32718ed3567a778472afb4c4dbf42920
jmacaluso711 <macaluso.john@gmail.com>
2021-11-01 14:36:37 -0400

    Streamline ids-hidden component

---
+++ demos/ids-hidden/index.js
@@ -0,0 +1,2 @@
+import IdsHidden from '../../src/components/ids-hidden/ids-hidden';
+import Card from '../../src/components/ids-card';
--- src/components/ids-hidden/ids-hidden.js
+++ src/components/ids-hidden/ids-hidden.js
@@ -2,6 +2,7 @@ import {
   IdsElement,
   customElement,
   attributes,
+  breakpoints,
   scss,
 } from '../../core';
 
@@ -12,15 +13,6 @@ import {
 } from '../../mixins';
 import { stringToBool } from '../../utils/ids-string-utils/ids-string-utils';
 import styles from './ids-hidden.scss';
-
-const breakpoints = [
-  { bpXxl: '1440px' },
-  { bpXl: '1280px' },
-  { bpLg: '1024px' },
-  { bpMd: '840px' },
-  { bpSm: '600px' },
-  { bpXs: '360px' }
-];
 
 /**
  * IDS Hidden Component
@@ -38,7 +30,7 @@ class IdsHidden extends IdsElement {
   }
 
   connectedCallback() {
-    this.setHidden();
+    // this.setHidden();
   }
 
   /**
@@ -47,19 +39,8 @@ class IdsHidden extends IdsElement {
    */
   static get attributes() {
     return [
-      attributes.BP_XXL_UP,
-      attributes.BP_XXL_DOWN,
-      attributes.BP_XL_UP,
-      attributes.BP_XL_DOWN,
-      attributes.BP_LG_UP,
-      attributes.BP_LG_DOWN,
-      attributes.BP_MD_UP,
-      attributes.BP_MD_DOWN,
-      attributes.BP_SM_UP,
-      attributes.BP_SM_DOWN,
-      attributes.BP_XS_UP,
-      attributes.BP_XS_DOWN,
-      'visible',
+      'hide-up',
+      'hide-down'
     ];
   }
 
@@ -71,168 +52,49 @@ class IdsHidden extends IdsElement {
     return `<slot part="children"></slot>`;
   }
 
-  set bpXxlUp(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute(attributes.BP_XXL_UP, true);
-    } else {
-      this.removeAttribute(attributes.BP_XXL_UP);
+  set hideDown(val) {
+    if (val) {
+      const mqUp = this.isWidthDown(breakpoints[val]);
+      mqUp.addEventListener('change', () => {
+        this.checkScreen(mqUp);
+      });
+      this.checkScreen(mqUp);
     }
   }
 
-  get bpXxlUp() {
-    return this.getAttribute(attributes.BP_XXL_UP);
+  get hideDown() {
+    return this.getAttribute('hide-down');
   }
 
-  set bpXxlDown(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute(attributes.BP_XXL_DOWN, true);
-    } else {
-      this.removeAttribute(attributes.BP_XXL_DOWN);
+  set hideUp(val) {
+    if (val) {
+      const mqUp = this.isWidthUp(breakpoints[val]);
+      mqUp.addEventListener('change', () => {
+        this.checkScreen(mqUp);
+      });
+      this.checkScreen(mqUp);
     }
   }
 
-  get bpXxlDown() {
-    return this.getAttribute(attributes.BP_XXL_DOWN);
+  get hideUp() {
+    return this.getAttribute('hide-up');
   }
 
-  set bpXlUp(val) {
+  set visible(val) {
     const isValTruthy = stringToBool(val);
     if (isValTruthy) {
-      this.setAttribute(attributes.BP_XL_UP, true);
+      this.setAttribute('visible', true);
     } else {
-      this.removeAttribute(attributes.BP_XL_UP);
+      this.removeAttribute('visible');
     }
   }
 
-  get bpXlUp() {
-    return this.getAttribute(attributes.BP_XL_UP);
+  get visible() {
+    return this.getAttribute('visible');
   }
 
-  set bpXlDown(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute(attributes.BP_XL_DOWN, true);
-    } else {
-      this.removeAttribute(attributes.BP_XL_DOWN);
-    }
-  }
-
-  get bpXlDown() {
-    return this.getAttribute(attributes.BP_XL_DOWN);
-  }
-
-  set bpLgUp(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute(attributes.BP_LG_UP, true);
-    } else {
-      this.removeAttribute(attributes.BP_LG_UP);
-    }
-  }
-
-  get bpLgUp() {
-    return this.getAttribute(attributes.BP_LG_UP);
-  }
-
-  set bpLgDown(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute(attributes.BP_LG_DOWN, true);
-    } else {
-      this.removeAttribute(attributes.BP_LG_DOWN);
-    }
-  }
-
-  get bpLgDown() {
-    return this.getAttribute(attributes.BP_LG_DOWN);
-  }
-
-  set bpMdUp(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute(attributes.BP_MD_UP, true);
-      this.bp = breakpoints[3].md;
-    } else {
-      this.removeAttribute(attributes.BP_MD_UP);
-      this.bg = null;
-    }
-  }
-
-  get bpMdUp() {
-    return this.getAttribute(attributes.BP_MD_UP);
-  }
-
-  set bpMdDown(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute(attributes.BP_MD_DOWN, true);
-    } else {
-      this.removeAttribute(attributes.BP_MD_DOWN);
-    }
-  }
-
-  get bpMdDown() {
-    return this.getAttribute(attributes.BP_MD_DOWN);
-  }
-
-  set bpSmUp(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute(attributes.BP_SM_UP, true);
-    } else {
-      this.removeAttribute(attributes.BP_SM_UP);
-    }
-  }
-
-  get bpSmUp() {
-    return this.getAttribute(attributes.BP_SM_UP);
-  }
-
-  set bpSmDown(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute(attributes.BP_SM_DOWN, true);
-      this.bp = breakpoints[4].sm;
-    } else {
-      this.removeAttribute(attributes.BP_SM_DOWN);
-      this.bp = null;
-    }
-  }
-
-  get bpSmDown() {
-    return this.getAttribute(attributes.BP_SM_DOWN);
-  }
-
-  set bpXsUp(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute(attributes.BP_XS_UP, true);
-    } else {
-      this.removeAttribute(attributes.BP_XS_UP);
-    }
-  }
-
-  get bpXsUp() {
-    return this.getAttribute(attributes.BP_XS_UP);
-  }
-
-  set bpXsDown(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute(attributes.BP_XS_DOWN, true);
-    } else {
-      this.removeAttribute(attributes.BP_XS_DOWN);
-    }
-  }
-
-  get bpXsDown() {
-    return this.getAttribute(attributes.BP_XS_DOWN);
-  }
-
-  checkScreen(e) {
-    if (e.matches) {
+  checkScreen(mq) {
+    if (mq.matches) {
       this.hidden = true;
       this.removeAttribute('visible');
     } else {
@@ -249,43 +111,6 @@ class IdsHidden extends IdsElement {
   isWidthUp(width) {
     const mq = window.matchMedia(`(min-width: ${width})`);
     return mq;
-  }
-
-  set visible(val) {
-    const isValTruthy = stringToBool(val);
-    if (isValTruthy) {
-      this.setAttribute('visible', true);
-    } else {
-      this.removeAttribute('visible');
-    }
-  }
-
-  get visible() {
-    return this.getAttribute('visible');
-  }
-
-  setHidden() {
-    breakpoints.forEach((breakpoint) => {
-      const breakpointKey = Object.keys(breakpoint)[0];
-      const breakpointUp = `${breakpointKey}Up`;
-      const breakpointDown = `${breakpointKey}Down`;
-
-      if (this[breakpointUp]) {
-        const mqUp = this.isWidthUp(breakpoint[breakpointKey]);
-        mqUp.addEventListener('change', () => {
-          this.checkScreen(mqUp);
-        });
-        this.checkScreen(mqUp);
-      }
-
-      if (this[breakpointDown]) {
-        const mqDown = this.isWidthDown(breakpoint[breakpointKey]);
-        mqDown.addEventListener('change', () => {
-          this.checkScreen(mqDown);
-        });
-        this.checkScreen(mqDown);
-      }
-    });
   }
 }
 
