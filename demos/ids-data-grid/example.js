4fab25a2163837b535d3c3181bca0326e4153294
vivz753 <vivz753@gmail.com>
2021-11-11 14:33:24 -0800

    ids-virtual-scroll: fix virtual-scroll example, update demo to use products.json instead of books.json

--- demos/ids-data-grid/example.js
+++ demos/ids-data-grid/example.js
@@ -3,71 +3,79 @@ import './index.scss';
 
 // Example for populating the DataGrid
 const dataGrid = document.querySelector('#data-grid-1');
+const container = document.querySelector('ids-container');
 
-// Do an ajax request
-const xmlhttp = new XMLHttpRequest();
-const url = '/data/products.json';
-const columns = [];
+(async function init() {
+  // Set Locale and wait for it to load
+  await container.setLocale('en-US');
 
-// Set up columns
-columns.push({
-  id: 'id',
-  name: 'ID',
-  field: 'id',
-  formatter: dataGrid.formatters.text,
-  width: 80,
-  sortable: true
-});
-columns.push({
-  id: 'color',
-  name: 'Color',
-  field: 'color',
-  formatter: dataGrid.formatters.text,
-  sortable: true
-});
-columns.push({
-  id: 'inStock',
-  name: 'In Stock',
-  field: 'inStock',
-  formatter: dataGrid.formatters.text,
-  sortable: true
-});
-columns.push({
-  id: 'productId',
-  name: 'Product Id',
-  field: 'productId',
-  formatter: dataGrid.formatters.text,
-  sortable: true
-});
-columns.push({
-  id: 'productName',
-  name: 'Product Name',
-  field: 'productName',
-  formatter: dataGrid.formatters.text,
-  sortable: true
-});
-columns.push({
-  id: 'unitPrice',
-  name: 'Unit Price',
-  field: 'unitPrice',
-  formatter: dataGrid.formatters.text,
-  sortable: true
-});
-columns.push({
-  id: 'units',
-  name: 'Units',
-  field: 'units',
-  formatter: dataGrid.formatters.text,
-  sortable: true
-});
+  // Do an ajax request
+  const xmlhttp = new XMLHttpRequest();
+  const url = '/data/products.json';
+  const columns = [];
 
-xmlhttp.onreadystatechange = function onreadystatechange() {
-  if (this.readyState === 4 && this.status === 200) {
-    dataGrid.columns = columns;
-    dataGrid.data = JSON.parse(this.responseText);
-  }
-};
+  // Set up columns
+  columns.push({
+    id: 'id',
+    name: 'ID',
+    field: 'id',
+    formatter: dataGrid.formatters.text,
+    width: 80,
+    sortable: true
+  });
+  columns.push({
+    id: 'color',
+    name: 'Color',
+    field: 'color',
+    formatter: dataGrid.formatters.text,
+    sortable: true
+  });
+  columns.push({
+    id: 'inStock',
+    name: 'In Stock',
+    field: 'inStock',
+    formatter: dataGrid.formatters.text,
+    sortable: true
+  });
+  columns.push({
+    id: 'productId',
+    name: 'Product Id',
+    field: 'productId',
+    formatter: dataGrid.formatters.text,
+    sortable: true
+  });
+  columns.push({
+    id: 'productName',
+    name: 'Product Name',
+    field: 'productName',
+    formatter: dataGrid.formatters.text,
+    sortable: true
+  });
+  columns.push({
+    id: 'unitPrice',
+    name: 'Unit Price',
+    field: 'unitPrice',
+    formatter: dataGrid.formatters.text,
+    sortable: true
+  });
+  columns.push({
+    id: 'units',
+    name: 'Units',
+    field: 'units',
+    formatter: dataGrid.formatters.text,
+    sortable: true
+  });
 
-// Execute the request
-xmlhttp.open('GET', url, true);
-xmlhttp.send();
+  xmlhttp.onreadystatechange = function onreadystatechange() {
+    if (this.readyState === 4 && this.status === 200) {
+      dataGrid.columns = columns;
+      dataGrid.data = JSON.parse(this.responseText);
+      console.info('Loading Time:', window.performance.now());
+      console.info('Page Memory:', window.performance.memory);
+    }
+  };
+
+  // Execute the request
+  xmlhttp.open('GET', url, true);
+  xmlhttp.send();
+}());
