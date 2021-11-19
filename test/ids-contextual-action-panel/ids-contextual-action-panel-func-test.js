408a560bfae205ee4809090dd180a7bf2dd6da0c
Tim McConechy <tim.mcconechy@infor.com>
2021-11-19 15:40:37 -0500

    Fix one more lint issue

--- test/ids-contextual-action-panel/ids-contextual-action-panel-func-test.js
+++ test/ids-contextual-action-panel/ids-contextual-action-panel-func-test.js
@@ -4,7 +4,7 @@
 import '../helpers/resize-observer-mock';
 import wait from '../helpers/wait';
 
-import IdsContextualActionPanel from '../../src/components/ids-contextual-action-panel/ids-contextual-action-pane-base';
+import IdsContextualActionPanel from '../../src/components/ids-contextual-action-panel/ids-contextual-action-panel';
 import { IdsButton } from '../../src/components/ids-button/ids-button';
 import { IdsToolbar } from '../../src/components/ids-toolbar/ids-toolbar';
 
