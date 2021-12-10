5230210b350ae4d9507bd58cbbec5fe283a913b1
jmacaluso711 <macaluso.john@gmail.com>
2021-12-10 14:06:15 -0500

    Add error page yaml

--- src/components/ids-error-page/ids-error-page.js
+++ src/components/ids-error-page/ids-error-page.js
@@ -12,10 +12,6 @@ import {
   IdsThemeMixin
 } from '../../mixins';
 
-import { IdsXssUtils } from '../../utils/ids-xss-utils/ids-xss-utils';
-
-import IdsInput from '../ids-input';
-
 import styles from './ids-error-page.scss';
 
 /**
@@ -44,6 +40,8 @@ class IdsErrorPage extends mix(IdsElement).with(
   static get attributes() {
     return [
       ...super.attributes,
+      attributes.MESSAGE,
+      attributes.ICON,
       attributes.TITLE
     ];
   }
