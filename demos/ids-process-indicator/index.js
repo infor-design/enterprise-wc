2923b7af69cd00c5e322e4737c2156ca18ba5990
Vivian Leung <vivian.leung@infor.com>
2021-12-10 09:31:08 -0800

    process-indicator: remove IDS text import from demos index.js

--- demos/ids-process-indicator/index.js
+++ demos/ids-process-indicator/index.js
@@ -1,3 +1,2 @@
 import IdsProcessIndicator from '../../src/components/ids-process-indicator';
 import IdsHyperlink from '../../src/components/ids-hyperlink';
-import IdsText from '../../src/components/ids-text';
