aaa15aed8242ef24021e0ac555acb41fc3fcc07b
Deepak Verma <deepak.verma@infor.com>
2021-12-22 12:59:57 -0500

    Update to be default toolbar in light dom

--- src/components/ids-editor/ids-editor.js
+++ src/components/ids-editor/ids-editor.js
@@ -70,6 +70,7 @@ export default class IdsEditor extends Base {
   connectedCallback() {
     this
       .elementsValueInModals()
+      .#addDefaultToolbar()
       .#initContent()
       .#setParagraphSeparator()
       .#attachEventHandlers();
@@ -93,120 +94,12 @@ export default class IdsEditor extends Base {
    * @returns {string} The template
    */
   template() {
-    const tmplToolbar = `
-      <ids-toolbar tabbable="true" type="formatter">
-        <ids-toolbar-section type="buttonset" favor>
-
-          <ids-menu-button editor-action="menu-button-formatblock" id="btn-formatblock" role="button" menu="menu-formatblock" tooltip="Choose Font Style" formatter-width="125px" dropdown-icon>
-            <span slot="text">Normal Text</span>
-          </ids-menu-button>
-          <ids-popup-menu id="menu-formatblock" target="#btn-formatblock">
-            <ids-menu-group>
-              <ids-menu-item value="normal" selected="true"><ids-text>Normal Text</ids-text></ids-menu-item>
-              <ids-menu-item value="h1"><ids-text font-size="28">Header 1</ids-text></ids-menu-item>
-              <ids-menu-item value="h2"><ids-text font-size="24">Header 2</ids-text></ids-menu-item>
-              <ids-menu-item value="h3"><ids-text font-size="20">Header 3</ids-text></ids-menu-item>
-            </ids-menu-group>
-          </ids-popup-menu>
-
-          <ids-separator vertical></ids-separator>
-
-          <ids-button editor-action="bold" square="true" tooltip="Toggle Bold Text">
-            <span slot="text" class="audible">Bold</span>
-            <ids-icon slot="icon" icon="bold"></ids-icon>
-          </ids-button>
-
-          <ids-button editor-action="italic" square="true" tooltip="Toggle Italic Text">
-            <span slot="text" class="audible">Italic</span>
-            <ids-icon slot="icon" icon="italic"></ids-icon>
-          </ids-button>
-
-          <ids-button editor-action="underline" square="true" tooltip="Toggle Underline Text">
-            <span slot="text" class="audible">Underline</span>
-            <ids-icon slot="icon" icon="underline"></ids-icon>
-          </ids-button>
-
-          <ids-button editor-action="strikethrough" square="true" tooltip="Toggle Strike Through Text">
-            <span slot="text" class="audible">Strike through</span>
-            <ids-icon slot="icon" icon="strike-through"></ids-icon>
-          </ids-button>
-
-          <ids-separator vertical></ids-separator>
-
-          <ids-button editor-action="forecolor" editor-action-value="#ff0000" square="true" tooltip="Text Color">
-            <span slot="text" class="audible">Text color</span>
-            <ids-icon slot="icon" icon="fore-color"></ids-icon>
-          </ids-button>
-
-          <ids-separator vertical></ids-separator>
-
-          <ids-button editor-action="alignleft" square="true" tooltip="Align Left">
-            <span slot="text" class="audible">Align left</span>
-            <ids-icon slot="icon" icon="left-text-align"></ids-icon>
-          </ids-button>
-
-          <ids-button editor-action="aligncenter" square="true" tooltip="Align Center">
-            <span slot="text" class="audible">Align center</span>
-            <ids-icon slot="icon" icon="center-text"></ids-icon>
-          </ids-button>
-
-          <ids-button editor-action="alignright" square="true" tooltip="Align Right">
-            <span slot="text" class="audible">Align right</span>
-            <ids-icon slot="icon" icon="right-text-align"></ids-icon>
-          </ids-button>
-
-          <ids-separator vertical></ids-separator>
-
-          <ids-button editor-action="blockquote" square="true" tooltip="Block Quote">
-            <span slot="text" class="audible">Block quote</span>
-            <ids-icon slot="icon" icon="quote"></ids-icon>
-          </ids-button>
-
-          <ids-button editor-action="orderedlist" square="true" tooltip="Insert/Remove Numbered List">
-            <span slot="text" class="audible">Ordered List</span>
-            <ids-icon slot="icon" icon="number-list"></ids-icon>
-          </ids-button>
-
-          <ids-button editor-action="unorderedlist" square="true" tooltip="Insert/Remove Bulleted List">
-            <span slot="text" class="audible">Unordered List</span>
-            <ids-icon slot="icon" icon="bullet-list"></ids-icon>
-          </ids-button>
-
-          <ids-separator vertical></ids-separator>
-
-          <ids-button editor-action="hyperlink" square="true" tooltip="Insert Anchor">
-            <span slot="text" class="audible">Insert Anchor</span>
-            <ids-icon slot="icon" icon="link"></ids-icon>
-          </ids-button>
-
-          <ids-button editor-action="insertimage" square="true" tooltip="Insert Image">
-            <span slot="text" class="audible">Insert Image</span>
-            <ids-icon slot="icon" icon="insert-image"></ids-icon>
-          </ids-button>
-
-          <ids-separator vertical></ids-separator>
-
-          <ids-button editor-action="clearformatting" square="true" tooltip="Clear Formatting">
-            <span slot="text" class="audible">Clear Formatting</span>
-            <ids-icon slot="icon" icon="clear-formatting"></ids-icon>
-          </ids-button>
-
-          <ids-separator vertical></ids-separator>
-
-          <ids-button editor-action="sourcemode" square="true" tooltip="View Source">
-            <span slot="text" class="audible">View Source</span>
-            <ids-icon slot="icon" icon="html" width="38" viewbox="0 0 54 18"></ids-icon>
-          </ids-button>
-
-        </ids-toolbar-section>
-      </ids-toolbar>`;
-
     return `
       <div class="ids-editor" part="editor">
         <slot name="content" class="hidden"></slot>
         <div class="main-container" part="main-container">
           <div class="toolbar-container" part="toolbar-container">
-            <slot name="toolbar">${tmplToolbar}</slot>
+            <slot name="toolbar"></slot>
           </div>
           <div id="editor-container" class="editor-container" part="editor-container" contenteditable="true" aria-multiline="true" role="textbox" aria-label="${this.label}"></div>
           <div class="source-container" part="source-container"></div>
@@ -361,6 +254,128 @@ export default class IdsEditor extends Base {
     redo: { action: 'redo' },
     undo: { action: 'undo' },
   };
+
+  /**
+   * Add the default toolbar
+   * @private
+   * @returns {object} This API object for chaining
+   */
+  #addDefaultToolbar() {
+    const tmplToolbar = `
+      <ids-toolbar slot="toolbar" tabbable="true" type="formatter">
+        <ids-toolbar-section type="buttonset" favor>
+
+          <ids-menu-button editor-action="menu-button-formatblock" id="btn-formatblock" role="button" menu="menu-formatblock" tooltip="Choose Font Style" formatter-width="125px" dropdown-icon>
+            <span slot="text">Normal Text</span>
+          </ids-menu-button>
+          <ids-popup-menu id="menu-formatblock" target="#btn-formatblock">
+            <ids-menu-group>
+              <ids-menu-item value="normal" selected="true"><ids-text>Normal Text</ids-text></ids-menu-item>
+              <ids-menu-item value="h1"><ids-text font-size="28">Header 1</ids-text></ids-menu-item>
+              <ids-menu-item value="h2"><ids-text font-size="24">Header 2</ids-text></ids-menu-item>
+              <ids-menu-item value="h3"><ids-text font-size="20">Header 3</ids-text></ids-menu-item>
+            </ids-menu-group>
+          </ids-popup-menu>
+
+          <ids-separator vertical></ids-separator>
+
+          <ids-button editor-action="bold" square="true" tooltip="Toggle Bold Text">
+            <span slot="text" class="audible">Bold</span>
+            <ids-icon slot="icon" icon="bold"></ids-icon>
+          </ids-button>
+
+          <ids-button editor-action="italic" square="true" tooltip="Toggle Italic Text">
+            <span slot="text" class="audible">Italic</span>
+            <ids-icon slot="icon" icon="italic"></ids-icon>
+          </ids-button>
+
+          <ids-button editor-action="underline" square="true" tooltip="Toggle Underline Text">
+            <span slot="text" class="audible">Underline</span>
+            <ids-icon slot="icon" icon="underline"></ids-icon>
+          </ids-button>
+
+          <ids-button editor-action="strikethrough" square="true" tooltip="Toggle Strike Through Text">
+            <span slot="text" class="audible">Strike through</span>
+            <ids-icon slot="icon" icon="strike-through"></ids-icon>
+          </ids-button>
+
+          <ids-separator vertical></ids-separator>
+
+          <ids-button editor-action="forecolor" editor-action-value="#ff0000" square="true" tooltip="Text Color">
+            <span slot="text" class="audible">Text color</span>
+            <ids-icon slot="icon" icon="fore-color"></ids-icon>
+          </ids-button>
+
+          <ids-separator vertical></ids-separator>
+
+          <ids-button editor-action="alignleft" square="true" tooltip="Align Left">
+            <span slot="text" class="audible">Align left</span>
+            <ids-icon slot="icon" icon="left-text-align"></ids-icon>
+          </ids-button>
+
+          <ids-button editor-action="aligncenter" square="true" tooltip="Align Center">
+            <span slot="text" class="audible">Align center</span>
+            <ids-icon slot="icon" icon="center-text"></ids-icon>
+          </ids-button>
+
+          <ids-button editor-action="alignright" square="true" tooltip="Align Right">
+            <span slot="text" class="audible">Align right</span>
+            <ids-icon slot="icon" icon="right-text-align"></ids-icon>
+          </ids-button>
+
+          <ids-separator vertical></ids-separator>
+
+          <ids-button editor-action="blockquote" square="true" tooltip="Block Quote">
+            <span slot="text" class="audible">Block quote</span>
+            <ids-icon slot="icon" icon="quote"></ids-icon>
+          </ids-button>
+
+          <ids-button editor-action="orderedlist" square="true" tooltip="Insert/Remove Numbered List">
+            <span slot="text" class="audible">Ordered List</span>
+            <ids-icon slot="icon" icon="number-list"></ids-icon>
+          </ids-button>
+
+          <ids-button editor-action="unorderedlist" square="true" tooltip="Insert/Remove Bulleted List">
+            <span slot="text" class="audible">Unordered List</span>
+            <ids-icon slot="icon" icon="bullet-list"></ids-icon>
+          </ids-button>
+
+          <ids-separator vertical></ids-separator>
+
+          <ids-button editor-action="hyperlink" square="true" tooltip="Insert Anchor">
+            <span slot="text" class="audible">Insert Anchor</span>
+            <ids-icon slot="icon" icon="link"></ids-icon>
+          </ids-button>
+
+          <ids-button editor-action="insertimage" square="true" tooltip="Insert Image">
+            <span slot="text" class="audible">Insert Image</span>
+            <ids-icon slot="icon" icon="insert-image"></ids-icon>
+          </ids-button>
+
+          <ids-separator vertical></ids-separator>
+
+          <ids-button editor-action="clearformatting" square="true" tooltip="Clear Formatting">
+            <span slot="text" class="audible">Clear Formatting</span>
+            <ids-icon slot="icon" icon="clear-formatting"></ids-icon>
+          </ids-button>
+
+          <ids-separator vertical></ids-separator>
+
+          <ids-button editor-action="sourcemode" square="true" tooltip="View Source">
+            <span slot="text" class="audible">View Source</span>
+            <ids-icon slot="icon" icon="html" width="38" viewbox="0 0 54 18"></ids-icon>
+          </ids-button>
+
+        </ids-toolbar-section>
+      </ids-toolbar>`;
+
+    const slot = this.querySelector('[slot="toolbar"]');
+    if (!slot) {
+      this.insertAdjacentHTML('afterbegin', tmplToolbar);
+    }
+
+    return this;
+  }
 
   /**
    * Get block element for given node
