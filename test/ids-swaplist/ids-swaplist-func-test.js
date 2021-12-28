f0bb236931194d6214db499a70e3ef82e9b68588
Vivian Leung <vivian.leung@infor.com>
2021-12-27 16:37:03 -0800

    ids-swaplist test: change VANILLA to SWAPLIST per tim's request

--- test/ids-swaplist/ids-swaplist-func-test.js
+++ test/ids-swaplist/ids-swaplist-func-test.js
@@ -4,7 +4,7 @@
 import IdsSwapList from '../../src/components/ids-swaplist/ids-swaplist';
 
 const HTMLSnippets = {
-  VANILLA_COMPONENT: (
+  SWAPLIST_COMPONENT: (
   `<ids-swaplist>
   </ids-swaplist>`
   ),
@@ -30,21 +30,21 @@ describe('IdsSwapList Component', () => {
   });
 
   it('renders with no errors', async () => {
-    idsSwapList = await createElemViaTemplate(HTMLSnippets.VANILLA_COMPONENT);
+    idsSwapList = await createElemViaTemplate(HTMLSnippets.SWAPLIST_COMPONENT);
 
     const errors = jest.spyOn(global.console, 'error');
     expect(document.querySelectorAll('ids-swaplist').length).toEqual(1);
 
     idsSwapList.remove();
 
-    idsSwapList = await createElemViaTemplate(HTMLSnippets.VANILLA_COMPONENT);
+    idsSwapList = await createElemViaTemplate(HTMLSnippets.SWAPLIST_COMPONENT);
     expect(document.querySelectorAll('ids-swaplist').length).toEqual(1);
 
     expect(errors).not.toHaveBeenCalled();
   });
 
   it('renders correctly', async () => {
-    idsSwapList = await createElemViaTemplate(HTMLSnippets.VANILLA_COMPONENT);
+    idsSwapList = await createElemViaTemplate(HTMLSnippets.SWAPLIST_COMPONENT);
     idsSwapList.template();
     expect(idsSwapList.outerHTML).toMatchSnapshot();
   });
