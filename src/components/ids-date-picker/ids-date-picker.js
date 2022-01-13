8716dab7ea83e197dd64739d3b29934ebda1ece8
Andrii Varhanov <andrii.varhanov@gmail.com>
2022-01-13 18:28:59 +0200

    Add format attr to datepicker #150

--- src/components/ids-date-picker/ids-date-picker.js
+++ src/components/ids-date-picker/ids-date-picker.js
@@ -8,6 +8,7 @@ import { stringToBool } from '../../utils/ids-string-utils/ids-string-utils';
 // Supporting components
 import IdsDropdown from '../ids-dropdown/ids-dropdown';
 import IdsIcon from '../ids-icon/ids-icon';
+import IdsInput from '../ids-input/ids-input';
 import IdsPopup from '../ids-popup/ids-popup';
 import IdsText from '../ids-text/ids-text';
 import IdsTriggerField from '../ids-trigger-field/ids-trigger-field';
@@ -57,10 +58,15 @@ class IdsDatePicker extends Base {
       ...super.attributes,
       attributes.DISABLED,
       attributes.FORMAT,
+      attributes.FORMAT,
+      attributes.ID,
       attributes.LABEL,
       attributes.PLACEHOLDER,
       attributes.READONLY,
+      attributes.TABBABLE,
       attributes.VALUE,
+      attributes.VALIDATE,
+      attributes.VALIDATION_EVENTS
     ];
   }
 
@@ -74,12 +80,13 @@ class IdsDatePicker extends Base {
         <ids-trigger-field
           label="${this.label}"
           size="${this.size}"
-          disabled="${this.disabled}"
-          readonly="${this.readonly}"
+          validate="${this.validate}"
         >
           <ids-text audible="true" translate-text="true">UseArrow</ids-text>
           <ids-input
             type="text"
+            value="${this.value}"
+            placeholder="${this.placeholder}"
           >
           </ids-input>
           <ids-trigger-button>
@@ -106,10 +113,13 @@ class IdsDatePicker extends Base {
 
   /**
    * Runs when a click event is propagated to the window.
+   * @param {MouseEvent} e the original click event
    * @returns {void}
    */
-  onOutsideClick() {
-    this.#togglePopup(false);
+  onOutsideClick(e) {
+    if (e.target !== this) {
+      this.#togglePopup(false);
+    }
   }
 
   /**
@@ -171,18 +181,43 @@ class IdsDatePicker extends Base {
     }
   }
 
+  get value() {
+    return this.getAttribute(attributes.VALUE) ?? '';
+  }
+
+  set value(val) {
+    if (!this.disabled && !this.readonly) {
+      this.setAttribute(attributes.VALUE, val);
+      this.#input.value = val;
+    }
+  }
+
+  get placeholder() {
+    return this.getAttribute(attributes.PLACEHOLDER) ?? '';
+  }
+
+  set placeholder(val) {
+    if (val) {
+      this.setAttribute(attributes.PLACEHOLDER, val);
+    } else {
+      this.removeAttribute(attributes.PLACEHOLDER);
+    }
+
+    this.#input.placeholder = val;
+  }
+
   get label() {
     return this.getAttribute(attributes.LABEL) ?? '';
   }
 
-  set label(value) {
-    if (value) {
-      this.setAttribute(attributes.LABEL, value);
+  set label(val) {
+    if (val) {
+      this.setAttribute(attributes.LABEL, val);
+      this.#triggerField.setAttribute(attributes.LABEL, val);
     } else {
       this.removeAttribute(attributes.LABEL);
+      this.#triggerField.removeAttribute(attributes.LABEL);
     }
-
-    this.#triggerField.label = value;
   }
 
   get disabled() {
@@ -224,14 +259,86 @@ class IdsDatePicker extends Base {
 
   get size() { return this.getAttribute(attributes.SIZE); }
 
-  set size(value) {
-    if (value) {
-      this.setAttribute(attributes.SIZE, value);
+  set size(val) {
+    this.setAttribute(attributes.SIZE, val);
+    this.#triggerField.size = val;
+  }
+
+  get tabbable() {
+    const attrVal = this.getAttribute(attributes.TABBABLE);
+
+    // tabbable by default
+    return stringToBool(attrVal) ?? true;
+  }
+
+  set tabbable(val) {
+    const boolVal = stringToBool(val);
+
+    if (boolVal) {
+      this.setAttribute(attributes.TABBABLE, boolVal);
     } else {
-      this.removeAttribute(attributes.SIZE);
+      this.removeAttribute(attributes.TABBABLE);
     }
 
-    this.#triggerField.size = value;
+    this.#triggerField.tabbable = boolVal;
+  }
+
+  get id() { return this.getAttribute(attributes.ID) ?? ''; }
+
+  set id(val) {
+    if (val) {
+      this.setAttribute(attributes.ID, val);
+      this.#triggerField.setAttribute(attributes.ID, val);
+    } else {
+      this.removeAttribute(attributes.ID);
+      this.#triggerField.removeAttribute(attributes.ID);
+    }
+  }
+
+  get validate() { return this.getAttribute(attributes.VALIDATE); }
+
+  set validate(val) {
+    if (val) {
+      this.setAttribute(attributes.VALIDATE, val);
+      this.#triggerField.setAttribute(attributes.VALIDATE, val);
+      this.#triggerField.setAttribute(attributes.VALIDATION_EVENTS, this.validationEvents);
+      this.#triggerField.handleValidation();
+    } else {
+      this.removeAttribute(attributes.VALIDATE);
+      this.#triggerField.removeAttribute(attributes.VALIDATE);
+      this.#triggerField.removeAttribute(attributes.VALIDATION_EVENTS);
+      this.#triggerField.handleValidation();
+    }
+  }
+
+  get validationEvents() { return this.getAttribute(attributes.VALIDATION_EVENTS) ?? 'change blur'; }
+
+  set validationEvents(val) {
+    if (val) {
+      this.setAttribute(attributes.VALIDATION_EVENTS, val);
+      this.#input.setAttribute(attributes.VALIDATION_EVENTS, val);
+    } else {
+      this.removeAttribute(attributes.VALIDATION_EVENTS);
+      this.#input.removeAttribute(attributes.VALIDATION_EVENTS);
+    }
+  }
+
+  get format() {
+    const attrVal = this.getAttribute(attributes.FORMAT);
+
+    if (attrVal) {
+      return attrVal;
+    }
+
+    return this.locale.calendar().dateFormat.short;
+  }
+
+  set format(val) {
+    if (val) {
+      this.setAttribute(attributes.FORMAT, val);
+    } else {
+      this.removeAttribute(attributes.FORMAT);
+    }
   }
 }
 
