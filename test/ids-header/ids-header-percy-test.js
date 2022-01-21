1764ee2e0cbefe7db9774a217cfc549f5c5e63ca
Ed Coyle <ed.coyle@infor.com>
2022-01-21 08:50:26 -0500

    Fix visual regression tests with Searchfield using a delay

--- test/ids-header/ids-header-percy-test.js
+++ test/ids-header/ids-header-percy-test.js
@@ -1,26 +1,21 @@
 import percySnapshot from '@percy/puppeteer';
+import waitForPageLoad from '../helpers/wait-for-page-load';
 
 describe('Ids Rating Percy Tests', () => {
   const url = 'http://localhost:4444/ids-header';
 
   it('should not have visual regressions in new light theme (percy)', async () => {
-    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
+    await waitForPageLoad(url, null, 30);
     await percySnapshot(page, 'ids-header-new-light');
   });
 
   it('should not have visual regressions in new dark theme (percy)', async () => {
-    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
-    await page.evaluate(() => {
-      document.querySelector('ids-theme-switcher').setAttribute('mode', 'dark');
-    });
+    await waitForPageLoad(url, 'dark', 30);
     await percySnapshot(page, 'ids-header-new-dark');
   });
 
   it('should not have visual regressions in new contrast theme (percy)', async () => {
-    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
-    await page.evaluate(() => {
-      document.querySelector('ids-theme-switcher').setAttribute('mode', 'contrast');
-    });
+    await waitForPageLoad(url, 'contrast', 30);
     await percySnapshot(page, 'ids-header-new-contrast');
   });
 });
--- test/ids-search-field/ids-search-field-percy-test.js
+++ test/ids-search-field/ids-search-field-percy-test.js
@@ -1,26 +1,21 @@
 import percySnapshot from '@percy/puppeteer';
+import waitForPageLoad from '../helpers/wait-for-page-load';
 
 describe('Ids Search Field Percy Tests', () => {
   const url = 'http://localhost:4444/ids-search-field';
 
   it('should not have visual regressions in new light theme (percy)', async () => {
-    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
+    await waitForPageLoad(url, null, 30);
     await percySnapshot(page, 'ids-search-field-new-light');
   });
 
   it('should not have visual regressions in new dark theme (percy)', async () => {
-    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
-    await page.evaluate(() => {
-      document.querySelector('ids-theme-switcher').setAttribute('mode', 'dark');
-    });
+    await waitForPageLoad(url, 'dark', 30);
     await percySnapshot(page, 'ids-search-field-new-dark');
   });
 
   it('should not have visual regressions in new contrast theme (percy)', async () => {
-    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
-    await page.evaluate(() => {
-      document.querySelector('ids-theme-switcher').setAttribute('mode', 'contrast');
-    });
+    await waitForPageLoad(url, 'contrast', 30);
     await percySnapshot(page, 'ids-search-field-new-contrast');
   });
 });
