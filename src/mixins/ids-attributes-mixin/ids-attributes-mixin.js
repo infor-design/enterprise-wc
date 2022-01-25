b7f41e6f4f732d81792484739e07cee2d79cc187
Clay Clecidor <clay.clecidor@infor.com>
2022-01-25 14:52:04 -0500

    428 - Added IdsAttributesMixin (saving decorator and updated docs/examples)

--- src/mixins/ids-attributes-mixin/ids-attributes-mixin.js
+++ src/mixins/ids-attributes-mixin/ids-attributes-mixin.js
@@ -47,6 +47,24 @@ export const defineProp = (target, name, defaultValue, { changing, changed }) =>
 };
 
 /**
+ * This decorator may go away, but I'm committing it for the sake of demonstration
+ *
+ * Possible usage could look like this:
+ *
+ * +  @attr(attributes.ALIGN, 'center', { changing, changed, })
+ * +  class IdsCustomComponent extends Base {}
+ *
+ * @param  {...any} args - attribute settings
+ * @returns {Function} - decorator handler
+ */
+export const decorator = (...args) => {
+  const [name, defaultValue, options = {}] = args;
+  return (target) => {
+    defineProp(target.prototype, name, defaultValue, options);
+  };
+};
+
+/**
  * A mixin that adds attribute getter/setter helper functionality to components
  * @param {any} superclass Accepts a superclass and creates a new subclass from it
  * @returns {any} The extended object
@@ -56,6 +74,29 @@ export const IdsAttributesMixin = (superclass) => class extends superclass {
 
   get props() { return this.#properties; }
 
+  /**
+   * Example usage:
+   *
+   * +  class IdsCustomComponent extends Base {
+   * +    constructor() {
+   * +      super();
+   * +      this.prop(attributes.ALIGN_X, 'start', { changed: this.alignXChanged });
+   * +      this.prop(attributes.ALIGN_Y, 'top').changing(this.alignYChanging).changed(this.alignYChanged);
+   * +      this.prop(attributes.ALIGN_Y, 'top');
+   * +      this.prop(attributes.AUTO_FOCUS, true);
+   * +      this.prop(attributes.DATA, { fname: "John", lname: "Doe" });
+   * +    }
+   * +
+   * +    alignXChanged(value) { console.log('alignXChanged, value, this.alignX); }
+   * +    alignYChanging(value) { console.log('alignYChanging, value, this.alignY); }
+   * +    alignYChanged(value) { console.log('alignYChanged, value, this.alignY); }
+   * +  }
+   *
+   * @param {string} name the attribute name that appears in the DOM
+   * @param {*} defaultValue the attributes default value (used to determine TypeCaster)
+   * @param {object} options additional settings
+   * @returns {object} - object containing changing/changed callbacks to react to attribute changes
+   */
   prop(name, defaultValue = false, options = {}) {
     const propInfo = defineProp(this, name, defaultValue, options);
     this.#properties.set(propInfo.attrName, propInfo);
@@ -71,6 +112,24 @@ export const IdsAttributesMixin = (superclass) => class extends superclass {
    * @returns {TypeCasters} needs keys for string|boolean|number|object
    */
   get propTypeCasters() { return TypeCasters; }
+
+  /**
+   * This class-based decorator wrapper may go away, but I'm committing it for the sake of demonstration
+   *
+   * Possible usage could look like this:
+   *
+   * + @IdsAttributes.attr(attributes.ALIGN_EDGE, 'center')
+   * + class IdsCustomComponent extends Base {}
+   *
+   * @see decorator()
+   * @param  {...any} args - attribute settings
+   * @returns {Function} - decorator handler
+   */
+  static attr(...args) {
+    return decorator(...args);
+  }
 };
+
+export const IdsAttributes = IdsAttributesMixin(HTMLElement);
 
 export default IdsAttributesMixin;
