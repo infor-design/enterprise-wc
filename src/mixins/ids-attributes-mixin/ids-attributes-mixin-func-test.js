58cba3f9813473c6839d4f68adb083c6fcb77396
Clay Clecidor <clay.clecidor@infor.com>
2022-01-25 21:40:21 -0500

    428 - Added IdsAttributesMixin (code review suggestions)

--- src/mixins/ids-attributes-mixin/ids-attributes-mixin-func-test.js
+++ src/mixins/ids-attributes-mixin/ids-attributes-mixin-func-test.js
@@ -6,8 +6,8 @@ describe('IdsAttributesMixin functional tests', () => {
   it.todo('makes prop names camelCase');
   it.todo('can add getter');
   it.todo('can add setter');
-  it.todo('can fire changing callback');
-  it.todo('can fire changed callback');
+  it.todo('can fire before callback');
+  it.todo('can fire after callback');
   it.todo('can override TypeCasters');
   it.todo('can be used as a Class @decorator');
 });
