5428132033bff2132259a5bda10093e7a592eaed
Tim McConechy <tim.mcconechy@infor.com>
2022-01-27 15:35:56 -0500

    Fix tests

--- test/ids-search-field/ids-search-field-percy-test.js
+++ test/ids-search-field/ids-search-field-percy-test.js
@@ -2,33 +2,29 @@ import percySnapshot from '@percy/puppeteer';
 
 describe('Ids Search Field Percy Tests', () => {
   const url = 'http://localhost:4444/ids-search-field';
-  const waitRAF = () => new Promise((resolve) => {
-    requestAnimationFrame(resolve);
-  });
 
   it('should not have visual regressions in new light theme (percy)', async () => {
-    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
-    await page.waitForSelector('#header-search[color-variant="alternate"]'); // Wait for Search Field to change variants/types
+    await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+    await page.waitFor('pierce/.ids-input.color-variant-alternate .ids-input-field');
     await percySnapshot(page, 'ids-search-field-new-light');
   });
 
   it('should not have visual regressions in new dark theme (percy)', async () => {
-    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
+    await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher').setAttribute('mode', 'dark');
     });
-    await page.waitForSelector('#header-search[color-variant="alternate"]'); // Wait for Search Field to change variants/types
+    await page.waitFor('pierce/.ids-input.color-variant-alternate .ids-input-field');
     await percySnapshot(page, 'ids-search-field-new-dark');
   });
 
   it('should not have visual regressions in new contrast theme (percy)', async () => {
-    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
+    await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher').setAttribute('mode', 'contrast');
     });
 
     await page.waitFor('pierce/.ids-input.color-variant-alternate .ids-input-field');
-    await waitRAF();
     await percySnapshot(page, 'ids-search-field-new-contrast');
   });
 });
