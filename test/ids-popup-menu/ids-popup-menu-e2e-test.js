59e9d95e888c9bfcbf79798e5572ba0b136598d3
jdilag <jdilag.dev@gmail.com>
2022-02-10 15:19:25 -0500

    fix popup hover clicks

--- test/ids-popup-menu/ids-popup-menu-e2e-test.js
+++ test/ids-popup-menu/ids-popup-menu-e2e-test.js
@@ -1,5 +1,8 @@
 describe('Ids Popup Menu e2e Tests', () => {
   const url = 'http://localhost:4444/ids-popup-menu';
+  const menuItemSelector = '#item-six'; // ids-menu-item with sub level ids-popup-menu
+  const subPopupMenuSelector = `${menuItemSelector} > ids-popup-menu`; // reference to sub level ids-popup-menu
+  const popupHoverDelay = 500; // popupDelay configured in ids-popup-interactions-mixin.js
 
   beforeAll(async () => {
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
@@ -14,5 +17,44 @@ describe('Ids Popup Menu e2e Tests', () => {
     await page.setBypassCSP(true);
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
     await expect(page).toPassAxeTests();
+  });
+
+  it('should open sub popup menu when menu item hovered', async () => {
+    // open popup menu
+    await page.click('ids-container', { button: 'right' });
+
+    // hover over menu item of top level popup menu
+    await page.hover(menuItemSelector);
+    await page.waitForTimeout(popupHoverDelay);
+
+    // check that sub popupmenu is visible
+    const isVisible = await page.$eval(subPopupMenuSelector, (el) => el?.visible);
+    expect(isVisible).toBeTruthy();
+  });
+
+  it('should open sub popup menu when menu item clicked', async () => {
+    // open popup menu
+    await page.click('ids-container', { button: 'right' });
+
+    // hover over menu item of top level popup menu
+    await page.click(menuItemSelector);
+
+    // check that sub popupmenu is visible
+    const isVisible = await page.$eval(subPopupMenuSelector, (el) => el?.visible);
+    expect(isVisible).toBeTruthy();
+  });
+
+  it('should keep sub ppopup menu open when menu item hovered and then clicked', async () => {
+    // open popup menu
+    await page.click('ids-container', { button: 'right' });
+
+    // hover over then click menu item of top level popup menu
+    await page.hover(menuItemSelector);
+    await page.waitForTimeout(popupHoverDelay);
+    await page.click(menuItemSelector);
+
+    // check that sub popupmenu is visible
+    const isVisible = await page.$eval(subPopupMenuSelector, (el) => el?.visible);
+    expect(isVisible).toBeTruthy();
   });
 });
