ef30b25d77ff1f6ad324caa6ee27a8a34644f952
Daniel Ortiz <dortiz@USNYMDORTIZ01.lan1>
2022-02-18 10:10:05 -0500

    remove sample data

--- src/components/ids-block-grid/demos/index.js
+++ src/components/ids-block-grid/demos/index.js
@@ -1,7 +1,6 @@
 import IdsBlockGrid from '../ids-block-grid';
 import CSP from '../../../assets/scripts/head-meta';
 import placeHolderImg from '../../../assets/images/placeholder-200x200.png'
-import sampleFile from '../../../assets/data/accounts.json';
 
 const images = document.querySelectorAll('img');
 images.forEach( img => img.src = placeHolderImg)
