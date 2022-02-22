4a9fa948c8ba8fa96412309396230e6eede24e4b
Jeffrey <jeff.wa428@gmail.com>
2022-02-22 08:13:11 -0600

    537 - Add ids-card examples

---
+++ demos/ids-card/example.js
@@ -0,0 +1,9 @@
+import './example.scss';
+
+import IdsCard from '../../src/components/ids-card/ids-card';
+import IdsImage from '../../src/components/ids-image/ids-image';
+import IdsMenuButton from '../../src/components/ids-menu-button/ids-menu-button';
+import IdsPopupMenu from '../../src/components/ids-popup-menu/ids-popup-menu';
+import IdsPopup from '../../src/components/ids-popup/ids-popup';
+import IdsMenuItem from '../../src/components/ids-menu/ids-menu-item';
+import IdsMenuGroup from '../../src/components/ids-menu/ids-menu-group';
