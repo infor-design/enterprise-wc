08158fdc36ee040a8680e729d1c6b1309ca44d34
Daniel Ortiz <dortiz@USNYMDORTIZ01.local>
2022-02-28 14:26:06 -0500

    adding isCore checker for build folder

--- scripts/webpack-prod-output.js
+++ scripts/webpack-prod-output.js
@@ -1,17 +1,16 @@
 const NodeFsFiles = require('./node-fs-files');
 
 const prodOutput = (pathData) => {
-  const srcFileListArray = NodeFsFiles('./src', 'js')
+  const srcFileListArray = NodeFsFiles('./src', 'js');
   const isWin32 = process.platform === 'win32' ? '\\' : '/';
-  const srcFileListFilter = srcFileListArray.filter( chunk => chunk.includes(`${pathData}.js`))
-  const srcFileListFilterItem = srcFileListFilter[0]
-  const srcFileListFilterItemArray = srcFileListFilterItem.split(isWin32)
-  srcFileListFilterItemArray.splice(0, 2)
-  srcFileListFilterItemArray.pop()
-  const srcFileListFilterItemArrayJoin = srcFileListFilterItemArray.join(isWin32)
-  console.log(srcFileListFilterItemArrayJoin)
-  return srcFileListFilterItemArrayJoin
-
+  const srcFileListFilter = srcFileListArray.filter((chunk) => chunk.includes(`${pathData}.js`));
+  const srcFileListFilterItem = srcFileListFilter[0];
+  const srcFileListFilterItemArray = srcFileListFilterItem.split(isWin32);
+  const isCore = srcFileListFilterItemArray[1] === 'core' ? 1 : 2;
+  srcFileListFilterItemArray.splice(0, isCore)
+  srcFileListFilterItemArray.pop();
+  const webpackOutputPath = srcFileListFilterItemArray.join(isWin32);
+  return webpackOutputPath;
 };
 
 module.exports = prodOutput;
