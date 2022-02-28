276a1ea91cc0e4d7c94249d29aaf10efece8fcae
Daniel Ortiz <dortiz@USNYMDORTIZ01.local>
2022-02-28 13:15:04 -0500

    update to webpack prod output

--- scripts/webpack-prod-output.js
+++ scripts/webpack-prod-output.js
@@ -1,12 +1,16 @@
 const NodeFsFiles = require('./node-fs-files');
 
 const prodOutput = (pathData) => {
-
   const srcFileListArray = NodeFsFiles('./src', 'js')
-
-  const sampleFilter = srcFileListArray.filter( chunk => chunk.includes(`${pathData}.js`))
-
-  console.log(sampleFilter)
+  const isWin32 = process.platform === 'win32' ? '\\' : '/';
+  const srcFileListFilter = srcFileListArray.filter( chunk => chunk.includes(`${pathData}.js`))
+  const srcFileListFilterItem = srcFileListFilter[0]
+  const srcFileListFilterItemArray = srcFileListFilterItem.split(isWin32)
+  srcFileListFilterItemArray.splice(0, 2)
+  srcFileListFilterItemArray.pop()
+  const srcFileListFilterItemArrayJoin = srcFileListFilterItemArray.join(isWin32)
+  console.log(srcFileListFilterItemArrayJoin)
+  return srcFileListFilterItemArrayJoin
 
 };
 
