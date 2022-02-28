57bc438682402ea002354a078ce9fb2ffb2b71bf
jmacaluso711 <macaluso.john@gmail.com>
2022-02-28 15:05:20 -0500

    Add swappable card example

--- demos/ids-swaplist/index.js
+++ demos/ids-swaplist/index.js
@@ -3,3 +3,4 @@ import IdsSwappable from '../../src/components/ids-swappable/ids-swappable';
 import IdsSwappableItem from '../../src/components/ids-swappable/ids-swappable-item';
 import IdsListView from '../../src/components/ids-list-view/ids-list-view';
 import IdsText from '../../src/components/ids-text/ids-text';
+import IdsCard from '../../src/components/ids-card/ids-card';
