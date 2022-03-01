1d7bc542bfba20b7771a8ae8499e72a4ebe36ab4
Ed Coyle <ed.coyle@infor.com>
2022-03-01 14:48:53 -0500

    Minor tweaks

--- src/components/ids-color-picker/ids-color-picker-base.js
+++ src/components/ids-color-picker/ids-color-picker-base.js
@@ -1,6 +1,9 @@
+import IdsLocaleMixin from '../../mixins/ids-locale-mixin/ids-locale-mixin';
 import IdsPopupOpenEventsMixin from '../../mixins/ids-popup-open-events-mixin/ids-popup-open-events-mixin';
 import IdsTriggerField from '../ids-trigger-field/ids-trigger-field';
 
-const Base = IdsPopupOpenEventsMixin(IdsTriggerField);
+const Base = IdsPopupOpenEventsMixin(
+  IdsLocaleMixin(IdsTriggerField)
+);
 
 export default Base;
