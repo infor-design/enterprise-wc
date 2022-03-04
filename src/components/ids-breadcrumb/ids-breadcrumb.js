9e3f67f042bdccf918a1733c80bc106d9a4a46f7
Jeffrey <jeff.wa428@gmail.com>
2022-03-04 01:38:29 -0600

    526 - Add truncated example

--- src/components/ids-breadcrumb/ids-breadcrumb.js
+++ src/components/ids-breadcrumb/ids-breadcrumb.js
@@ -2,6 +2,16 @@ import { customElement, scss } from '../../core/ids-decorators';
 import Base from './ids-breadcrumb-base';
 import styles from './ids-breadcrumb.scss';
 
+import IdsMenuButton from '../ids-menu-button/ids-menu-button';
+import IdsPopupMenu from '../ids-popup-menu/ids-popup-menu';
+import IdsPopup from '../ids-popup/ids-popup';
+
+import IdsMenuHeader from '../ids-menu/ids-menu-header';
+import IdsMenu from '../ids-menu/ids-menu';
+import IdsMenuItem from '../ids-menu/ids-menu-item';
+import IdsMenuGroup from '../ids-menu/ids-menu-group';
+import IdsSeparator from '../ids-separator/ids-separator';
+
 /**
  *  IDS Breadcrumb Component
  * @type {IdsBreadcrumb}
@@ -16,6 +26,67 @@ export default class IdsBreadcrumb extends Base {
   constructor() {
     super();
     this.#init();
+  }
+
+  connectedCallback() {
+    // Set observer for resize
+    this.#resizeObserver.disconnect();
+    this.#resizeObserver.observe(this.container);
+
+    super.connectedCallback();
+  }
+
+  #itemsCollapsed = [];
+
+  /**
+   * Attach the resize observer.
+   * @private
+   * @type {number}
+   */
+  #resizeObserver = new ResizeObserver(() => this.#resize());
+
+  /**
+   * Resize
+   * @private
+   * @returns {object} This API object for chaining
+   */
+  #resize() {
+    const widthLimit = this.clientWidth - 50;
+    const elementsToRemove = [];
+    let totalWidth = [...this.children].map((elem) => elem.offsetWidth).reduce((prev, cur) => prev + cur, 0);
+
+    // check if there is any element to collapse
+    for (const elem of this.children) {
+      if (totalWidth >= widthLimit) {
+        elementsToRemove.push(elem);
+        totalWidth -= elem.offsetWidth;
+      }
+    }
+
+    this.#itemsCollapsed = this.#itemsCollapsed.concat(elementsToRemove.map((elem) => ({
+      element: elem,
+      width: elem.offsetWidth,
+    })));
+    elementsToRemove.forEach((elem) => this.removeChild(elem));
+
+    // check if there is any element to show
+    while (totalWidth < widthLimit || this.#itemsCollapsed.length) {
+      const breadCrumb = this.#itemsCollapsed.pop();
+      if (totalWidth + breadCrumb.width < widthLimit) {
+        totalWidth += breadCrumb.width;
+        this.insertBefore(breadCrumb.element, this.firstElementChild);
+      } else {
+        this.#itemsCollapsed.push(breadCrumb);
+        break;
+      }
+    }
+
+    const menuStr = this.#itemsCollapsed
+      .map((item) => item.element.innerHTML)
+      .reduce((prev, cur) => `${prev}<ids-menu-item>${cur}</ids-menu-item>`, '');
+
+    this.container.querySelector('ids-menu-group').innerHTML = menuStr;
+    return this;
   }
 
   /**
@@ -37,6 +108,20 @@ export default class IdsBreadcrumb extends Base {
   template() {
     return `
       <div class="ids-breadcrumb">
+        <div class="ids-breadcrumb-menu">
+          <ids-menu-button id="icon-button" menu="icon-menu">
+            <ids-icon slot="icon" icon="more"></ids-icon>
+            <span class="audible">Icon Only Button</span>
+          </ids-menu-button>
+          <ids-popup-menu id="icon-menu" target="icon-button" trigger="click">
+            <ids-menu-group>
+              <ids-menu-item>Option One</ids-menu-item>
+              <ids-menu-item>Option Two</ids-menu-item>
+              <ids-menu-item>Option Three</ids-menu-item>
+            </ids-menu-group>
+          </ids-popup-menu>
+        </div>
+        
         <nav part="breadcrumb">
           <slot></slot>
         </nav>
