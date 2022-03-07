6dd75bbdc12781f05eb4c39a7425543b3a9f7840
Tim McConechy <tim.mcconechy@infor.com>
2022-03-07 17:24:39 -0500

    Fix lint issues

--- src/components/ids-editor/demos/side-by-side.js
+++ src/components/ids-editor/demos/side-by-side.js
@@ -3,7 +3,6 @@ import IdsEditor from '../ids-editor';
 
 // Initialize the 4.x
 document.addEventListener('DOMContentLoaded', () => {
-  console.log($);
   $('body').initialize();
   $('#editor-4x').editor();
 });
