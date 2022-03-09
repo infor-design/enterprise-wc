3145ba49ba811072a17794071a8fc48535468593
Tim McConechy <tim.mcconechy@infor.com>
2022-03-09 12:22:15 -0500

    Remove pushes on wrong branch

--- test/ids-date-picker/ids-date-picker-func-test.js
+++ test/ids-date-picker/ids-date-picker-func-test.js
@@ -207,12 +207,6 @@ describe('IdsDatePicker Component Tests', () => {
       expect(component.day).toEqual('18');
     });
 
-    it('can set visible and get the popup', () => {
-      expect(component.container.querySelector('ids-popup').hasAttribute('visible')).toBeFalsy();
-      component.popup.visible = true;
-      expect(component.container.querySelector('ids-popup').hasAttribute('visible')).toBeTruthy();
-    });
-
     it('should change attributes', () => {
       component.setAttribute('tabbable', true);
       component.setAttribute('show-today', true);
