f930559c049c2559096c426bb06f2fa87fe3a2d5
Tim McConechy <tim.mcconechy@infor.com>
2022-03-09 15:12:20 -0500

    Merge with main

--- demos/ids-swaplist/index.js
+++
@@ -1,6 +0,0 @@
-import IdsSwapList from '../../src/components/ids-swaplist/ids-swaplist';
-import IdsSwappable from '../../src/components/ids-swappable/ids-swappable';
-import IdsSwappableItem from '../../src/components/ids-swappable/ids-swappable-item';
-import IdsListView from '../../src/components/ids-list-view/ids-list-view';
-import IdsText from '../../src/components/ids-text/ids-text';
-import IdsCard from '../../src/components/ids-card/ids-card';
