c158be90caba7bef596adbd9269ac60245068c06
Jeffrey <jeff.wa428@gmail.com>
2022-03-14 20:07:14 -0500

    526 - Fix truncated item style issue

--- src/components/ids-csp/demos/index.js
+++ src/components/ids-csp/demos/index.js
@@ -5,7 +5,7 @@ const metaTags = `
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="Content-Security-Policy" content="
     script-src 'self' https://unpkg.com/;
-    style-src 'self' https://fonts.googleapis.com 'nonce-0a59a005';
+    style-src 'self' https://fonts.googleapis.com 'unsafe-inline';
     font-src 'self' data: https://fonts.gstatic.com;
   ">
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600&amp;amp;display=swap" rel="stylesheet">
