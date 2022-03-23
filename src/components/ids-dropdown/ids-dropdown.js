2f54a9da399ccf44d9316c0c109778c9a5807ab2
Ed Coyle <ed.coyle@infor.com>
2022-03-23 12:11:31 -0400

    Fix some accessibility problems in Dropdown/Listbox from display type change in Popup

--- src/components/ids-dropdown/ids-dropdown.js
+++ src/components/ids-dropdown/ids-dropdown.js
@@ -118,13 +118,41 @@ export default class IdsDropdown extends Base {
       'aria-expanded': 'false',
       'aria-autocomplete': 'list',
       'aria-haspopup': 'listbox',
-      'aria-description': this.locale?.translate('PressDown'),
+      'aria-label': this.locale?.translate('PressDown'),
       'aria-controls': this.listBox?.getAttribute('id') || `ids-list-box-${this.id}`
     };
 
-    this.listBox?.setAttribute('id', `ids-list-box-${this.id}`);
+    if (this.listBox) {
+      this.listBox.setAttribute('id', `ids-list-box-${this.id}`);
+      this.listBox.setAttribute('aria-labelledby', this.id);
+    }
     Object.keys(attrs).forEach((key) => this.setAttribute(key, attrs[key]));
     return this;
+  }
+
+  #setAriaOnMenuOpen() {
+    this.setAttribute('aria-expanded', 'true');
+
+    // Add aria for the open state
+    const selected = this.selectedOption;
+    this.listBox.setAttribute('tabindex', '0');
+    this.listBox?.setAttribute('aria-activedescendant', selected?.id || this.selectedIndex);
+    if (selected) {
+      selected.setAttribute('tabindex', '0');
+      selected.focus();
+    }
+  }
+
+  #setAriaOnMenuClose() {
+    this.setAttribute('aria-expanded', 'false');
+    this.listBox.removeAttribute('tabindex');
+
+    const selected = this.selected;
+    if (selected) {
+      selected.classList.remove('is-selected');
+      selected.setAttribute('tabindex', '-1');
+      this.selectedOption.classList.add('is-selected');
+    }
   }
 
   /**
@@ -177,11 +205,21 @@ export default class IdsDropdown extends Base {
   get value() { return this.getAttribute('value'); }
 
   /**
-   * Returns the selected option DOM element
+   * Returns the selected Listbox option based on the Dropdown's value.
    * @returns {HTMLElement} the selected option
    */
   get selectedOption() {
     return this.querySelector(`ids-list-box-option[value="${this.value}"]`);
+  }
+
+  /**
+   * Returns the currently-selected Listbox option
+   * (may be different from the Dropdown's value because of user input)
+   * @readonly
+   * @returns {HTMLElement|null} Reference to a selected Listbox option if one is present
+   */
+  get selected() {
+    return this.querySelector('ids-list-box-option.is-selected');
   }
 
   /**
@@ -332,6 +370,11 @@ export default class IdsDropdown extends Base {
     this.popup.arrow = 'none';
     this.popup.y = -1;
     this.popup.type = 'dropdown';
+
+    // Fix aria if the menu is closed
+    if (!this.popup.visible) {
+      this.#setAriaOnMenuClose();
+    }
   }
 
   /**
@@ -352,15 +395,7 @@ export default class IdsDropdown extends Base {
     this.popup.visible = true;
     this.addOpenEvents();
     this.container.active = true;
-    this.setAttribute('aria-expanded', 'true');
-
-    // Add aria for the open state
-    this.listBox?.setAttribute('aria-activedescendant', this.selectedOption?.id || this.selectedIndex);
-    const selected = this.listBox?.querySelector('.is-selected');
-    if (selected) {
-      selected.setAttribute('tabindex', 0);
-      selected.focus();
-    }
+    this.#setAriaOnMenuOpen();
   }
 
   /**
@@ -408,14 +443,7 @@ export default class IdsDropdown extends Base {
   close(noFocus) {
     this.popup.visible = false;
     this.container.active = false;
-    this.setAttribute('aria-expanded', 'false');
-    const selected = this.querySelector('ids-list-box-option.is-selected');
-
-    if (selected) {
-      selected.classList.remove('is-selected');
-      this.selectedOption.classList.add('is-selected');
-    }
-
+    this.#setAriaOnMenuClose();
     this.removeOpenEvents();
 
     if (!noFocus) {
@@ -472,6 +500,16 @@ export default class IdsDropdown extends Base {
     this.onEvent('languagechange.data-grid-container', this.closest('ids-container'), () => {
       this.#addAria();
     });
+
+    // Listen to IdsPopup's `hide/show` events and control some attributes
+    // on ListBox items for accessibility purposes.
+    this.onEvent('hide', this.popup, () => {
+
+    });
+    this.onEvent('show', this.popup, () => {
+
+    });
+
     return this;
   }
 
@@ -491,7 +529,7 @@ export default class IdsDropdown extends Base {
       e.stopImmediatePropagation();
       e.preventDefault();
 
-      const selected = this.querySelector('ids-list-box-option.is-selected');
+      const selected = this.selected;
       if (e.key === 'ArrowUp' && e.altKey) {
         this.value = selected.getAttribute('value');
         this.close();
@@ -526,7 +564,7 @@ export default class IdsDropdown extends Base {
         return;
       }
 
-      const selected = this.querySelector('ids-list-box-option.is-selected');
+      const selected = this.selected;
       this.value = selected.getAttribute('value');
       this.close();
     });
@@ -541,7 +579,7 @@ export default class IdsDropdown extends Base {
         this.container.focus();
       }
 
-      const selected = this.querySelector('ids-list-box-option.is-selected');
+      const selected = this.selected;
       this.value = selected.getAttribute('value');
       this.close(true);
     });
@@ -561,7 +599,7 @@ export default class IdsDropdown extends Base {
       .filter((a) => a.textContent.toLowerCase().indexOf(keyString.toLowerCase()) === 0);
 
     if (matches[0]) {
-      const selected = this.querySelector('ids-list-box-option.is-selected');
+      const selected = this.selected;
       selected?.classList.remove('is-selected');
       selected?.setAttribute('tabindex', '-1');
       matches[0].classList.add('is-selected');
