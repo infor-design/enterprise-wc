12027681cba548f3feeb01e2c97546c0f3adf874
Andrii Varhanov <andrii.varhanov@gmail.com>
2022-03-25 11:51:40 +0200

    Add forward/backward range selection examples #538

--- src/components/ids-date-picker/demos/range.js
+++ src/components/ids-date-picker/demos/range.js
@@ -4,6 +4,12 @@ import IdsDatePicker from '../ids-date-picker';
 // Example to set start/end of the range via component settings
 const rangePickerSettings = document.querySelector('#e2e-datepicker-settings-value');
 
+// Example range selection forward
+const rangePickerForward = document.querySelector('#e2e-datepicker-forward');
+
+// Example range selection backward
+const rangePickerBackward = document.querySelector('#e2e-datepicker-backward');
+
 if (rangePickerSettings) {
   rangePickerSettings.rangeSettings = {
     start: '2/5/2018',
@@ -13,4 +19,16 @@ if (rangePickerSettings) {
   rangePickerSettings.addEventListener('dayselected', (e) => {
     console.info('Range Selected', e.detail.rangeStart, e.detail.rangeEnd);
   });
+}
+
+if (rangePickerForward) {
+  rangePickerForward.rangeSettings = {
+    selectForward: true
+  };
+}
+
+if (rangePickerBackward) {
+  rangePickerBackward.rangeSettings = {
+    selectBackward: true
+  };
 }
