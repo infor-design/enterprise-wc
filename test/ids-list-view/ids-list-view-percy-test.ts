4715cd76fb35e046022fba4e008b76a536450da7
Tim McConechy <tim.mcconechy@infor.com>
2022-04-13 11:10:44 -0400

    Fix progress bar and dropdown

--- test/ids-list-view/ids-list-view-percy-test.ts
+++ test/ids-list-view/ids-list-view-percy-test.ts
@@ -7,22 +7,4 @@ describe('Ids List View Percy Tests', () => {
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
     await percySnapshot(page, 'ids-listview-new-light');
   });
-
-  it('should not have visual regressions in new dark theme (percy)', async () => {
-    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
-    await page.evaluate(() => {
-      document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'dark');
-    });
-    await page.waitForTimeout(300); // approx. time for a theme switch to take effect
-    await percySnapshot(page, 'ids-listview-new-dark');
-  });
-
-  it('should not have visual regressions in new contrast theme (percy)', async () => {
-    await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
-    await page.evaluate(() => {
-      document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'contrast');
-    });
-    await page.waitForTimeout(300); // approx. time for a theme switch to take effect
-    await percySnapshot(page, 'ids-listview-new-contrast');
-  });
 });
