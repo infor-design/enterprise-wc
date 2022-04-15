18a858246ad77f8896b1e9e874e40c78b85e72c6
Tim McConechy <tim.mcconechy@infor.com>
2022-04-15 10:12:10 -0400

    Fix standalone example

--- scripts/append-stylesheets.ts
+++ scripts/append-stylesheets.ts
@@ -1,3 +1,11 @@
+import cssVars from '../src/core/ids-element.scss';
+
+const style = document.createElement('style');
+style.textContent = cssVars.replace(':host {', ':root {');
+style.id = 'ids-styles';
+const doc = (document.head as any);
+doc.appendChild(style);
+
 export const appendStyleSheets = (...styles: string[]) => {
   let sheet = styles.join('\n');
   sheet = sheet.replaceAll(':host {', ':root {');
