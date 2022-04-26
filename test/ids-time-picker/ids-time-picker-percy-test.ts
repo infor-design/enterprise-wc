2d13d9de716d5524fcbebb74ef6f9fccf6f5bf3a
Tim McConechy <tim.mcconechy@infor.com>
2022-04-26 12:32:58 -0400

    Adjust the timepicker test

--- test/ids-time-picker/ids-time-picker-percy-test.ts
+++ test/ids-time-picker/ids-time-picker-percy-test.ts
@@ -1,10 +1,14 @@
 import percySnapshot from '@percy/puppeteer';
 
 describe('Ids Time Picker Percy Tests', () => {
-  const url = 'http://localhost:4444/ids-time-picker';
+  const url = 'http://localhost:4444/ids-time-picker/open.html';
 
   it('should not have visual regressions in new light theme (percy)', async () => {
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
+    await page.evaluate(() => {
+      (document.querySelector('#e2e-timepicker-locale') as any).toggleTimePopup();
+    });
+
     await percySnapshot(page, 'ids-time-picker-new-light');
   });
 
@@ -12,6 +16,7 @@ describe('Ids Time Picker Percy Tests', () => {
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'dark');
+      (document.querySelector('#e2e-timepicker-locale') as any).toggleTimePopup();
     });
     await page.waitForTimeout(222);
     await percySnapshot(page, 'ids-time-picker-new-dark');
@@ -21,6 +26,7 @@ describe('Ids Time Picker Percy Tests', () => {
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'contrast');
+      (document.querySelector('#e2e-timepicker-locale') as any).toggleTimePopup();
     });
     await page.waitForTimeout(222);
     await percySnapshot(page, 'ids-time-picker-new-contrast');
