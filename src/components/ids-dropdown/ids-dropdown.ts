d1a2b63384d77cfb82d815107b283b8d81bd07bc
Deepak Verma <deepak.verma@infor.com>
2022-04-28 12:04:40 -0400

    Fix input refrance

--- src/components/ids-dropdown/ids-dropdown.ts
+++ src/components/ids-dropdown/ids-dropdown.ts
@@ -42,7 +42,7 @@ export default class IdsDropdown extends Base {
     this.popup = this.container.querySelector('ids-popup');
     this.trigger = this.container.querySelector('ids-trigger-button');
     this.listBox = this.querySelector('ids-list-box');
-    this.labelEl = this.input?.querySelector('label');
+    this.labelEl = this.input?.labelEl;
 
     this
       .#addAria()
@@ -541,7 +541,7 @@ export default class IdsDropdown extends Base {
     // Should not open if clicked on label
     this.onEvent('click', this.labelEl, (e: MouseEvent) => {
       e.preventDefault();
-      this.container.focus();
+      this.input.focus();
     });
 
     // Disable text selection on tab (extra info in the screen reader)
