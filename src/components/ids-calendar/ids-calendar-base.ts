e670712a941e651f1a426c7bd541af5c8ed6066c
jdilag <jdilag.dev@gmail.com>
2022-05-05 17:28:34 -0400

    remove calendar event

--- src/components/ids-calendar/ids-calendar-base.ts
+++
@@ -1,6 +0,0 @@
-import IdsElement from '../../core/ids-element';
-import IdsLocaleMixin from '../../mixins/ids-locale-mixin/ids-locale-mixin';
-
-const Base = IdsLocaleMixin(IdsElement);
-
-export default Base;
