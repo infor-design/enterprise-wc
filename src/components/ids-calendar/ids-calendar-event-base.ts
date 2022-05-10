625b8415544b3382f0bc5e02c284eaedeb2c84e6
jdilag <jdilag.dev@gmail.com>
2022-05-10 10:40:43 -0400

    add icons, fix border colors, remove timeline

--- src/components/ids-calendar/ids-calendar-event-base.ts
+++ src/components/ids-calendar/ids-calendar-event-base.ts
@@ -1,6 +1,11 @@
 import IdsElement from '../../core/ids-element';
 import IdsLocaleMixin from '../../mixins/ids-locale-mixin/ids-locale-mixin';
+import IdsThemeMixin from '../../mixins/ids-theme-mixin/ids-theme-mixin';
 
-const Base = IdsLocaleMixin(IdsElement);
+const Base = IdsThemeMixin(
+  IdsLocaleMixin(
+    IdsElement
+  )
+);
 
 export default Base;
