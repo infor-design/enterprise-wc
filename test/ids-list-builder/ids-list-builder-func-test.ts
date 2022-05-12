c4485e84a02b56e08047812e1f640dada48a2db6
Jeffrey <jeff.wa428@gmail.com>
2022-05-12 08:52:50 -0500

    660 - Improve test coverage from 2x to 9x

--- test/ids-list-builder/ids-list-builder-func-test.ts
+++ test/ids-list-builder/ids-list-builder-func-test.ts
@@ -52,14 +52,20 @@ const sampleData = [
   },
 ];
 
+const sampleData1 = [
+  { manufacturerName: 'name1' },
+  { manufacturerName: 'name2' },
+  { manufacturerName: 'name3' }
+];
+
 const HTMLSnippets = {
   VANILLA_COMPONENT: (
-    `<ids-list-builder>
+    `<ids-list-builder selectable="single">
    </ids-list-builder>`
   ),
   FULL_COMPONENT: (
     // eslint-disable-next-line no-template-curly-in-string
-    '<ids-list-builder height="310px"><template><ids-text font-size="16" type="span">${manufacturerName}</ids-text></template></ids-list-builder>'
+    '<ids-list-builder selectable="single" height="310px"><template><ids-text font-size="16" type="span">${manufacturerName}</ids-text></template></ids-list-builder>'
   ),
 };
 
@@ -76,6 +82,11 @@ describe('IdsListBuilder Component', () => {
     document.body.appendChild(idsListBuilder);
 
     return idsListBuilder;
+  };
+
+  const createKeyboardEvent = (keyName: any, type = 'keydown') => {
+    const event = new KeyboardEvent(type, { key: keyName });
+    return event;
   };
 
   afterEach(async () => {
@@ -130,6 +141,197 @@ describe('IdsListBuilder Component', () => {
   it('renders the header', async () => {
     idsListBuilder = await createElemViaTemplate(HTMLSnippets.FULL_COMPONENT);
     expect(idsListBuilder.container.querySelector('#button-add')).toBeTruthy();
+  });
+
+  it('add new list item to non-empty list', async () => {
+    idsListBuilder = await createElemViaTemplate(HTMLSnippets.FULL_COMPONENT);
+    idsListBuilder.data = sampleData1;
+    const clickEvent = new MouseEvent('click');
+
+    // click add button
+    const addBtnElem = idsListBuilder.container.querySelector('#button-add');
+    addBtnElem.dispatchEvent(clickEvent);
+
+    const listItems = idsListBuilder.container.querySelectorAll('div[part=list-item]');
+    expect(listItems.length).toEqual(sampleData1.length + 1);
+
+    const itemInputElem = listItems[0].querySelector('ids-input');
+    expect(itemInputElem).toBeTruthy();
+    expect(itemInputElem.value).toEqual('New Value');
+  });
+
+  it('add new list item to empty-list', async () => {
+    idsListBuilder = await createElemViaTemplate(HTMLSnippets.FULL_COMPONENT);
+    idsListBuilder.data = [];
+    const clickEvent = new MouseEvent('click');
+
+    // click add button
+    const addBtnElem = idsListBuilder.container.querySelector('#button-add');
+    addBtnElem.dispatchEvent(clickEvent);
+
+    const listItems = idsListBuilder.container.querySelectorAll('div[part=list-item]');
+    expect(listItems.length).toEqual(1);
+
+    const itemInputElem = listItems[0].querySelector('ids-input');
+    expect(itemInputElem).toBeTruthy();
+    expect(itemInputElem.value).toEqual('New Value');
+  });
+
+  it('edit new list item', async () => {
+    idsListBuilder = await createElemViaTemplate(HTMLSnippets.FULL_COMPONENT);
+    idsListBuilder.data = sampleData1;
+    const clickEvent = new MouseEvent('click');
+
+    // click edit button
+    const editBtnElem = idsListBuilder.container.querySelector('#button-edit');
+    const listItems = idsListBuilder.container.querySelectorAll('div[part=list-item]');
+
+    // select first item and edit
+    idsListBuilder.onClick(listItems[0]);
+    editBtnElem.dispatchEvent(clickEvent);
+
+    // check editor element and value
+    const itemInputElem = listItems[0].querySelector('ids-input');
+    expect(itemInputElem).toBeTruthy();
+    expect(itemInputElem.value).toEqual(sampleData1[0].manufacturerName);
+  });
+
+  it('remove list item', async () => {
+    idsListBuilder = await createElemViaTemplate(HTMLSnippets.FULL_COMPONENT);
+    idsListBuilder.data = sampleData1;
+    const clickEvent = new MouseEvent('click');
+
+    const removeBtnElem = idsListBuilder.container.querySelector('#button-delete');
+    let listItems = idsListBuilder.container.querySelectorAll('div[part=list-item]');
+    expect(listItems.length).toEqual(sampleData1.length);
+
+    // click remove button without selection
+    removeBtnElem.dispatchEvent(clickEvent);
+    expect(listItems.length).toEqual(sampleData1.length);
+
+    // remove first list item
+    idsListBuilder.onClick(listItems[0]);
+    removeBtnElem.dispatchEvent(clickEvent);
+
+    listItems = idsListBuilder.container.querySelectorAll('div[part=list-item]');
+    expect(listItems.length).toEqual(sampleData1.length - 1);
+  });
+
+  it('move up list item', async () => {
+    idsListBuilder = await createElemViaTemplate(HTMLSnippets.FULL_COMPONENT);
+    idsListBuilder.data = sampleData1;
+    const clickEvent = new MouseEvent('click');
+
+    const moveUpBtnElem = idsListBuilder.container.querySelector('#button-up');
+    const listItems = idsListBuilder.container.querySelectorAll('div[part=list-item]');
+    expect(listItems.length).toEqual(sampleData1.length);
+
+    // click move up button without selection
+    moveUpBtnElem.dispatchEvent(clickEvent);
+    expect(idsListBuilder.data[0].manufacturerName).toBe('name1');
+
+    // remove first list item
+    idsListBuilder.onClick(listItems[1]);
+    moveUpBtnElem.dispatchEvent(clickEvent);
+
+    expect(idsListBuilder.data[0].manufacturerName).toBe(sampleData1[1].manufacturerName);
+    expect(idsListBuilder.data[1].manufacturerName).toBe(sampleData1[0].manufacturerName);
+  });
+
+  it('move down list item', async () => {
+    idsListBuilder = await createElemViaTemplate(HTMLSnippets.FULL_COMPONENT);
+    idsListBuilder.data = sampleData1;
+    const clickEvent = new MouseEvent('click');
+
+    const moveDownBtnElem = idsListBuilder.container.querySelector('#button-down');
+    const listItems = idsListBuilder.container.querySelectorAll('div[part=list-item]');
+    expect(listItems.length).toEqual(sampleData1.length);
+
+    // click move up button without selection
+    moveDownBtnElem.dispatchEvent(clickEvent);
+    expect(idsListBuilder.data[0].manufacturerName).toBe('name1');
+
+    // remove first list item
+    idsListBuilder.onClick(listItems[0]);
+    moveDownBtnElem.dispatchEvent(clickEvent);
+
+    expect(idsListBuilder.data[0].manufacturerName).toBe(sampleData1[1].manufacturerName);
+    expect(idsListBuilder.data[1].manufacturerName).toBe(sampleData1[0].manufacturerName);
+  });
+
+  it('keyboard support for select/toggle/arrow up/arrow down/delete', async () => {
+    idsListBuilder = await createElemViaTemplate(HTMLSnippets.FULL_COMPONENT);
+    idsListBuilder.data = sampleData1;
+
+    const listItems = idsListBuilder.container.querySelectorAll('div[part=list-item]');
+    idsListBuilder.onClick(listItems[0]);
+
+    // toggle editor
+    listItems[0].dispatchEvent(createKeyboardEvent('Enter'));
+    let itemInputElem = listItems[0].querySelector('ids-input');
+    expect(itemInputElem).toBeTruthy();
+    expect(itemInputElem.value).toEqual(sampleData1[0].manufacturerName);
+
+    listItems[0].dispatchEvent(createKeyboardEvent('Enter'));
+    itemInputElem = listItems[0].querySelector('ids-input');
+    expect(itemInputElem).toBeFalsy();
+
+    // arrow down
+    idsListBuilder.dispatchEvent(createKeyboardEvent('ArrowDown'));
+    expect(listItems[0].getAttribute('tabindex')).toBe('-1');
+    expect(listItems[1].getAttribute('tabindex')).toBe('0');
+    expect(listItems[2].getAttribute('tabindex')).toBe('-1');
+
+    idsListBuilder.dispatchEvent(createKeyboardEvent('ArrowDown'));
+    expect(listItems[0].getAttribute('tabindex')).toBe('-1');
+    expect(listItems[1].getAttribute('tabindex')).toBe('-1');
+    expect(listItems[2].getAttribute('tabindex')).toBe('0');
+
+    listItems[2].dispatchEvent(createKeyboardEvent(' '));
+    expect(idsListBuilder.selectedLi.getAttribute('index')).toBe('2');
+
+    // arrow up
+    idsListBuilder.dispatchEvent(createKeyboardEvent('ArrowUp'));
+    expect(listItems[0].getAttribute('tabindex')).toBe('-1');
+    expect(listItems[1].getAttribute('tabindex')).toBe('0');
+    expect(listItems[2].getAttribute('tabindex')).toBe('-1');
+
+    listItems[1].dispatchEvent(createKeyboardEvent(' '));
+    expect(idsListBuilder.selectedLi.getAttribute('index')).toBe('1');
+
+    // for item level keyboard
+    const keys = ['ArrowDown', 'ArrowUp', 'Tab'];
+    for (let i = 0; i < keys.length; i++) {
+      idsListBuilder.onClick(listItems[0]);
+      listItems[0].dispatchEvent(createKeyboardEvent('Enter'));
+      listItems[0].dispatchEvent(createKeyboardEvent(keys[i]));
+      expect(listItems[0].querySelector('ids-input')).toBeFalsy();
+    }
+
+    listItems[0].dispatchEvent(createKeyboardEvent('Delete'));
+    expect(idsListBuilder.container.querySelectorAll('div[part=list-item]')).toHaveLength(2);
+  });
+
+  it('update list item editor value', async () => {
+    idsListBuilder = await createElemViaTemplate(HTMLSnippets.FULL_COMPONENT);
+    idsListBuilder.data = [];
+    const clickEvent = new MouseEvent('click');
+
+    // click add button
+    const addBtnElem = idsListBuilder.container.querySelector('#button-add');
+    addBtnElem.dispatchEvent(clickEvent);
+
+    const listItems = idsListBuilder.container.querySelectorAll('div[part=list-item]');
+    expect(listItems.length).toEqual(1);
+
+    const itemInputElem = listItems[0].querySelector('ids-input');
+    expect(itemInputElem).toBeTruthy();
+    expect(itemInputElem.value).toEqual('New Value');
+
+    itemInputElem.value = 'test value';
+    itemInputElem.dispatchEvent(createKeyboardEvent('e', 'keyup'));
+    listItems[0].dispatchEvent(createKeyboardEvent('Enter'));
+    expect(idsListBuilder.data[0].manufacturerName).toEqual('test value');
   });
 
   // TODO: Errors are thrown when the button is clicked for no items
