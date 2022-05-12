e2257605686fe9c08e6a04b807243c306a11acda
Tim McConechy <tim.mcconechy@infor.com>
2022-05-12 13:58:49 -0400

    Fix some  wrong text

--- src/components/ids-accordion/demos/index.ts
+++ src/components/ids-accordion/demos/index.ts
@@ -3,3 +3,4 @@ import '../ids-accordion';
 import '../../ids-icon/ids-icon';
 import '../../ids-block-grid/ids-block-grid';
 import '../../ids-card/ids-card';
+import '../../ids-data-grid/ids-data-grid';
