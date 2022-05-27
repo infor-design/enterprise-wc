67100a1af6f66a3c7805a105fbf3e2e6185a0b25
Ed Coyle <ed.coyle@infor.com>
2022-05-27 10:40:04 -0400

    Add/fix missing tabs visual regression tests

--- test/ids-tabs/ids-tabs-header-percy-test.ts
+++ test/ids-tabs/ids-tabs-header-percy-test.ts
@@ -1,17 +1,12 @@
 import percySnapshot from '@percy/puppeteer';
 
-describe('Ids Tabs Percy Tests', () => {
-  const url = 'http://localhost:4444/ids-tabs/example.html';
+describe('Ids Tabs Percy Tests (Header Tabs)', () => {
+  const url = 'http://localhost:4444/ids-tabs/header-tabs.html';
 
   it('should not have visual regressions in new light theme (percy)', async () => {
     await page.goto(url, { waitUntil: ['networkidle0', 'domcontentloaded'] });
     await page.waitForTimeout(60);
-    await percySnapshot(page, 'ids-tabs-new-light');
-  });
-
-  it('should not have visual regressions in standalone css', async () => {
-    await page.goto('http://localhost:4444/ids-tabs/standalone-css.html', { waitUntil: ['networkidle2', 'load'] });
-    await percySnapshot(page, 'ids-tabs-standalone-css', { widths: [960] });
+    await percySnapshot(page, 'ids-tabs-header-new-light');
   });
 
   it('should not have visual regressions in new dark theme (percy)', async () => {
@@ -20,7 +15,7 @@ describe('Ids Tabs Percy Tests', () => {
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'dark');
     });
-    await percySnapshot(page, 'ids-tabs-new-dark');
+    await percySnapshot(page, 'ids-tabs-header-new-dark');
   });
 
   it('should not have visual regressions in new contrast theme (percy)', async () => {
@@ -29,6 +24,6 @@ describe('Ids Tabs Percy Tests', () => {
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'contrast');
     });
-    await percySnapshot(page, 'ids-tabs-new-contrast');
+    await percySnapshot(page, 'ids-tabs-header-new-contrast');
   });
 });
--- test/ids-tabs/ids-tabs-vertical-percy-test.ts
+++ test/ids-tabs/ids-tabs-vertical-percy-test.ts
@@ -1,17 +1,12 @@
 import percySnapshot from '@percy/puppeteer';
 
-describe('Ids Tabs Percy Tests', () => {
-  const url = 'http://localhost:4444/ids-tabs/example.html';
+describe('Ids Tabs Percy Tests (Vertical Tabs)', () => {
+  const url = 'http://localhost:4444/ids-tabs/vertical.html';
 
   it('should not have visual regressions in new light theme (percy)', async () => {
     await page.goto(url, { waitUntil: ['networkidle0', 'domcontentloaded'] });
     await page.waitForTimeout(60);
-    await percySnapshot(page, 'ids-tabs-new-light');
-  });
-
-  it('should not have visual regressions in standalone css', async () => {
-    await page.goto('http://localhost:4444/ids-tabs/standalone-css.html', { waitUntil: ['networkidle2', 'load'] });
-    await percySnapshot(page, 'ids-tabs-standalone-css', { widths: [960] });
+    await percySnapshot(page, 'ids-tabs-vertical-new-light');
   });
 
   it('should not have visual regressions in new dark theme (percy)', async () => {
@@ -20,7 +15,7 @@ describe('Ids Tabs Percy Tests', () => {
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'dark');
     });
-    await percySnapshot(page, 'ids-tabs-new-dark');
+    await percySnapshot(page, 'ids-tabs-vertical-new-dark');
   });
 
   it('should not have visual regressions in new contrast theme (percy)', async () => {
@@ -29,6 +24,6 @@ describe('Ids Tabs Percy Tests', () => {
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'contrast');
     });
-    await percySnapshot(page, 'ids-tabs-new-contrast');
+    await percySnapshot(page, 'ids-tabs-vertical-new-contrast');
   });
 });
