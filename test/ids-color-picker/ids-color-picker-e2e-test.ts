f5cc7c4f7451616bbe0effd28f5e4def6402e3bc
Clay Clecidor <clay.clecidor@infor.com>
2022-06-07 10:45:04 -0400

    551: Fix IdsColorPicker and IdsColor remaining features (fix e2e tests)

--- test/ids-color-picker/ids-color-picker-e2e-test.ts
+++ test/ids-color-picker/ids-color-picker-e2e-test.ts
@@ -1,6 +1,6 @@
 describe('Ids Color Picker e2e Tests', () => {
-  const url = 'http://localhost:4444/ids-color-picker/example.html';
   const axeUrl = 'http://localhost:4444/ids-color-picker/axe.html';
+  const url = 'http://localhost:4444/ids-color-picker/example.html';
 
   beforeEach(async () => {
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
@@ -17,36 +17,36 @@ describe('Ids Color Picker e2e Tests', () => {
   });
 
   it('should open on clicking the swatch', async () => {
-    let isVisible = await page.evaluate(`document.querySelector("#ids-input-1").popup.visible`);
+    let isVisible = await page.evaluate(`document.querySelector("#color-picker-e2e-test").popup.visible`);
     expect(isVisible).toEqual(false);
-    await page.evaluate(`document.querySelector("#ids-input-1").colorPreview.click()`);
-    isVisible = await page.evaluate(`document.querySelector("#ids-input-1").popup.visible`);
+    await page.evaluate(`document.querySelector("#color-picker-e2e-test").colorPreview.click()`);
+    isVisible = await page.evaluate(`document.querySelector("#color-picker-e2e-test").popup.visible`);
     expect(isVisible).toEqual(true);
-    await page.evaluate(`document.querySelector("#ids-input-1").popup.visible = false`);
+    await page.evaluate(`document.querySelector("#color-picker-e2e-test").popup.visible = false`);
   });
 
   it('should select when clicking the hex', async () => {
-    const elem = await page.$('#ids-input-1');
+    const elem = await page.$('#color-picker-e2e-test');
     let value = await page.evaluate((el: any) => el.value, elem);
 
     expect(value).toEqual('');
     await page.evaluate((el: any) => {
       el.popup.visible = true;
-      (document.querySelector('#ids-input-1 > ids-color[hex]') as any).click();
+      (document.querySelector('#color-picker-e2e-test > ids-color[hex]') as any).click();
     }, elem);
     value = await page.evaluate((el: any) => el.value, elem);
     expect(value).toEqual('ruby-10');
   });
 
   it('should open when pressing the down arrow', async () => {
-    let isVisible = await page.evaluate(`document.querySelector("#ids-input-1").popup.visible`);
+    let isVisible = await page.evaluate(`document.querySelector("#color-picker-e2e-test").popup.visible`);
     expect(isVisible).toEqual(false);
 
-    const input = await page.evaluateHandle('document.querySelector("#ids-input-1")');
+    const input = await page.evaluateHandle('document.querySelector("#color-picker-e2e-test")');
     await input.focus();
     await page.keyboard.press('ArrowDown');
 
-    isVisible = await page.evaluate(`document.querySelector("#ids-input-1").popup.visible`);
+    isVisible = await page.evaluate(`document.querySelector("#color-picker-e2e-test").popup.visible`);
     expect(isVisible).toEqual(true);
   });
 });
