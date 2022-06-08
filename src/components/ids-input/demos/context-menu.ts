5b98c8b268b1751b73f41979226b7475f96a8d4d
Jeffrey <jeff.wa428@gmail.com>
2022-06-07 21:02:14 -0500

    655 - Add context menu on input example

---
+++ src/components/ids-input/demos/context-menu.ts
@@ -0,0 +1,28 @@
+// Supporting components
+import '../../ids-popup-menu/ids-popup-menu';
+import '../../ids-popup/ids-popup';
+import json from '../../../assets/data/menu-contents.json';
+
+// Example for populating the Popup Menu
+const popupmenuEl: any = document.querySelector('#popupmenu');
+if (popupmenuEl) {
+  const popupEl = popupmenuEl.popup;
+
+  // Standard menu configuration
+  document.addEventListener('DOMContentLoaded', () => {
+    popupmenuEl.addEventListener('selected', (e: any) => {
+      console.info(`Item "${e.detail.elem.text}" was selected`);
+    });
+  });
+
+  const url: any = json;
+  popupEl.align = 'top, left';
+
+  const setData = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    popupmenuEl.data = data;
+  };
+
+  setData();
+}
