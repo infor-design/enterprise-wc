7963300f0171bcd530a1df618efdeb50e88e4203
Andrii Varhanov <andrii.varhanov@gmail.com>
2022-06-17 11:15:41 +0300

    Make percy screenshots consistant for all themes #636

--- test/ids-time-picker/ids-time-picker-percy-test.ts
+++ test/ids-time-picker/ids-time-picker-percy-test.ts
@@ -5,7 +5,9 @@ describe('Ids Time Picker Percy Tests', () => {
 
   it('should not have visual regressions in new light theme (percy)', async () => {
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
-    await page.waitForTimeout(200);
+    await page.evaluate(() => {
+      document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'light');
+    });
     await percySnapshot(page, 'ids-time-picker-new-light');
   });
 
@@ -14,7 +16,6 @@ describe('Ids Time Picker Percy Tests', () => {
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'dark');
     });
-    await page.waitForTimeout(200);
     await percySnapshot(page, 'ids-time-picker-new-dark');
   });
 
@@ -23,7 +24,6 @@ describe('Ids Time Picker Percy Tests', () => {
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'contrast');
     });
-    await page.waitForTimeout(200);
     await percySnapshot(page, 'ids-time-picker-new-contrast');
   });
 });
