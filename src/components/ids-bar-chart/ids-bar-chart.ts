677d75fc2b4cfcd737241e0ee49a2b05aac4f510
Deepak Verma <deepak.verma@infor.com>
2022-06-28 11:08:16 -0400

    Add selection feature with charts

--- src/components/ids-bar-chart/ids-bar-chart.ts
+++ src/components/ids-bar-chart/ids-bar-chart.ts
@@ -1,8 +1,22 @@
 import { customElement, scss } from '../../core/ids-decorators';
 import { attributes } from '../../core/ids-attributes';
+import { stringToNumber } from '../../utils/ids-string-utils/ids-string-utils';
 import Base from './ids-bar-chart-base';
 import styles from './ids-bar-chart.scss';
 import type IdsChartData from '../ids-axis-chart/ids-axis-chart';
+
+type IdsBarChartSelected = {
+  data?: any,
+  groupIndex?: number | string,
+  index?: number | string,
+  bars?: any[],
+  indexes?: any[]
+};
+
+type IdsBarChartSelectedBy = {
+  groupIndex?: number | string,
+  index?: number | string
+};
 
 /**
  * IDS Bar Chart Component
@@ -19,6 +33,9 @@ import type IdsChartData from '../ids-axis-chart/ids-axis-chart';
 export default class IdsBarChart extends Base {
   constructor() {
     super();
+
+    // Setup default values
+    this.DEFAULT_SELECTABLE = false;
   }
 
   /**
@@ -35,6 +52,7 @@ export default class IdsBarChart extends Base {
 
   rendered() {
     this.#adjustVerticalLines();
+    this.#preSelected();
     this.attachTooltipEvents();
   }
 
@@ -48,6 +66,184 @@ export default class IdsBarChart extends Base {
     return `<g class="bars" role="list" aria-label="${ariaLabel}">
       ${this.#bars()}
     </g>`;
+  }
+
+  /**
+   * Set initially selected
+   * @private
+   * @returns {void}
+   */
+  #preSelected(): void {
+    if (!this.initialized || !this.selectable || !this.data?.length || !this.selectionElements?.length) return;
+
+    const isValid = (n: any) => typeof n === 'number' && n > -1;
+    const removeSelectedInData = () => {
+      this.data.forEach((g: any) => {
+        delete g.selected;
+        g.data?.forEach((n: any) => { delete n?.selected; });
+      });
+    };
+    const gIndex = this.data.findIndex((n: any) => n.selected);
+    if (isValid(gIndex)) {
+      removeSelectedInData();
+      (this.data as any)[gIndex].data?.forEach((n: any) => { n.selected = true; });
+      this.selectionElements.forEach((el: SVGElement) => {
+        if (el.getAttribute('group-index') === `${gIndex}`) el.setAttribute('selected', '');
+        else el.classList.add('not-selected');
+      });
+      return;
+    }
+
+    let index: any;
+    for (let i = 0, l = this.data.length; i < l; i++) {
+      index = (this.data as any)[i].data?.findIndex((n: any) => n.selected);
+      if (isValid(index)) break;
+    }
+    if (isValid(index)) {
+      removeSelectedInData();
+      this.data.forEach((g: any) => {
+        const d = g?.data?.[index];
+        if (d) d.selected = true;
+      });
+      this.selectionElements.forEach((el: SVGElement) => {
+        if (el.getAttribute('index') === `${index}`) el.setAttribute('selected', '');
+        else el.classList.add('not-selected');
+      });
+    }
+  }
+
+  /**
+   * Get currently selected
+   * @returns {IdsBarChartSelected} selected.
+   */
+  getSelected(): IdsBarChartSelected {
+    const selected: any = this.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    if (!selected.length) return {};
+
+    const groupIndex: any = selected[0].getAttribute('group-index');
+    const isGroup = !(selected.some((el: SVGElement) => el.getAttribute('group-index') !== groupIndex));
+    if (isGroup) {
+      const data: any = (this.data as any)[groupIndex];
+      return { bars: selected, data, groupIndex };
+    }
+    const data: any[] = [];
+    const indexes = selected.map((el: SVGElement) => {
+      const g: any = el.getAttribute('group-index');
+      const i: any = el.getAttribute('index');
+      const d: any = (this.data as any)[g].data[i];
+      if (d) data.push(d);
+      return { group: g, index: i };
+    });
+    return { bars: selected, data, indexes };
+  }
+
+  /**
+   * Set selected by give indexes
+   * @param {IdsBarChartSelectedBy} opt The in comeing options
+   * @returns {void}
+   */
+  setSelected(opt: IdsBarChartSelectedBy): void {
+    if (!this.initialized || !this.selectable || !this.data?.length || !this.selectionElements?.length) return;
+
+    const gIndex = stringToNumber(opt.groupIndex);
+    const index = stringToNumber(opt.index);
+    if (Number.isNaN(gIndex)) return;
+    if (Number.isNaN(index)) this.setSelection(gIndex, true);
+    else {
+      const idx = this.selectionElements.findIndex(
+        (el: SVGElement) => el.getAttribute('group-index') === `${gIndex}`
+          && el.getAttribute('index') === `${index}`
+      );
+      if (typeof idx === 'number' && idx > -1) this.setSelection(idx);
+    }
+  }
+
+  /**
+   * Set the selection for given index
+   * @private
+   * @param {number|string} index The index value
+   * @param {boolean} isLegendClick True if legend clicked
+   * @returns {boolean} False, if veto.
+   */
+  setSelection(index: any, isLegendClick?: boolean): boolean {
+    if (Number.isNaN(index) || !this.selectionElements?.[0]) return false;
+
+    // Set type of bar elements
+    const extract = (cb: any) => this.selectionElements.filter((a: SVGElement) => cb(a));
+    const bars: any = { selected: extract((el: SVGElement) => el.hasAttribute('selected')) };
+    if (isLegendClick) {
+      bars.target = extract((el: SVGElement) => el.getAttribute('group-index') === `${index}`);
+      bars.inverse = extract((el: SVGElement) => el.getAttribute('group-index') !== `${index}`);
+    } else {
+      const idx = this.selectionElements?.[index]?.getAttribute('index');
+      bars.target = extract((el: SVGElement) => el.getAttribute('index') === `${idx}`);
+      bars.inverse = extract((el: SVGElement) => el.getAttribute('index') !== `${idx}`);
+    }
+
+    // Get data and indexes for given list of elements
+    let isGroup = false;
+    const args = (list: any[]) => {
+      const groupIndex: any = list[0].getAttribute('group-index');
+      isGroup = !(list.some((el: SVGElement) => el.getAttribute('group-index') !== groupIndex));
+      if (isGroup) {
+        const data: any = (this.data as any)[groupIndex];
+        return { bars: list, data, groupIndex };
+      }
+      const data: any[] = [];
+      const indexes = list.map((el: SVGElement) => {
+        const g: any = el.getAttribute('group-index');
+        const i: any = el.getAttribute('index');
+        const d: any = (this.data as any)[g].data[i];
+        if (d) data.push(d);
+        return { group: g, index: i };
+      });
+      return { bars: list, data, indexes };
+    };
+
+    // Deselect
+    const deselect = (): boolean => {
+      const argsSelected = args(bars.selected);
+      if (!this.triggerVetoableEvent('beforedeselected', argsSelected)) {
+        return false;
+      }
+      const unmarkSelected = (n: any) => { delete n.selected; };
+      if (isGroup) unmarkSelected(argsSelected.data);
+      else argsSelected.data.forEach((n: any) => unmarkSelected(n));
+      this.selectionElements.forEach((el: SVGElement) => el.classList.remove('not-selected'));
+      bars.selected.forEach((el: SVGElement) => el.removeAttribute('selected'));
+      this.triggerEvent('deselected', this, { bubbles: true, detail: { elem: this, ...argsSelected } });
+      return true;
+    };
+
+    // Previously selected
+    if (bars.selected.length) {
+      const targetSelected = !(bars.target.some((el: SVGElement) => !el.hasAttribute('selected')));
+      if (targetSelected) return deselect();
+      if (!deselect()) return false;
+    }
+
+    // Traget data and indexes
+    const argsTarget = args(bars.target);
+    if (!this.triggerVetoableEvent('beforeselected', argsTarget)) {
+      return false;
+    }
+    const markSelected = (n: any) => { n.selected = true; };
+    if (isGroup) markSelected(argsTarget.data);
+    else argsTarget.data.forEach((n: any) => markSelected(n));
+    bars.target.forEach((el: SVGElement) => el.setAttribute('selected', ''));
+    bars.inverse.forEach((el: SVGElement) => el.classList.add('not-selected'));
+    this.triggerEvent('selected', this, { bubbles: true, detail: { elem: this, ...argsTarget } });
+
+    return true;
+  }
+
+  /**
+   * Return chart elements that get selection
+   * @returns {Array<SVGElement>} The elements
+   */
+  get selectionElements(): Array<SVGElement> {
+    if (!this.selectable) return [];
+    return [...this.container.querySelectorAll('rect.bar')];
   }
 
   /**
@@ -115,7 +311,7 @@ export default class IdsBarChart extends Base {
 
         barHTML += `<g role="listitem">
           <text class="audible" x="${left}" y="${this.markerData.gridBottom}">${label} ${point.value}</text>
-          <rect class="bar color-${groupIndex + 1}" aria-hidden="true" index="${index}" width="${this.barWidth}" height="${height}" x="${left}" y="${top}"${pattern}>
+          <rect class="bar color-${groupIndex + 1}" aria-hidden="true" group-index="${groupIndex}" index="${index}" width="${this.barWidth}" height="${height}" x="${left}" y="${top}"${pattern}>
             <animate attributeName="height" from="0" to="${height}" ${this.animated ? this.cubicBezier : this.cubicBezier.replace('0.8s', '0.01s')}></animate>
             <animate attributeName="y" from="${bottom}" to="${top}" ${this.animated ? this.cubicBezier : this.cubicBezier.replace('0.8s', '0.01s')}></animate>
           </rect></g>`;
--- test/ids-pie-chart/ids-pie-chart-func-test.ts
+++ test/ids-pie-chart/ids-pie-chart-func-test.ts
@@ -4,6 +4,7 @@
 import IdsPieChart from '../../src/components/ids-pie-chart/ids-pie-chart';
 import dataset from '../../src/assets/data/items-single.json';
 import IdsContainer from '../../src/components/ids-container/ids-container';
+import { deepClone } from '../../src/utils/ids-deep-clone-utils/ids-deep-clone-utils';
 import '../helpers/resize-observer-mock';
 
 describe('IdsPieChart Component', () => {
@@ -266,5 +267,138 @@ describe('IdsPieChart Component', () => {
     expect(pieChart.title).toBe('Test Title');
     expect(pieChart.shadowRoot.querySelectorAll('title')[0].textContent).toBe('');
     expect(pieChart.shadowRoot.querySelectorAll('title')[1].textContent).toBe('Test Title');
+  });
+
+  it('should set selectable', () => {
+    expect(pieChart.selectable).toEqual(false);
+    expect(pieChart.getAttribute('selectable')).toEqual(null);
+    pieChart.selectable = true;
+    expect(pieChart.selectable).toEqual(true);
+    expect(pieChart.getAttribute('selectable')).toEqual('');
+    pieChart.selectable = false;
+    expect(pieChart.selectable).toEqual(false);
+    expect(pieChart.getAttribute('selectable')).toEqual(null);
+  });
+
+  it('should select/deselect by click', () => {
+    expect(pieChart.selectionElements.length).toEqual(0);
+    expect(pieChart.setSelection()).toEqual(false);
+    const triggerClick = (el: any) => el.dispatchEvent(new Event('click', { bubbles: true }));
+    pieChart.selectable = true;
+    let selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(0);
+
+    let elem = pieChart.shadowRoot.querySelector('.chart-legend-item:nth-child(1)');
+    triggerClick(elem);
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(1);
+    elem = pieChart.shadowRoot.querySelector('.chart-legend-item:nth-child(1)');
+    triggerClick(elem);
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(0);
+
+    elem = pieChart.shadowRoot.querySelector('.slice[index="2"]');
+    triggerClick(elem);
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(1);
+    elem = pieChart.shadowRoot.querySelector('.slice[index="0"]');
+    triggerClick(elem);
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(1);
+    elem = pieChart.shadowRoot.querySelector('.slice[index="0"]');
+    triggerClick(elem);
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(0);
+  });
+
+  it('should set pre selected elements', () => {
+    document.body.innerHTML = '';
+    const ds = deepClone(dataset);
+    (ds as any)[0].data[2].selected = true;
+
+    container = new IdsContainer();
+    pieChart = new IdsPieChart();
+    pieChart.selectable = true;
+    pieChart.animated = false;
+    document.body.appendChild(container);
+    container.appendChild(pieChart);
+    pieChart.data = ds;
+
+    const selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(1);
+  });
+
+  it('should veto before selected', async () => {
+    let veto: boolean;
+    pieChart.addEventListener('beforeselected', (e: CustomEvent) => {
+      e.detail.response(veto);
+    });
+    veto = false;
+    const triggerClick = (el: any) => el.dispatchEvent(new Event('click', { bubbles: true }));
+    pieChart.selectable = true;
+
+    let selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(0);
+
+    let elem = pieChart.shadowRoot.querySelector('.chart-legend-item:nth-child(1)');
+    triggerClick(elem);
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(0);
+    veto = true;
+    elem = pieChart.shadowRoot.querySelector('.chart-legend-item:nth-child(1)');
+    triggerClick(elem);
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(1);
+  });
+
+  it('should veto before deselected', async () => {
+    let veto: boolean;
+    pieChart.addEventListener('beforedeselected', (e: CustomEvent) => {
+      e.detail.response(veto);
+    });
+
+    const triggerClick = (el: any) => el.dispatchEvent(new Event('click', { bubbles: true }));
+    pieChart.selectable = true;
+
+    let selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(0);
+
+    let elem = pieChart.shadowRoot.querySelector('.chart-legend-item:nth-child(1)');
+    triggerClick(elem);
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(1);
+    veto = false;
+    elem = pieChart.shadowRoot.querySelector('.chart-legend-item:nth-child(1)');
+    triggerClick(elem);
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(1);
+    elem = pieChart.shadowRoot.querySelector('.chart-legend-item:nth-child(2)');
+    triggerClick(elem);
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(1);
+    veto = true;
+    elem = pieChart.shadowRoot.querySelector('.chart-legend-item:nth-child(1)');
+    triggerClick(elem);
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(0);
+  });
+
+  it('should get/set selected by api', async () => {
+    pieChart.setSelected();
+    let selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(0);
+    expect(pieChart.getSelected()).toEqual({});
+    pieChart.selectable = true;
+
+    pieChart.setSelected('test');
+    expect(pieChart.getSelected()).toEqual({});
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(0);
+    expect(pieChart.getSelected()).toEqual({});
+
+    pieChart.setSelected({ index: 0 });
+    selected = pieChart.selectionElements.filter((el: SVGElement) => el.hasAttribute('selected'));
+    expect(selected.length).toEqual(1);
+    expect(pieChart.getSelected().index).toEqual('0');
   });
 });
