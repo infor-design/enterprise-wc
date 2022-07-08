fae79522afa53da65f54369a28e1689083203e1b
Tim McConechy <tim.mcconechy@infor.com>
2022-07-08 09:48:40 -0400

    Attemptto stabilize some tests

--- src/components/ids-time-picker/demos/open.ts
+++ src/components/ids-time-picker/demos/open.ts
@@ -1,5 +1,1 @@
 import '../ids-time-picker';
-
-const timepickerOpen: any = document.querySelector('#e2e-timepicker-percy-axe');
-const event = new KeyboardEvent('keydown', { key: 'ArrowDown' });
-timepickerOpen?.dispatchEvent(event);
