b05428947994f12b8f6a054298710522f47b7708
danielortiz1982 <danielortiz1982@gmail.com>
2022-07-18 13:27:43 -0400

    fix lint errors

--- src/components/ids-form/ids-form.ts
+++ src/components/ids-form/ids-form.ts
@@ -16,14 +16,13 @@ import '../ids-editor/ids-editor';
 import '../ids-input/ids-input';
 import '../ids-fieldset/ids-fieldset';
 import '../ids-lookup/ids-lookup';
-import '../ids-trigger-field/ids-trigger-field.ts';
+import '../ids-trigger-field/ids-trigger-field';
 import '../ids-time-picker/ids-time-picker';
 import '../ids-date-picker/ids-date-picker';
 import '../ids-radio/ids-radio';
-import '../ids-upload/ids-upload.ts';
-import '../ids-upload-advanced/ids-upload-advanced.ts';
+import '../ids-upload/ids-upload';
+import '../ids-upload-advanced/ids-upload-advanced';
 import IdsElement from '../../core/ids-element';
-
 
 @customElement('ids-form')
 @scss(styles)
@@ -65,7 +64,7 @@ export default class IdsForm extends Base {
    * @returns {string} The template
    */
   template(): string {
-    return `<form><slot></slot></form>`
+    return `<form><slot></slot></form>`;
   }
 
   /**
@@ -74,8 +73,8 @@ export default class IdsForm extends Base {
    */
 
   set action(value: string) {
-    if(value) {
-      const form: HTMLElement[] = this.shadowRoot.childNodes
+    if (value) {
+      const form: HTMLElement[] = this.shadowRoot.childNodes;
       form[1].setAttribute('action', value);
       this.setAttribute('action', value);
     }
@@ -90,11 +89,11 @@ export default class IdsForm extends Base {
    * @param {boolean | string} value string value for compact
    */
   set compact(value: boolean | string) {
-    if(value) {
+    if (value) {
       this.setAttribute('compact', value);
       const idsFormComponents: IdsElement[] = this.getIdsFormComponents();
       [...idsFormComponents].forEach((el) => {
-        el.setAttribute('compact', value)
+        el.setAttribute('compact', value);
       });
     }
   }
@@ -102,14 +101,14 @@ export default class IdsForm extends Base {
   get compact(): boolean | string {
     return this.getAttribute('compact') || '';
   }
-  
+
   /**
    * Sets the id attribute
    * @param {number | string | any} value string value for id
    */
   set id(value: number | string | any) {
-    if(value) {
-      const form: HTMLElement[] = this.shadowRoot.childNodes
+    if (value) {
+      const form: HTMLElement[] = this.shadowRoot.childNodes;
       form[1].setAttribute('id', value);
       this.setAttribute('id', value);
     }
@@ -124,8 +123,8 @@ export default class IdsForm extends Base {
    * @param {string} value string value for method
    */
   set method(value: string) {
-    if(value) {
-      const form: HTMLElement[] = this.shadowRoot.childNodes
+    if (value) {
+      const form: HTMLElement[] = this.shadowRoot.childNodes;
       form[1].setAttribute('method', value);
       this.setAttribute('method', value);
     }
@@ -140,8 +139,8 @@ export default class IdsForm extends Base {
    * @param {string} value string value for name
    */
   set name(value: string) {
-    if(value) {
-      const form: HTMLElement[] = this.shadowRoot.childNodes
+    if (value) {
+      const form: HTMLElement[] = this.shadowRoot.childNodes;
       form[1].setAttribute('name', value);
       this.setAttribute('name', value);
     }
@@ -156,8 +155,8 @@ export default class IdsForm extends Base {
    * @param {string} value string value for target
    */
   set target(value: string) {
-    if(value) {
-      const form: HTMLElement[] = this.shadowRoot.childNodes
+    if (value) {
+      const form: HTMLElement[] = this.shadowRoot.childNodes;
       form[1].setAttribute('target', value);
       this.setAttribute('target', value);
     }
@@ -172,8 +171,8 @@ export default class IdsForm extends Base {
    * @param {string} value string value for title
    */
   set title(value: string) {
-    if(value) {
-      const form: HTMLElement[] = this.shadowRoot.childNodes
+    if (value) {
+      const form: HTMLElement[] = this.shadowRoot.childNodes;
       form[1].setAttribute('title', value);
       this.setAttribute('title', value);
     }
@@ -197,20 +196,19 @@ export default class IdsForm extends Base {
    * @private
    * @returns {IdsElement[]}
    */
-  
   #getIdsElements(): IdsElement[] {
-    let IdsElements: IdsElement[] = [];
+    const IdsElements: IdsElement[] = [];
     const idsForm: IdsForm = this;
     const findIdsElements = (el: IdsElement | any) => {
       if (el.hasChildNodes()) {
-        const formChildren = [...el.children]
+        const formChildren = [...el.children];
         formChildren.forEach((e: IdsElement) => {
           if (e.tagName.includes('IDS-')) {
             IdsElements.push(e);
-          };
+          }
           findIdsElements(e);
         });
-      };
+      }
     };
     findIdsElements(idsForm);
     return IdsElements;
@@ -225,8 +223,8 @@ export default class IdsForm extends Base {
   #submitIdsForm(): void {
     this.onEvent('click', this, () => {
       const idsFormEl: IdsElement[] = this.getIdsFormComponents();
-      const formValues: object[] = []
-      idsFormEl.forEach( el => formValues.push({name: el.nodeName, value: el.value}));
-    })
+      const formValues: object[] = [];
+      idsFormEl.forEach((el) => formValues.push({ name: el.nodeName, value: el.value }));
+    });
   }
 }
