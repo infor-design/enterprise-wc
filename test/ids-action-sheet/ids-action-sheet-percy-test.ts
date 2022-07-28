52484e7baa70441b6a102d970b8714248e214c2a
celhalal <cathyelhalal@gmail.com>
2022-07-28 15:48:56 -0400

    percy test

--- test/ids-action-sheet/ids-action-sheet-percy-test.ts
+++ test/ids-action-sheet/ids-action-sheet-percy-test.ts
@@ -4,15 +4,12 @@ describe('Ids Action Sheet Percy Tests', () => {
   const url = 'http://localhost:4444/ids-action-sheet/example.html';
 
   it('should not have visual regressions when cancel button is not visible', async () => {
+    await page.setViewport({ width: 450, height: 800 });
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
     await page.evaluate(() => {
       (document.querySelector('#icon-button') as any).click();
     });
     await page.waitForSelector('ids-action-sheet[visible]');
-    await page.evaluate(() => {
-      const idsButton = document.querySelector('ids-action-sheet')?.shadowRoot?.querySelector('ids-button') as HTMLElement;
-      idsButton.style.display = 'none';
-    });
     await percySnapshot(page, 'ids-action-sheet-example', { widths: [375] });
   });
 });
