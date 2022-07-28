6446e4c29b7af9a405080e688eea2a1e784c25f9
celhalal <cathyelhalal@gmail.com>
2022-07-28 16:39:52 -0400

    percy test

--- test/ids-action-sheet/ids-action-sheet-percy-test.ts
+++ test/ids-action-sheet/ids-action-sheet-percy-test.ts
@@ -10,6 +10,10 @@ describe('Ids Action Sheet Percy Tests', () => {
       (document.querySelector('#icon-button') as any).click();
     });
     await page.waitForSelector('ids-action-sheet[visible]');
+    await page.evaluate(() => {
+      const idsButton = document.querySelector('ids-action-sheet')?.shadowRoot?.querySelector('ids-button') as HTMLElement;
+      idsButton.style.display = 'none';
+    });
     await percySnapshot(page, 'ids-action-sheet-example', { widths: [375] });
   });
 });
