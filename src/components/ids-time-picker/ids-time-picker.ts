c8641e5504ab37311c7cb305e1dd8acb45af335b
celhalal <cathyelhalal@gmail.com>
2022-08-02 14:29:29 -0400

    updated paths for click event

--- src/components/ids-time-picker/ids-time-picker.ts
+++ src/components/ids-time-picker/ids-time-picker.ts
@@ -205,8 +205,9 @@ export default class IdsTimePicker extends Base {
    * @returns {void}
    */
   onOutsideClick(e: any): void {
-    if ((!this.autoselect && !e.composedPath()?.includes(this.popup))
-    || (this.autoselect && !e.composedPath()?.includes(this.popup) && !e.composedPath()?.includes(this.input))) {
+    const path = e.path || (e.composedPath && e.composedPath());
+    if ((!this.autoselect && !path?.includes(this.popup))
+    || (this.autoselect && !path?.includes(this.popup) && !path?.includes(this.input))) {
       this.close();
     }
   }
