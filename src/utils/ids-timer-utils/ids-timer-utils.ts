5d677f9e5886a3dca6de2b7e4fdfca0c5f1f8683
Ed Coyle <ed.coyle@infor.com>
2022-09-06 10:25:44 -0400

    Replace IdsRenderLoop usage in IdsWeekView with timer functions

--- src/utils/ids-timer-utils/ids-timer-utils.ts
+++ src/utils/ids-timer-utils/ids-timer-utils.ts
@@ -6,6 +6,45 @@ export type FrameRequestLoopHandler = {
 };
 
 /**
+ * Behaves similarly to `setInterval`, using `requestAnimationFrame()` where possible for better performance
+ * @param {FrameRequestCallback} fn The callback function
+ * @param {number} delay The delay in milliseconds
+ * @returns {number} loop handle
+ */
+export function requestAnimationInterval(fn: FrameRequestCallback, delay: number): FrameRequestLoopHandler {
+  if (!RAF) return { value: setTimeout(fn, delay) };
+
+  let start = new Date().getTime();
+  const handle: FrameRequestLoopHandler = {};
+
+  const loop = (): void => {
+    const current = new Date().getTime();
+    const delta = current - start;
+
+    if (delta >= delay) {
+      fn(current);
+      start = new Date().getTime();
+    }
+
+    handle.value = RAF(loop);
+  };
+
+  handle.value = RAF(loop);
+  return handle;
+}
+
+/**
+ * Behaves similarly to `clearInterval`, using `cancelAnimationFrame()` where possible for better performance
+ * @param {FrameRequestLoopHandler} handle The callback function
+ */
+export function clearAnimationInterval(handle: FrameRequestLoopHandler) {
+  if (handle?.value) {
+    if (!CAF) clearInterval(handle.value);
+    else CAF(handle.value);
+  }
+}
+
+/**
  * Behaves similarly to `setTimeout`, using `requestAnimationFrame()` where possible for better performance
  * @param {FrameRequestCallback} fn The callback function
  * @param {number} delay The delay in milliseconds
