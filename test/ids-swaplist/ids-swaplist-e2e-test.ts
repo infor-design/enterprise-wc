3f8669685433f7497cbc1b357e4ca159a591bffb
Tim McConechy <tim.mcconechy@infor.com>
2022-09-12 09:58:38 -0400

    Adjust tests

--- test/ids-swaplist/ids-swaplist-e2e-test.ts
+++ test/ids-swaplist/ids-swaplist-e2e-test.ts
@@ -10,6 +10,8 @@ describe('Ids Swap List e2e Tests', () => {
 
   it('should not have errors', async () => {
     await expect(page.title()).resolves.toMatch('IDS Swaplist Component');
+    const count = (await page.$$('pierce/ids-swappable-item')).length;
+    expect(count).toEqual(15);
   });
 
   it('should pass Axe accessibility tests', async () => {
