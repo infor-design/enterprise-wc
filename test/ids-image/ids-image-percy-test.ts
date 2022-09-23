a3db1de3652575a47f5ded3da9f5bfe3b1d7400c
jmacaluso711 <macaluso.john@gmail.com>
2022-09-23 13:55:57 -0400

    Remove wait function

--- test/ids-image/ids-image-percy-test.ts
+++ test/ids-image/ids-image-percy-test.ts
@@ -1,12 +1,10 @@
 import percySnapshot from '@percy/puppeteer';
-import wait from '../helpers/wait';
 
 describe('Ids Image Percy Tests', () => {
   const url = 'http://localhost:4444/ids-image/example.html';
 
   it('should not have visual regressions in new light theme (percy)', async () => {
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
-    await wait(2000);
     await percySnapshot(page, 'ids-image-new-light');
   });
 
@@ -17,21 +15,17 @@ describe('Ids Image Percy Tests', () => {
 
   it('should not have visual regressions in new dark theme (percy)', async () => {
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
-    await wait(2000);
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'dark');
     });
-    await wait(2000);
     await percySnapshot(page, 'ids-image-new-dark');
   });
 
   it('should not have visual regressions in new contrast theme (percy)', async () => {
     await page.goto(url, { waitUntil: ['networkidle2', 'load'] });
-    await wait(2000);
     await page.evaluate(() => {
       document.querySelector('ids-theme-switcher')?.setAttribute('mode', 'contrast');
     });
-    await wait(2000);
     await percySnapshot(page, 'ids-image-new-contrast');
   });
 });
