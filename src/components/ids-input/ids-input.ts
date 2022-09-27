3939a3f2ce6b07f50b4efafbe679dfd64cd265bb
Andrii Varhanov <andrii.varhanov@gmail.com>
2022-09-27 18:54:26 +0300

    Remove if in cursor property #922

--- src/components/ids-input/ids-input.ts
+++ src/components/ids-input/ids-input.ts
@@ -842,9 +842,7 @@ export default class IdsInput extends Base {
    */
   set cursor(value: string) {
     this.setAttribute(attributes.CURSOR, value);
-    if (this.input) {
-      this.input.style.cursor = value;
-    }
+    this.input?.style.setProperty('cursor', value);
   }
 
   get cursor(): string {
