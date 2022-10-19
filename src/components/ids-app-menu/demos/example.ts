aed7dea4270bf7cb0cdc7cef2116fb21eafe314c
Andrii Varhanov <andrii.varhanov@gmail.com>
2022-10-19 12:10:12 +0300

    Make the menu "push" over a container in the main example #852

--- src/components/ids-app-menu/demos/example.ts
+++ src/components/ids-app-menu/demos/example.ts
@@ -1,21 +1,36 @@
 import '../../ids-header/ids-header';
 
 import avatarPlaceholder from '../../../assets/images/avatar-placeholder.jpg';
+import css from '../../../assets/css/ids-app-menu/example.css';
 
 const avatarImg: any = window.document.getElementById('avatar');
 avatarImg.src = avatarPlaceholder;
 
+// Custom Css
+const cssLink = `<link href="${css}" rel="stylesheet">`;
+(document.querySelector('head') as any).insertAdjacentHTML('afterbegin', cssLink);
+
 document.addEventListener('DOMContentLoaded', () => {
   const appMenuDrawer: any = document.querySelector('#app-menu');
   const appMenuTriggerBtn: any = document.querySelector('#app-menu-trigger');
+  const container: any = document.querySelector('ids-container');
 
-  appMenuDrawer.target = appMenuTriggerBtn;
-  appMenuTriggerBtn.addEventListener('click', () => {
-    appMenuTriggerBtn.disabled = true;
+  appMenuTriggerBtn.addEventListener('click', (e) => {
+    if (appMenuDrawer.visible) {
+      appMenuDrawer.hide();
+    } else {
+      appMenuDrawer.show();
+    }
+  });
+
+  appMenuDrawer.onOutsideClick = () => {};
+
+  appMenuDrawer.addEventListener('show', () => {
+    container.classList.add('app-menu-is-open');
   });
 
   appMenuDrawer.addEventListener('hide', () => {
-    appMenuTriggerBtn.disabled = false;
+    container.classList.remove('app-menu-is-open');
   });
 
   appMenuDrawer.addEventListener('selected', (e: CustomEvent) => {
