20430c98d844338136a706e2894cbd51b7676948
Tim McConechy <tim.mcconechy@infor.com>
2022-12-20 09:57:00 -0500

    Add new example for m3

---
+++ src/components/ids-data-grid/demos/m3-features.ts
@@ -0,0 +1,253 @@
+import type IdsDataGrid from '../ids-data-grid';
+import '../ids-data-grid';
+import type { IdsDataGridColumn } from '../ids-data-grid-column';
+import productInfo from '../../../assets/data/product-info.json';
+
+// Example for populating the DataGrid
+const dataGrid = document.querySelector<IdsDataGrid>('#m3-virtual-scroll')!;
+
+// Do an ajax request
+const url: any = productInfo;
+const columns: IdsDataGridColumn[] = [];
+
+// Set up columns
+columns.push({
+  id: 'selectionCheckbox',
+  name: 'selection',
+  sortable: false,
+  resizable: false,
+  formatter: dataGrid.formatters.selectionCheckbox,
+  align: 'center'
+});
+columns.push({
+  id: 'id',
+  name: 'Id',
+  field: 'id',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'itemNumber',
+  name: 'Item Number',
+  field: 'itemNumber',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.hyperlink,
+  filterType: dataGrid.filters.text,
+  filterConditions: []
+});
+columns.push({
+  id: 'testingProcedure',
+  name: 'Testing Procedure',
+  field: 'testingProcedure',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'eco',
+  name: 'Eco',
+  field: 'eco',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.checkbox
+});
+columns.push({
+  id: 'check',
+  name: 'Check',
+  field: 'check',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.date
+});
+columns.push({
+  id: 'release',
+  name: 'Release',
+  field: 'release',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'itemMgr',
+  name: 'Item Mgr',
+  field: 'itemMgr',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'allocation',
+  name: 'Allocation',
+  field: 'allocation',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'unitOfMeasure',
+  name: 'Unit Of Measure',
+  field: 'unitOfMeasure',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'businessArea',
+  name: 'Business Area',
+  field: 'businessArea',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'price',
+  name: 'Price',
+  field: 'price',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.decimal
+});
+columns.push({
+  id: 'color',
+  name: 'Color',
+  field: 'color',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'icon',
+  name: 'Icon',
+  field: 'icon',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'qtyOnHand',
+  name: 'Qty On Hand',
+  field: 'qtyOnHand',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'refProductId',
+  name: 'Ref Product Id',
+  field: 'refProductId',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'sku',
+  name: 'SKU',
+  field: 'sku',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'upc',
+  name: 'UPC',
+  field: 'upc',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'category',
+  name: 'Category',
+  field: 'category',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'weight',
+  name: 'Weight',
+  field: 'weight',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'shipWeight',
+  name: 'Ship Weight',
+  field: 'shipWeight',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'manufacturer',
+  name: 'Manufacturer',
+  field: 'manufacturer',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.text
+});
+columns.push({
+  id: 'qtyOnOrder',
+  name: 'Qty On Order',
+  field: 'qtyOnOrder',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.integer
+});
+columns.push({
+  id: 'enable',
+  name: 'Enable',
+  field: 'enable',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.checkbox
+});
+columns.push({
+  id: 'taxable',
+  name: 'Taxable',
+  field: 'taxable',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.checkbox
+});
+columns.push({
+  id: 'lowStockAlert',
+  name: 'Low Stock Alert',
+  field: 'lowStockAlert',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.checkbox
+});
+columns.push({
+  id: 'createdData',
+  name: 'Created Data',
+  field: 'createdData',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.date
+});
+columns.push({
+  id: 'modifiedData',
+  name: 'Modified Data',
+  field: 'modifiedData',
+  sortable: true,
+  resizable: true,
+  formatter: dataGrid.formatters.date
+});
+
+dataGrid.columns = columns;
+
+const setData = async () => {
+  const res = await fetch(url);
+  const data = await res.json();
+  dataGrid.data = data;
+};
+
+setData();
+
+dataGrid.addEventListener('selectionchanged', (e: Event) => {
+  console.info(`Selection Changed`, (<CustomEvent>e).detail);
+});
