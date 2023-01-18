0d434d44f316bd32c786b2eb7d53ac3f1c316a1f
Andrii Varhanov <andrii.varhanov@gmail.com>
2023-01-18 19:34:59 +0200

    Add func test for hide focus mixin #1044

--- test/mixins/ids-hide-focus-mixin-func-test.ts
+++ test/mixins/ids-hide-focus-mixin-func-test.ts
@@ -4,21 +4,33 @@
 import IdsButton from '../../src/components/ids-button/ids-button';
 
 describe('IdsHideFocusMixin Tests', () => {
-  let idsButton: any;
+  let button: any;
 
-  beforeEach(async () => {
-    // create ids button
-    idsButton = new IdsButton();
-    idsButton.innerHTML = '<span>Default Button</span>';
-    document.body.appendChild(idsButton);
+  beforeEach(() => {
+    button = new IdsButton();
+    button.innerHTML = '<span>Default Button</span>';
+    document.body.appendChild(button);
   });
 
-  afterEach(async () => {
-    // clear body
+  afterEach(() => {
     document.body.innerHTML = '';
   });
 
-  it('should add ripple effect on click.ripple', async () => {
+  it('should add/remove CSS class on property/attribute change', () => {
+    expect(button.hideFocus).toBeTruthy();
+    expect(button.container.classList.contains('hide-focus')).toBeTruthy();
 
+    button.hideFocus = false;
+
+    expect(button.hideFocus).toBeFalsy();
+    expect(button.getAttribute('hide-focus')).toEqual('false');
+    expect(button.container.classList.contains('hide-focus')).toBeFalsy();
+  });
+
+  it('should add/remove CSS class on click/focus', async () => {
+    button.focus();
+    expect(button.container.classList.contains('hide-focus')).toBeFalsy();
+    button.dispatchEvent(new MouseEvent('mousedown'));
+    expect(button.container.classList.contains('hide-focus')).toBeTruthy();
   });
 });
