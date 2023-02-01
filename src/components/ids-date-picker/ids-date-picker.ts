8755d19bd6b1c9f5b6729e0dc465cbfca616f606
jdilag <jdilag.dev@gmail.com>
2023-02-01 09:59:21 -0500

    revert datepicker parse fn

--- src/components/ids-date-picker/ids-date-picker.ts
+++ src/components/ids-date-picker/ids-date-picker.ts
@@ -553,7 +553,7 @@ class IdsDatePicker extends Base {
   parseEventDate(val: string) {
     if (!val || typeof val !== 'string') return;
 
-    const date = this.locale.parseDate(val) as Date;
+    const date = new Date(val);
     if (!isValidDate(date)) return;
 
     const month = date.getMonth();
