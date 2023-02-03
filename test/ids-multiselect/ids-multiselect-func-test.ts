7ef9d87d88d7220414d0bd9a5d847f5bc5ec9b59
Ed Coyle <ed.coyle@infor.com>
2023-02-03 16:23:52 -0500

    Fix some multiselect tests

--- test/ids-multiselect/ids-multiselect-func-test.ts
+++ test/ids-multiselect/ids-multiselect-func-test.ts
@@ -9,6 +9,7 @@ import IdsLocaleData from '../../src/components/ids-locale/ids-locale-data';
 
 import IdsMultiselect from '../../src/components/ids-multiselect/ids-multiselect';
 import '../../src/components/ids-dropdown/ids-dropdown';
+import '../../src/components/ids-dropdown/ids-dropdown-list';
 import '../../src/components/ids-list-box/ids-list-box';
 import '../../src/components/ids-list-box/ids-list-box-option';
 import '../../src/components/ids-trigger-field/ids-trigger-field';
@@ -231,7 +232,7 @@ describe('IdsMultiselect Component', () => {
   it('can click outside an open list to close it', (done) => {
     const clickEvent = new MouseEvent('click', { bubbles: true });
 
-    multiselect.onOutsideClick = jest.fn();
+    multiselect.dropdownList.onOutsideClick = jest.fn();
     multiselect.open();
 
     setTimeout(() => {
@@ -239,7 +240,7 @@ describe('IdsMultiselect Component', () => {
       document.body.dispatchEvent(clickEvent);
 
       setTimeout(() => {
-        expect(multiselect.onOutsideClick).toHaveBeenCalled();
+        expect(multiselect.dropdownList.onOutsideClick).toHaveBeenCalled();
         done();
       });
     }, 70);
@@ -357,10 +358,10 @@ describe('IdsMultiselect Component', () => {
   it('selects on space/enter when open', () => {
     multiselect.value = [];
     multiselect.open();
-    multiselect.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown' }));
-    multiselect.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown' }));
-    multiselect.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown' }));
-    multiselect.dispatchEvent(new KeyboardEvent('keydown', { key: ' ' }));
+    multiselect.dropdownList.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown' }));
+    multiselect.dropdownList.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown' }));
+    multiselect.dropdownList.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowDown' }));
+    multiselect.dropdownList.dispatchEvent(new KeyboardEvent('keydown', { key: ' ' }));
 
     multiselect.close();
     expect(multiselect.value).toEqual(['opt5']);
