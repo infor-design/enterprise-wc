6e4582a4a37a5e29d869e396a0c231e0afc9a8ab
Tim McConechy <tim.mcconechy@infor.com>
2023-02-20 11:11:22 -0500

    Fix build and import issues

--- src/components/ids-calendar/ids-calendar.ts
+++ src/components/ids-calendar/ids-calendar.ts
@@ -5,6 +5,7 @@ import IdsEventsMixin from '../../mixins/ids-events-mixin/ids-events-mixin';
 import IdsCalendarEventsMixin from '../../mixins/ids-calendar-events-mixin/ids-calendar-events-mixin';
 import IdsDateAttributeMixin from '../../mixins/ids-date-attribute-mixin/ids-date-attribute-mixin';
 import { CalendarEventData, CalendarEventTypeData } from './ids-calendar-event';
+import '../ids-date-picker/ids-date-picker';
 import IdsDatePickerPopup from '../ids-date-picker/ids-date-picker-popup';
 import IdsMonthView from '../ids-month-view/ids-month-view';
 import IdsWeekView from '../ids-week-view/ids-week-view';
