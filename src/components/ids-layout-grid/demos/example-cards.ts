bad3750490bb19f298f1548b408bfd431ae42944
jmacaluso711 <macaluso.john@gmail.com>
2023-03-07 13:41:04 -0500

    Rename layout grid examples

--- src/components/ids-layout-grid/demos/example-cards.ts
+++ src/components/ids-layout-grid/demos/example-cards.ts
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const btnToggle: any = document.querySelector('#edit-toggle-button');
-  const gridCells: any = document.querySelectorAll('ids-grid-cell');
+  const gridCells: any = document.querySelectorAll('ids-layout-grid-cell');
 
   btnToggle?.addEventListener('click', (e: any) => {
     e.target.toggle();
