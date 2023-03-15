57649ea1254c9f17a7c0c8566d5148483c02f530
Tim McConechy <tim.mcconechy@infor.com>
2023-03-15 13:49:06 -0400

    Add Close menu example

---
+++ src/components/ids-popup-menu/demos/side-by-side.ts
@@ -0,0 +1,15 @@
+// Add Event For Web Component Menu
+document.querySelector('ids-popup-menu#popupmenu')!.addEventListener('show', () => {
+  console.info('On Show', $('input#input-menu').data('popupmenu'));
+
+  const input = $('input#input-menu');
+  if (input.data('popupmenu').isOpen) {
+    input.data('popupmenu').close();
+  }
+});
+
+// Add Event for old component
+$('input#input-menu')!.on('beforeopen', () => {
+  console.info('On Before Open');
+  document.querySelector('ids-popup-menu#popupmenu')?.setAttribute('hidden', '');
+});
