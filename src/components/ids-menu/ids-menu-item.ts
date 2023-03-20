7d3bad05a31614173f38972586c1cd348a6ffe50
Ed Coyle <ed.coyle@infor.com>
2023-03-20 14:55:03 -0400

    Fix editor test failures due to menu item setter change

--- src/components/ids-menu/ids-menu-item.ts
+++ src/components/ids-menu/ids-menu-item.ts
@@ -526,7 +526,7 @@ export default class IdsMenuItem extends Base {
       return;
     }
 
-    if (stringToBool(this.state.selected) !== trueVal) {
+    if (this.state.selected !== trueVal) {
       if (this.isSelectable) {
         this.state.selected = trueVal;
 
