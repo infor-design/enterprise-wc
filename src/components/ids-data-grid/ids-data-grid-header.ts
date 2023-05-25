a1f8863b2591029b00b97d82cbedff723a672931
Tim McConechy <tim.mcconechy@infor.com>
2023-05-25 13:22:41 -0400

    Fix error in tests

--- src/components/ids-data-grid/ids-data-grid-header.ts
+++ src/components/ids-data-grid/ids-data-grid-header.ts
@@ -193,9 +193,11 @@ export default class IdsDataGridHeader extends IdsEventsMixin(IdsElement) {
       const curIndex = cell.getAttribute('aria-colindex');
       const cellLeft = rect.left + (startIndex < curIndex ? rect.width + 1 : 1);
       const cellRight = rect.left + (startIndex < curIndex ? 1 : rect.width + 1);
-      const offsetLeft = (this.offsetParent?.getBoundingClientRect().left || 0)
-          - ((this.offsetParent as any).offsetLeft || 0);
-
+      let offsetLeft = 0;
+      if (this.offsetParent) {
+        offsetLeft = this.offsetParent.getBoundingClientRect().left
+          - (this.offsetParent as HTMLElement).offsetLeft;
+      }
       dragArrows?.style.setProperty('left', `${this.dataGrid?.localeAPI.isRTL() ? cellRight - offsetLeft : cellLeft - offsetLeft}px`);
       dragArrows?.style.setProperty('height', `${rect.height}px`);
       dragArrows?.style.setProperty('display', 'block');
