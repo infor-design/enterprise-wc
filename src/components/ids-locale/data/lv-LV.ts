fe23f317d9541860dc5a76b89aa32b321fb3dde0
Tim McConechy <tim.mcconechy@infor.com>
2023-08-31 11:43:03 -0400

    Fix latvian locale date

--- src/components/ids-locale/data/lv-LV.ts
+++ src/components/ids-locale/data/lv-LV.ts
@@ -13,7 +13,7 @@ const locale = {
     dateFormat: {
       separator: '.', // Infered
       timeSeparator: ':',
-      short: 'dd.MM.yyyy', // use four digit year
+      short: 'dd.MM.yyyy.', // use four digit year
       medium: 'MMM yyyy',
       long: 'd. MMMM yyyy',
       full: 'EEEE, y. gada d. MMMM',
