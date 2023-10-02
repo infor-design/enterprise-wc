5f96b493860be36e18110a2a3fb50b8355b62a8f
Clay Clecidor <clay.clecidor@infor.com>
2023-10-02 14:57:31 -0400

    1352-list-view-refresh-list-on-new-data

--- src/components/ids-list-view/demos/data-changing.ts
+++ src/components/ids-list-view/demos/data-changing.ts
@@ -1,27 +1,37 @@
 import '../ids-list-view';
+import type IdsButton from '../../ids-button/ids-button';
 import type IdsListView from '../ids-list-view';
 import productsJSON from '../../../assets/data/products-100.json';
 
 const listView = document.querySelector<IdsListView>('ids-list-view');
+const randomizeDataBtn = document.querySelector<IdsButton>('ids-button');
 
 if (listView) {
   const url: any = productsJSON;
+  let data: any = [];
 
   const setInitialData = async () => {
     const res = await fetch(url);
-    const data = await res.json();
-    listView.data = data.slice(0, 10);
+    data = await res.json();
+    listView.data = data;
   };
   setInitialData();
 
-  // Changes data when an item is activated
-  const setNewData = async (e: any) => {
-    console.info(e.detail.index);
+  randomizeDataBtn?.addEventListener('click', () => {
+    const numProducts = Math.floor(Math.random() * data.length);
+    listView.data = [...data].slice(numProducts);
+  });
 
-    const res = await fetch(url);
-    const data = await res.json();
-    listView.data = data.slice(11, 16);
-  };
+  // listView.addEventListener('itemactivated', setNewData);
+  listView.addEventListener('itemactivated', (e: any) => {
+    console.log('itemactivated', <CustomEvent>e.detail);
+  });
 
-  listView.addEventListener('itemactivated', setNewData);
+  listView.addEventListener('selected', (e: any) => {
+    console.log('selected', <CustomEvent>e.detail);
+  });
+
+  listView.addEventListener('deselected', (e: any) => {
+    console.log('deselected', <CustomEvent>e.detail);
+  });
 }
