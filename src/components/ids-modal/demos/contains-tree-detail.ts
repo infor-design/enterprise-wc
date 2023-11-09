bcff729b2155fce9de2783ee95e20231d8751358
Ed Coyle <ed.coyle@infor.com>
2023-11-09 11:30:41 -0500

    Add modal example showing splitter/tree-detail pattern

---
+++ src/components/ids-modal/demos/contains-tree-detail.ts
@@ -0,0 +1,69 @@
+import treeBasicJSON from '../../../assets/data/tree-basic.json';
+import eventsJSON from '../../../assets/data/accounts.json';
+
+import '../../ids-button/ids-button';
+import '../../ids-list-view/ids-list-view';
+import '../ids-modal';
+import '../../ids-modal-button/ids-modal-button';
+import '../../ids-splitter/ids-splitter';
+import '../../ids-splitter/ids-splitter-pane';
+import '../../ids-tree/ids-tree';
+
+import type IdsModal from '../ids-modal';
+import type IdsListView from '../../ids-list-view/ids-list-view';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const triggerId = '#modal-trigger-btn';
+  const triggerBtn: any = document.querySelector(triggerId);
+  const modal = document.querySelector<IdsModal>('ids-modal')!;
+  const tree: any = document.querySelector('ids-tree');
+  const listView = document.querySelector<IdsListView>('ids-list-view:not([id])');
+
+  modal.style.setProperty('--ids-modal-content-padding', '0');
+  modal.style.setProperty('--ids-modal-footer-margin', '1px 0 0 0');
+
+  // get tree data
+  if (tree) {
+    (async function init() {
+      // Do an ajax request
+      const res = await fetch(treeBasicJSON as any);
+      const data: any = await res.json();
+      tree.data = data;
+    }());
+  }
+
+  // get listView data
+  if (listView) {
+    // Do an ajax request and apply the data to the list
+    const url: any = eventsJSON;
+
+    const setData = async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      (listView as any).data = data;
+    };
+
+    setData();
+  }
+
+  // Links the Modal to its trigger button (sets up click/focus events)
+  modal.target = triggerBtn;
+  modal.triggerType = 'click';
+  modal.fullsize = 'always';
+
+  // Disable the trigger button when showing the Modal.
+  modal.addEventListener('beforeshow', () => {
+    triggerBtn.disabled = true;
+    return true;
+  });
+
+  // Close the modal when its inner button is clicked.
+  modal.onButtonClick = () => {
+    modal.hide();
+  };
+
+  // After the modal is done hiding, re-enable its trigger button.
+  modal.addEventListener('hide', () => {
+    triggerBtn.disabled = false;
+  });
+});
