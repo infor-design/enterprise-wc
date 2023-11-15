579ced3621d7a845fdba7092f7e2eb9b5f79abdd
Tim McConechy <tim.mcconechy@infor.com>
2023-11-15 15:34:49 -0500

    up the timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 10000,
+  timeout: 12500,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
