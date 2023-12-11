02722f42059e1acf0f27ce70a17cc72e7f8155aa
Ed Coyle <ed.coyle@infor.com>
2023-12-11 16:49:12 -0500

    Add Scroll View and Layout Grid test page

---
+++ src/components/ids-scroll-view/demos/layout-grid.ts
@@ -0,0 +1,7 @@
+// Supporting components
+import '../ids-scroll-view';
+import '../../ids-layout-grid/ids-layout-grid';
+import '../../ids-layout-grid/ids-layout-grid-cell';
+import '../../ids-icon/ids-icon';
+import '../../ids-text/ids-text';
+
