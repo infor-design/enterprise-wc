ddf9a329fc583f0a9450efd750d10be642231a31
Ed Coyle <ed.coyle@infor.com>
2023-12-12 10:53:08 -0500

    Remove layout grid demo

--- src/components/ids-scroll-view/demos/layout-grid.ts
+++
@@ -1,6 +0,0 @@
-// Supporting components
-import '../ids-scroll-view';
-import '../../ids-layout-grid/ids-layout-grid';
-import '../../ids-layout-grid/ids-layout-grid-cell';
-import '../../ids-icon/ids-icon';
-import '../../ids-text/ids-text';
