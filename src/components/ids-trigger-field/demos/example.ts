d5a97e6933fe01e951ba8965647cb1f5b26d8801
Ed Coyle <ed.coyle@infor.com>
2023-12-15 11:05:43 -0500

    Correct IdsPopupMenu/IdsTriggerButton targeting/arrows

--- src/components/ids-trigger-field/demos/example.ts
+++ src/components/ids-trigger-field/demos/example.ts
@@ -1,5 +1,7 @@
 import statesJSON from '../../../assets/data/states.json';
 
+import type IdsPopupMenu from '../../ids-popup-menu/ids-popup-menu';
+
 const autocomplete: Element | any = document.querySelector('#trigger-field-7');
 const url: any = statesJSON;
 
@@ -12,3 +14,7 @@ if (autocomplete) {
 
   await setData();
 }
+
+// Configures some PopupMenu properties
+const popupMenu8 = document.querySelector<IdsPopupMenu>('#icon-menu')!;
+popupMenu8.popup!.arrow = 'left';
