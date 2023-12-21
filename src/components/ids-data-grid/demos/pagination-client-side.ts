aa7108f79613be98606363f90a306272cf7ea98d
Ed Coyle <ed.coyle@infor.com>
2023-12-21 10:11:32 -0500

    Implement 'add', 'clear', and 'delete' buttons in client side paging example

--- src/components/ids-data-grid/demos/pagination-client-side.ts
+++ src/components/ids-data-grid/demos/pagination-client-side.ts
@@ -1,10 +1,18 @@
 import productsJSON from '../../../assets/data/products.json';
 import type IdsDataGrid from '../ids-data-grid';
+import type IdsPopupMenu from '../../ids-popup-menu/ids-popup-menu';
+import type IdsMenuItem from '../../ids-menu/ids-menu-item';
 import '../ids-data-grid';
 import type { IdsDataGridColumn } from '../ids-data-grid-column';
 
 // Example for populating the DataGrid
 const dataGrid = document.querySelector<IdsDataGrid>('#data-grid-paging-client-side')!;
+const rowHeightMenu = document.querySelector<IdsPopupMenu>('#row-height-menu')!;
+
+// Change row height with popup menu
+rowHeightMenu?.addEventListener('selected', (e: Event) => {
+  dataGrid.rowHeight = (e.target as IdsMenuItem).value as string;
+});
 
 (async function init() {
   const columns: IdsDataGridColumn[] = [];
@@ -87,4 +95,24 @@ const dataGrid = document.querySelector<IdsDataGrid>('#data-grid-paging-client-s
   });
 
   console.info('Loading Time:', window.performance.now());
+
+  // Example Buttons
+  document.querySelector('#add-row')?.addEventListener('click', () => {
+    const newRow = { description: 'New Row', ledgder: 'CORE' };
+    dataGrid.addRow(newRow);
+    dataGrid.setActiveCell(0, dataGrid.data.length - 1);
+    dataGrid.editFirstCell();
+  });
+
+  document.querySelector('#delete-row')?.addEventListener('click', () => {
+    dataGrid.selectedRows.reverse().forEach((row: any) => {
+      dataGrid.removeRow(row.index);
+    });
+  });
+
+  document.querySelector('#clear-row')?.addEventListener('click', () => {
+    dataGrid.selectedRows.reverse().forEach((row: any) => {
+      dataGrid.clearRow(row.index);
+    });
+  });
 }());
