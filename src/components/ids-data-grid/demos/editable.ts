6b436da4c909270bda3ee6d500e59d17e18a4212
Andrii Varhanov <andrii.varhanov@infor.com>
2024-02-14 13:19:12 +0200

    Include the cell to grid.dirtyCells and mark the cell as dirty with the orange mark #1747

--- src/components/ids-data-grid/demos/editable.ts
+++ src/components/ids-data-grid/demos/editable.ts
@@ -206,7 +206,7 @@ rowHeightMenu?.addEventListener('selected', (e: Event) => {
     editor: {
       type: 'checkbox',
       editorSettings: {
-        dirtyTracker: false,
+        dirtyTracker: true,
       }
     },
   });
