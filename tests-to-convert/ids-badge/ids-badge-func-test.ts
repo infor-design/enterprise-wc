2f1ba3c42fa204b1f7584f3046f23bd47dceb813
Glenlie <glenliemaye.orillo@infor.com>
2024-03-07 17:15:43 +0800

    initial code for badge conversion

--- tests-to-convert/ids-badge/ids-badge-func-test.ts
+++ tests-to-convert/ids-badge/ids-badge-func-test.ts
@@ -16,79 +16,7 @@ describe('IdsBadge Component', () => {
   afterEach(async () => {
     document.body.innerHTML = '';
   });
-
-  test('renders alert color and round shape of badge', () => {
-    badge.color = 'alert';
-    expect(badge.getAttribute('color')).toEqual('alert');
-    expect(badge.color).toEqual('alert');
-    badge.shape = 'round';
-    expect(badge.getAttribute('shape')).toEqual('round');
-    expect(badge.shape).toEqual('round');
-  });
-
-  test('renders error color of badge', () => {
-    badge.color = 'error';
-    expect(badge.getAttribute('color')).toEqual('error');
-    expect(badge.color).toEqual('error');
-  });
-
-  test('renders info color of badge', () => {
-    badge.color = 'info';
-    expect(badge.getAttribute('color')).toEqual('info');
-    expect(badge.color).toEqual('info');
-  });
-
-  test('renders warning color and round shape of badge', () => {
-    badge.color = 'warning';
-    expect(badge.getAttribute('color')).toEqual('warning');
-    expect(badge.color).toEqual('warning');
-    badge.shape = 'round';
-    expect(badge.getAttribute('shape')).toEqual('round');
-    expect(badge.shape).toEqual('round');
-  });
-
-  test('renders success color of badge', () => {
-    badge.color = 'success';
-    expect(badge.getAttribute('color')).toEqual('success');
-    expect(badge.color).toEqual('success');
-  });
-
-  test('renders badge with normal icon', () => {
-    badge.color = 'alert';
-    expect(badge.getAttribute('color')).toBe('alert');
-    expect(badge.color).toBe('alert');
-  });
-
-  test('removes the color and attribute', () => {
-    badge.color = 'alert';
-    expect(badge.getAttribute('color')).toEqual('alert');
-    expect(badge.color).toEqual('alert');
-
-    badge.removeAttribute('color');
-    expect(badge.getAttribute('color')).toEqual(null);
-    expect(badge.color).toEqual(null);
-  });
-
-  test('removes the shape attribute', () => {
-    badge.shape = 'round';
-    expect(badge.getAttribute('shape')).toEqual('round');
-    expect(badge.shape).toEqual('round');
-
-    badge.removeAttribute('shape');
-    expect(badge.getAttribute('shape')).toEqual(null);
-    expect(badge.shape).toEqual('normal');
-  });
-
-  test('should be able to set attributes before append', async () => {
-    const elem: any = new IdsBadge();
-    elem.color = 'error';
-    elem.shape = 'round';
-    document.body.appendChild(elem);
-
-    expect(elem.container.getAttribute('color')).toEqual('error');
-    expect(elem.container.classList.contains('round')).toBeTruthy();
-  });
-
+  
   test('should be able to set attributes after append', async () => {
     const elem: any = new IdsBadge();
     document.body.appendChild(elem);
@@ -97,16 +25,5 @@ describe('IdsBadge Component', () => {
 
     expect(elem.container.classList.contains('round')).toBeTruthy();
     expect(elem.container.getAttribute('color')).toEqual('error');
-  });
-
-  test('should be able to set disabled', async () => {
-    expect(badge.getAttribute('disabled')).toEqual(null);
-    expect(badge.disabled).toEqual(false);
-    badge.disabled = true;
-    expect(badge.getAttribute('disabled')).toEqual('');
-    expect(badge.disabled).toEqual(true);
-    badge.disabled = false;
-    expect(badge.getAttribute('disabled')).toEqual(null);
-    expect(badge.disabled).toEqual(false);
   });
 });
--- tests/ids-badge/ids-badge.spec.ts
+++ tests/ids-badge/ids-badge.spec.ts
@@ -1,9 +1,10 @@
 import AxeBuilder from '@axe-core/playwright';
 import percySnapshot from '@percy/playwright';
-import { expect } from '@playwright/test';
+import { Locator, expect } from '@playwright/test';
 import { test } from '../base-fixture';
 
 import IdsBadge from '../../src/components/ids-badge/ids-badge';
+import IdsContainer from '../../src/components/ids-container/ids-container';
 
 test.describe('IdsBadge tests', () => {
   const url = '/ids-badge/example.html';
@@ -62,5 +63,93 @@ test.describe('IdsBadge tests', () => {
       if (browserName !== 'chromium') return;
       await percySnapshot(page, 'ids-badge-light');
     });
+  });
+  test('can change color', async ({ page }) => {
+    const colors = ['alert', 'error', 'info', 'warning', 'success', 'invalid', null];
+    const badge: Locator = page.locator('ids-badge:first-child');
+    const span: Locator = badge.locator('span');
+    for (const color of colors) {
+      await badge.evaluate((element: IdsBadge, tColor: string | null) => {
+        element.color = tColor;
+      }, color);
+      if (color) {
+        await expect(badge).toHaveAttribute('color', color);
+        await expect(span).toHaveAttribute('color', color);
+      } else {
+        await expect(badge).not.toHaveAttribute('color');
+        await expect(span).not.toHaveAttribute('color');
+      }
+    }
+  });
+
+  test('can change shape', async ({ page }) => {
+    const shapes = ['round', 'normal', null];
+    const badge: Locator = page.locator('ids-badge:first-child');
+    const span: Locator = badge.locator('span');
+    for (const shape of shapes) {
+      await badge.evaluate((element: IdsBadge, tShape: string | null) => {
+        element.shape = tShape;
+      }, shape);
+      if (shape) {
+        await expect(badge).toHaveAttribute('shape', shape);
+        await expect(span).toHaveClass(new RegExp(shape, 'g'));
+      } else {
+        await expect(badge).not.toHaveAttribute('shape');
+        await expect(span).toHaveClass(/normal/);
+      }
+    }
+  });
+
+  // to improve code
+  test('able to set attributes before append', async ({ page }) => {
+    await page.evaluate(() => {
+      const element = document.createElement('ids-badge') as IdsBadge;
+      element.setAttribute('id', 'test-badge');
+      element.color = 'error';
+      element.shape = 'round';
+      (document.querySelector<IdsContainer>('ids-container'))!.appendChild(element);
+    });
+    const badge = page.locator('#test-badge');
+    await expect(badge).toBeAttached();
+    await expect(badge).toHaveAttribute('color', 'error');
+  });
+
+  test('can disabled/enabled', async ({ page }) => {
+    const badge: Locator = page.locator('ids-badge:first-child');
+    await expect(badge).toBeEnabled();
+    await badge.evaluate((element: IdsBadge) => { element.disabled = true; });
+    await expect(badge).toHaveAttribute('disabled');
+    await badge.evaluate((element: IdsBadge) => { element.disabled = false; });
+    await expect(badge).not.toHaveAttribute('enabled');
+  });
+
+  test('removes the color attribute', async ({ page }) => {
+    let badge: Locator = page.locator('ids-badge[color="error"]').first();
+    let span: Locator = badge.locator('span');
+    await expect(badge).toHaveAttribute('color');
+    await expect(span).toHaveAttribute('color');
+    await badge.evaluate((element: IdsBadge) => {
+      element.removeAttribute('color');
+      element.setAttribute('id', 'test-badge');
+    });
+    badge = page.locator('#test-badge');
+    span = badge.locator('span');
+    await expect(badge).not.toHaveAttribute('color');
+    await expect(span).not.toHaveAttribute('color');
+  });
+
+  test('removes the shape attribute', async ({ page }) => {
+    let badge: Locator = page.locator('ids-badge[color="warning"][shape="round"]').first();
+    let span: Locator = badge.locator('span');
+    await expect(badge).toHaveAttribute('shape');
+    await expect(span).toHaveClass(/round/);
+    await badge.evaluate((element: IdsBadge) => {
+      element.removeAttribute('shape');
+      element.setAttribute('id', 'test-badge');
+    });
+    badge = page.locator('#test-badge');
+    span = badge.locator('span');
+    await expect(badge).not.toHaveAttribute('shape');
+    await expect(span).not.toHaveClass(/round/);
   });
 });
