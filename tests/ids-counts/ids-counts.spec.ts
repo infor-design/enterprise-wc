6e721e87d475973fa2a1b400de6af745c861af59
Cheno Barcena <johnedwincheno.barcena@infor.com>
2024-03-15 22:46:59 +0800

    convert ids-count script

--- tests/ids-counts/ids-counts.spec.ts
+++ tests/ids-counts/ids-counts.spec.ts
@@ -7,9 +7,13 @@ import IdsCounts from '../../src/components/ids-counts/ids-counts';
 
 test.describe('IdsCounts tests', () => {
   const url = '/ids-counts/example.html';
+  let el: any;
+  let counts: any;
 
   test.beforeEach(async ({ page }) => {
     await page.goto(url);
+    el = 'ids-count';
+    counts = await page.locator('ids-counts').first();
   });
 
   test.describe('general page checks', () => {
@@ -44,15 +48,15 @@ test.describe('IdsCounts tests', () => {
     test('should match innerHTML snapshot', async ({ page, browserName }) => {
       if (browserName !== 'chromium') return;
       const handle = await page.$('ids-counts');
-      const html = await handle?.evaluate((el: IdsCounts) => el?.outerHTML);
+      const html = await handle?.evaluate((eL: IdsCounts) => eL?.outerHTML);
       await expect(html).toMatchSnapshot('counts-html');
     });
 
     test('should match shadowRoot snapshot', async ({ page, browserName }) => {
       if (browserName !== 'chromium') return;
       const handle = await page.$('ids-counts');
-      const html = await handle?.evaluate((el: IdsCounts) => {
-        el?.shadowRoot?.querySelector('style')?.remove();
+      const html = await handle?.evaluate((eL: IdsCounts) => {
+        eL?.shadowRoot?.querySelector('style')?.remove();
         return el?.shadowRoot?.innerHTML;
       });
       await expect(html).toMatchSnapshot('counts-shadow');
@@ -61,6 +65,64 @@ test.describe('IdsCounts tests', () => {
     test('should match the visual snapshot in percy', async ({ page, browserName }) => {
       if (browserName !== 'chromium') return;
       await percySnapshot(page, 'ids-counts-light');
+    });
+
+    test.describe('e2e tests', () => {
+      test('renders a specific hex color', async () => {
+        await counts.evaluate((element: IdsCounts, eColor: string | null) => {
+          element.color = eColor;
+        }, '#800000');
+        expect(await counts.getAttribute('color')).toEqual('#800000');
+      });
+
+      test('renders a status color', async () => {
+        await counts.evaluate((element: IdsCounts, eColor: string | null) => {
+          element.color = eColor;
+        }, 'base');
+        expect(await counts.getAttribute('color')).toEqual('base');
+      });
+
+      test('is able to change sizes via compact attribute', async () => {
+        await counts.evaluate((element: IdsCounts, val: string | boolean) => {
+          element.compact = val;
+        }, 'true');
+        expect(await counts.getAttribute('compact')).toEqual('true');
+      });
+
+      test('is able to change link via href attribute', async () => {
+        await counts.evaluate((element: IdsCounts, val: string | null) => {
+          element.href = val;
+        }, 'http://www.google.com');
+        expect(await counts.getAttribute('href')).toEqual('http://www.google.com');
+      });
+
+      test('creates an ids-hyperlink container', async ({ page }) => {
+        const hyperlinks = await page.locator('ids-hyperlink').all();
+        await page.evaluate(() => {
+          const template = `
+          <ids-counts href="#">
+            <ids-text count-value>71</ids-text>
+            <ids-text count-text>Active <br /> Opportunities</ids-text>
+          </ids-counts>`;
+          document.querySelector('ids-counts')!.remove();
+          document.body.insertAdjacentHTML('beforeend', template);
+        });
+        await expect(hyperlinks).toHaveLength(5);
+      });
+
+      test('creates a compact counts component', async ({ page }) => {
+        const countVal = await counts.locator('ids-text[count-value]');
+        await page.evaluate(() => {
+          const compact = `
+          <ids-counts compact="true" href="#">
+            <ids-text count-value>7</ids-text>
+            <ids-text count-text>Active <br /> Opportunities</ids-text>
+          </ids-counts>`;
+          document.querySelector('ids-counts')!.remove();
+          document.body.insertAdjacentHTML('beforeend', compact);
+        });
+        await expect(countVal).toHaveCSS('font-size', '16px');
+      });
     });
   });
 });
