f4f3acbfcd0db891c376def93e858eb6988adb03
Clay Clecidor <clay.clecidor@infor.com>
2024-03-26 13:16:02 -0400

    1918-ids-axis-chart-tests (ids-nice-scale-func-test.spec.ts)

---
+++ tests/ids-axis-chart/ids-nice-scale-func-test.spec.ts
@@ -0,0 +1,49 @@
+import { expect } from '@playwright/test';
+import { test } from '../base-fixture';
+import NiceScale from '../../src/components/ids-axis-chart/ids-nice-scale';
+
+test.describe('Nice Scale Tests', () => {
+  test('computes scales', () => {
+    let scale = new NiceScale(-0.085, 0.173);
+    expect(scale).toEqual({ niceMax: 0.2, niceMin: -0.1, tickSpacing: 0.05 });
+
+    scale = new NiceScale(100, 200, { maxTicks: 10, minPoint: 100, maxPoint: 200 });
+    expect(scale).toEqual({ niceMax: 200, niceMin: 100, tickSpacing: 10 });
+
+    scale = new NiceScale(105, 543);
+    expect(scale).toEqual({ niceMax: 550, niceMin: 100, tickSpacing: 50 });
+
+    scale = new NiceScale(2.04, 2.16);
+    expect(scale).toEqual({ niceMax: 2.16, niceMin: 2.04, tickSpacing: 0.02 });
+
+    scale = new NiceScale(0, 100);
+    expect(scale).toEqual({ niceMax: 100, niceMin: 0, tickSpacing: 10 });
+
+    scale = new NiceScale(0, 9999);
+    expect(scale).toEqual({ niceMax: 10000, niceMin: 0, tickSpacing: 1000 });
+
+    scale = new NiceScale(0, 10);
+    expect(scale).toEqual({ niceMax: 10, niceMin: 0, tickSpacing: 1 });
+  });
+
+  test('can loop the scale', () => {
+    const scale: any = new NiceScale(0, 3411);
+    expect(scale).toEqual({ niceMax: 3500, niceMin: 0, tickSpacing: 500 });
+    const values = [];
+    for (let i = scale.niceMin; i <= scale.niceMax; i += scale.tickSpacing) {
+      values.push(i);
+    }
+    expect(values).toEqual([0, 500, 1000, 1500, 2000, 2500, 3000, 3500]);
+  });
+
+  test('can set the max ticks the scale', () => {
+    const scale = new NiceScale(0, 3411, { maxTicks: 8 });
+    expect(scale).toEqual({ niceMax: 4000, niceMin: 0, tickSpacing: 1000 });
+
+    const values = [];
+    for (let i = scale.niceMin; i <= scale.niceMax; i += (Number(scale.tickSpacing))) {
+      values.push(i);
+    }
+    expect(values).toEqual([0, 1000, 2000, 3000, 4000]);
+  });
+});
