cf3fa62e93d639c53652cbe38afe37e37c0f7b6a
Tim McConechy <tim.mcconechy@infor.com>
2024-04-02 15:21:00 -0400

    Configure and improve tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Retry if fails */
   retries: 2,
   /* Set workers process.env.CI ? 2 :  */
-  workers: 5,
+  workers: process.env.CI ? 3 : undefined,
   /* Control the snap shot names */
   snapshotPathTemplate: '{testDir}/{testFileDir}/snapshots/{arg}.snap',
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
