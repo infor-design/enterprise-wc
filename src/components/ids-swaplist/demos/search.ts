dc3bcb370f0650d74f7587e9a6c31b0fba1faf23
Clay Clecidor <clay.clecidor@infor.com>
2024-04-09 01:07:45 -0400

    1702-ids-swaplist-search-filter

---
+++ src/components/ids-swaplist/demos/search.ts
@@ -0,0 +1,18 @@
+import swaplistData from '../../../assets/data/swaplist-data.json';
+
+// Example for populating the List View
+const swaplist: any = document.querySelector('#swaplist-1');
+const url: any = swaplistData;
+
+// Listen for swaplist updates
+swaplist?.addEventListener('updated', (e: any) => {
+  console.info('Swaplist Updated', e.detail.data);
+});
+
+const setData = async () => {
+  const res = await fetch(url);
+  const data = await res.json();
+  swaplist.data = data;
+};
+
+await setData();
