c757602ecf03789dfd41442b5999da3f4807ca41
Clay Clecidor <clay.clecidor@infor.com>
2024-04-15 17:53:31 -0400

    2186-ids-upload-trigger-focusin

--- src/mixins/ids-hide-focus-mixin/ids-hide-focus-mixin.ts
+++ src/mixins/ids-hide-focus-mixin/ids-hide-focus-mixin.ts
@@ -62,7 +62,7 @@ const IdsHideFocusMixin = <T extends Constraints>(superclass: T) => class extend
     this.#removeHideFocusEvents();
 
     this.onEvent('focusin.hide-focus', this, (e) => {
-      if (!this.#isClick && !this.#isFocused) {
+      if (!this.#isFocused) {
         this.#addIsFocusedCssClass();
         this.#removeHideFocusCssClass();
         this.triggerEvent('hidefocusremove', this, e);
