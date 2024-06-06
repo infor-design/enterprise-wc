c08b84b54e298030d903f5455ed6162f69bbd594
jmacaluso711 <macaluso.john@gmail.com>
2024-06-06 12:05:46 -0400

    Width adjustment to token columns

--- src/components/ids-personalize/demos/token-table.ts
+++ src/components/ids-personalize/demos/token-table.ts
@@ -24,7 +24,8 @@ columns.push({
     console.info('Tree Expander Clicked', info);
   },
   sortable: false,
-  resizable: false,
+  resizable: true,
+  minWidth: 400,
   width: 400,
   formatter: dataGrid.formatters.tree,
   filterType: dataGrid.filters.text
@@ -35,7 +36,8 @@ columns.push({
   name: 'Token Value',
   field: 'tokenValue',
   sortable: false,
-  resizable: false,
+  resizable: true,
+  minWidth: 400,
   width: 400,
   formatter: dataGrid.formatters.text,
   filterType: dataGrid.filters.text
