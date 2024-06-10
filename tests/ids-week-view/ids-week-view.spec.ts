fec1b2ba962df8a8c979ffd0bc8ae70c60c6c89e
Norberto Ace Ancog <156752175+n-ace-ancog@users.noreply.github.com>
2024-06-10 19:32:01 +0800

    Initial test conversion

--- tests/ids-week-view/ids-week-view.spec.ts
+++ tests/ids-week-view/ids-week-view.spec.ts
@@ -1,7 +1,8 @@
 import AxeBuilder from '@axe-core/playwright';
 import percySnapshot from '@percy/playwright';
-import { expect } from '@playwright/test';
+import { Locator, expect } from '@playwright/test';
 import { test } from '../base-fixture';
+import IdsWeekView from '../../src/components/ids-week-view/ids-week-view';
 
 test.describe('IdsWeekView tests', () => {
   const url = '/ids-week-view/example.html';
@@ -43,6 +44,72 @@ test.describe('IdsWeekView tests', () => {
     test.skip('should match the visual snapshot in percy', async ({ page, browserName }) => {
       if (browserName !== 'chromium') return;
       await percySnapshot(page, 'ids-week-view-light');
+    });
+  });
+
+  test.describe('functionality test', () => {
+    let idsWeekView: Locator;
+
+    test.beforeEach(async ({ page }) => {
+      idsWeekView = await page.locator('ids-week-view').first();
+    });
+
+    test('can append early to DOM', async ({ page, pageErrorsTest }) => {
+      await page.evaluate(() => {
+        const elem = document.createElement('ids-week-view')! as IdsWeekView;
+        document.querySelector('ids-container')!.appendChild(elem);
+        elem.setAttribute('id', 'new-week-view');
+        elem.name = 'test-week-view';
+      });
+      await page.locator('#new-week-view').waitFor({ state: 'attached' });
+      expect(pageErrorsTest.hasErrors()).toBeFalsy();
+    });
+
+    test('can append late to DOM', async ({ page, pageErrorsTest }) => {
+      await page.evaluate(() => {
+        const elem = document.createElement('ids-week-view')! as IdsWeekView;
+        elem.setAttribute('id', 'new-week-view');
+        elem.name = 'test-week-view';
+        document.querySelector('ids-container')!.appendChild(elem);
+      });
+      await page.locator('#new-week-view').waitFor({ state: 'attached' });
+      expect(pageErrorsTest.hasErrors()).toBeFalsy();
+    });
+
+    test('can be inserted in an element', async ({ page, pageErrorsTest }) => {
+      await page.evaluate(() => {
+        document.querySelector('ids-container')!.insertAdjacentHTML('beforeend', `<ids-week-view id="new-week-view"></ids-week-view>`);
+      });
+      await page.locator('#new-week-view').waitFor({ state: 'attached' });
+      expect(pageErrorsTest.hasErrors()).toBeFalsy();
+    });
+
+    test('can set/get showToday', async () => {
+      const testData = [
+        { data: true, expected: true },
+        { data: 'false', expected: false },
+        { data: '', expected: true },
+        { data: null, expected: false }
+      ];
+
+      expect(await idsWeekView.evaluate((element: IdsWeekView) => element.showToday)).toBeTruthy();
+      await expect(idsWeekView).toHaveAttribute('show-today', 'true');
+
+      for (const data of testData) {
+        expect(await idsWeekView.evaluate((element: IdsWeekView, tData) => {
+          element.showToday = tData as any;
+          return element.showToday;
+        }, data.data)).toEqual(data.expected);
+        if (data.expected) {
+          await expect(idsWeekView).toHaveAttribute('show-today', 'true');
+        } else {
+          await expect(idsWeekView).not.toHaveAttribute('show-today');
+        }
+      }
+    });
+
+    test('can set/get startDate', async () => {
+      expect(await idsWeekView.evaluate((element: IdsWeekView) => element.startDate)).toContain('Oct 23 2019');
     });
   });
 });
